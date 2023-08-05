3c8494a49af7ae126111779e202b932ef9ca4e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:48 +0000

    Add package so3g/0.1.1/so3g-0.1.1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/so3g/so3g-0.1.1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/so3g/spt3g_internal/version.py
@@ -0,0 +1,12 @@
+# AUTO-GENERATED FILE: DO NOT EDIT
+
+# Contains version control information (revision IDs, paths, etc.)
+# as of the last time make was run.
+
+upstream_url="UNKNOWN VCS"
+upstream_branch="UNKNOWN VCS"
+revision="38006325f2af57f369f44cc6d325f656fa5ef0af"
+gitrevision="38006325f2af57f369f44cc6d325f656fa5ef0af"
+localdiffs=True
+versionname=""
+fullversion="0.3-97-g3800632-dirty"
