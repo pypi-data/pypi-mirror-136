df2835023e8afcf1f52f23f207db1eb180387e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:12 +0000

    Add package swachhdata/1.2.8/swachhdata-1.2.8.tar.gz

---
+++ packages/swachhdata/swachhdata-1.2.8.tar.gz/swachhdata-1.2.8/README.md
@@ -0,0 +1,26 @@
+![](https://raw.githubusercontent.com/swachhdata/swachhdata/main/logo/sd-cover.png)
+
+# swachhdata
+
+[![pypi package](https://badge.fury.io/py/swachhdata.svg)](https://pypi.org/project/swachhdata)
+[![Downloads](https://static.pepy.tech/personalized-badge/swachhdata?period=total&units=international_system&left_color=gray&right_color=blue&left_text=Downloads)](https://pepy.tech/project/swachhdata)
+[![GitHub last commit](https://img.shields.io/github/last-commit/kritikseth/swachhdata.svg)](https://github.com/swachhdata/swachhdata/)
+[![GitHub](https://img.shields.io/github/license/kritikseth/swachhdata.svg)](https://github.com/kritikseth/swachhdata/blob/master/LICENSE)
+
+* Simple and efficient tools for cleaning and transforming data
+* Accessible to everybody, and reusable in various contexts
+* Open source, commercially usable - MPL-2.0 license
+
+## Documentation- 
+
+* https://swachhdata.readthedocs.io/en/latest/
+* [Examples](https://colab.research.google.com/drive/1IH7ve5xoQ4vLyrRP4HvTCYBlj1Ub1GGS?usp=sharing#scrollTo=3Seymy37xQk4)
+
+## Author-
+
+<a href="https://kritikseth.github.io/ipynbtagredirect" target="_parent"><img src="https://raw.githack.com/kritikseth/kritikseth/master/assets/icons/kritik_ipynbtagredirect.svg" alt="Kritik Seth"/></a>
+
+
+<p align='center'>
+  <img align='center' src="https://visitor-badge.glitch.me/badge?page_id=swachhdata.visitor-badge">
+<p/>
---
+++ packages/swachhdata/swachhdata-1.2.8.tar.gz/swachhdata-1.2.8/setup.py
@@ -0,0 +1,45 @@
+import os
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='swachhdata',
+    version='1.2.8',
+    author='Kritik Seth',
+    author_email='sethkritik@gmail.com',
+    description='Data cleaning made easy with swachhdata',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/swachhdata/swachhdata',
+    classifiers=[
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta'
+    ],
+    packages=find_namespace_packages(include=['swachhdata', 'swachhdata.*']),
+    install_requires=[
+        'regex>=2019.12.20',
+        'pandas>=1.1.4',
+        'tqdm>=4.41.1',
+        'beautifulsoup4>=4.6.3',
+        'html5lib>=1.0.1',
+        'contractions>=0.0.25',
+        'emoji>=0.6.0',
+        'nltk>=3.6.5',
+        'spacy>=2.2.4',
+        'gensim>=3.6.0',
+        'num2words>=0.5.10',
+        'textblob>=0.15.3',
+        'requests>=2.23.0',
+        'opencv-python>=4.1.2.30',
+        'tweepy>=3.6.0'
+    ]
+)
