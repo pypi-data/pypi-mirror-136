1e3cfb6eb9a2a25069ee55b266d54f4dc4054a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:18:02 +0000

    Add package vandal/3.3.1/vandal-3.3.1.tar.gz

---
+++ packages/vandal/vandal-3.3.1.tar.gz/vandal-3.3.1/vandal/__main__.py
@@ -0,0 +1,15 @@
+# imports all CLI clients.
+from vandal.objects import montecarlo 
+import vandal
+
+# runs the main CLI client.
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--option', help = 'Enter options', type = str, 
+    choices = ['montecarlo'])
+    args = parser.parse_args()
+
+    if args.option == 'montecarlo' or args.option == 'mc':
+        vandal.objects.montecarlo.Main()
+
