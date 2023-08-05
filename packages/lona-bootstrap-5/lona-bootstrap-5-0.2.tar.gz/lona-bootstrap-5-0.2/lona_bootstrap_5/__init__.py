aad0562fed2e4c4bfdd7c3ecf05fe4ee1b8aa58a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:14 +0000

    Add package lona-bootstrap-5/0.2/lona-bootstrap-5-0.2.tar.gz

---
+++ packages/lona-bootstrap-5/lona-bootstrap-5-0.2.tar.gz/lona-bootstrap-5-0.2/lona_bootstrap_5/__init__.py
@@ -0,0 +1,12 @@
+try:
+    from .progress import Progress  # NOQA
+    from .modal import Modal  # NOQA
+    from .nodes import *  # NOQA
+
+except ImportError:
+    # this happens while packaging and can be ignored
+
+    pass
+
+VERSION = (0, 2)
+VERSION_STRING = '{}'.format('.'.join([str(i) for i in VERSION]))
