e13388085b0ecaa92f438d825ad0e08ae00466a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:18 +0000

    Add package eyja-nats-hub/0.1.5/eyja_nats_hub-0.1.5-py3-none-any.whl

---
+++ packages/eyja-nats-hub/eyja_nats_hub-0.1.5-py3-none-any.whl/eyja_nats/__init__.py
@@ -0,0 +1,24 @@
+from eyja.interfaces.plugins import BasePlugin
+from eyja.constants.types import PluginTypes
+
+from .nats_hub import NATSHub
+from .nats_route_group import (
+    NATSRoute,
+    NATSRouteGroup,
+)
+
+
+class RethinkDBPlugin(BasePlugin):
+    name = 'nats'
+    plugin_type = PluginTypes.HUB
+
+    @classmethod
+    async def init(cls):
+        await NATSHub.init()
+
+
+__all__ = [
+    'NATSHub',
+    'NATSRoute',
+    'NATSRouteGroup',
+]
