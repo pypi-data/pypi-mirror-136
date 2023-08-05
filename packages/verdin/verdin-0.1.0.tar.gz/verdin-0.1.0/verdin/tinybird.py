97d962b8c7025bc102daf07f6c67cdbc9e5f75d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:26 +0000

    Add package verdin/0.1.0/verdin-0.1.0.tar.gz

---
+++ packages/verdin/verdin-0.1.0.tar.gz/verdin-0.1.0/verdin/tinybird.py
@@ -0,0 +1,15 @@
+from .client import Client
+from .datasource import Datasource, Record
+from .pipe import Pipe, PipeError, PipeJsonData, PipeJsonResponse, PipeMetadata
+
+__all__ = [
+    "Client",
+    "Datasource",
+    "Record",
+    "Pipe",
+    "PipeError",
+    "PipeMetadata",
+    "PipeJsonData",
+    "PipeJsonResponse",
+    "PipePageIterator",
+]
