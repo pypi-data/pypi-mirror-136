3ab1dc30e52651a17cd9e976d80d8db9480dd322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:05:56 +0000

    Add package mmf-meta/0.1.7/mmf_meta-0.1.7-py3-none-any.whl

---
+++ packages/mmf-meta/mmf_meta-0.1.7-py3-none-any.whl/mmf_meta/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "0.1.7"
+from .core import target, artifact
+from .descriptors import (
+    DescriptorBase,
+    DataFrameFormat,
+    DataFrame,
+    ColorMode,
+    ImageFormat,
+    Image,
+    String,
+    Integer,
+    Float,
+    Datetime,
+    Bool,
+    Dict,
+    JsonFile,
+)
+from .__main__ import cli
