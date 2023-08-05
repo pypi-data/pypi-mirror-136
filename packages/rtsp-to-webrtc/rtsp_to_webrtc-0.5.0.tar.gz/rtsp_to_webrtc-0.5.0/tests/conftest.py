bf45b6940483879e361873e7bd3f383acb1989c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:29 +0000

    Add package rtsp-to-webrtc/0.5.0/rtsp_to_webrtc-0.5.0.tar.gz

---
+++ packages/rtsp-to-webrtc/rtsp_to_webrtc-0.5.0.tar.gz/rtsp_to_webrtc-0.5.0/tests/conftest.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+import logging
+from collections.abc import Awaitable, Callable, Generator
+from typing import cast
+
+import aiohttp
+import pytest
+from aiohttp import web
+
+from rtsp_to_webrtc import diagnostics
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def handler(request: aiohttp.web.Request) -> aiohttp.web.Response:
+    """Handles the request, inserting response prepared by tests."""
+    if request.method == "POST":
+        await request.post()
+    response = request.app["response"].pop(0)
+    request.app["request"].append(request.url.path)
+    return cast(aiohttp.web.Response, response)
+
+
+@pytest.fixture
+async def request_handler() -> Callable[
+    [aiohttp.web.Request], Awaitable[aiohttp.web.Response]
+]:
+    return handler
+
+
+@pytest.fixture
+def app() -> web.Application:
+    app = web.Application()
+    app["response"] = []
+    app["request"] = []
+    return app
+
+
+@pytest.fixture(autouse=True)
+def reset_diagnostics() -> Generator[None, None, None]:
+    yield
+    diagnostics.reset()
