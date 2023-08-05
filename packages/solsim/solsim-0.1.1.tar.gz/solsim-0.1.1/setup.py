95adcbdcb34380fe4a3db8a5be0aeebc00fbd2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:15 +0000

    Add package solsim/0.1.1/solsim-0.1.1.tar.gz

---
+++ packages/solsim/solsim-0.1.1.tar.gz/solsim-0.1.1/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['solsim']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['anchorpy>=0.6.4,<0.7.0',
+ 'numpy>=1.22.1,<2.0.0',
+ 'pandas>=1.4.0,<2.0.0',
+ 'tqdm>=4.62.3,<5.0.0']
+
+setup_kwargs = {
+    'name': 'solsim',
+    'version': '0.1.1',
+    'description': 'The Solana complex systems simulator.',
+    'long_description': None,
+    'author': 'Will Wolf',
+    'author_email': 'williamabrwolf@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
