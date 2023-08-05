3e5abe44ff05db0f2213daada735a26519e34eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:53 +0000

    Add package scan2wiki/0.0.12/scan2wiki-0.0.12.tar.gz

---
+++ packages/scan2wiki/scan2wiki-0.0.12.tar.gz/scan2wiki-0.0.12/setup.py
@@ -0,0 +1,45 @@
+# ! important
+# see https://stackoverflow.com/a/27868004/1497139
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='scan2wiki',
+    version='0.0.12',
+
+    packages=['scan'],
+    classifiers=[
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9'
+    ],
+
+    install_requires=[
+      'gitpython',
+      'jinja2',
+      'pywikibot',
+      'py-3rdparty-mediawiki',
+      'pycryptodome',
+      'mwclient',
+      'pyLodStorage',
+      'pyFlaskBootstrap4~=0.4.4'
+    ],
+    entry_points={
+      'console_scripts': [
+        'scan2wiki = scan.webserver:main',
+      ],
+    },
+    author='Wolfgang Fahl',
+    maintainer='Wolfgang Fahl',
+    url='https://github.com/WolfgangFahl/scan2wiki',
+    license='Apache License',
+    description='Scan to Wiki by watching a scan folder',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
