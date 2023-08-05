ad3d68b9e7a0f03d43dc7d17593fea2a60a699c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:36 +0000

    Add package cirq-core/0.14.0.dev20220125024703/cirq_core-0.14.0.dev20220125024703-py3-none-any.whl

---
+++ packages/cirq-core/cirq_core-0.14.0.dev20220125024703-py3-none-any.whl/cirq/interop/__init__.py
@@ -0,0 +1,7 @@
+# pylint: disable=wrong-or-nonexistent-copyright-notice
+"""Package containing code for interoperating with other quantum software."""
+
+from cirq.interop.quirk import (
+    quirk_json_to_circuit,
+    quirk_url_to_circuit,
+)
