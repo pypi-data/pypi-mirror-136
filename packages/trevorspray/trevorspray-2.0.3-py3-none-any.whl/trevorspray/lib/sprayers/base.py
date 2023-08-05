b01caa1b2a2691122e874f92e16393f2abf1b6f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:19 +0000

    Add package trevorspray/2.0.3/trevorspray-2.0.3-py3-none-any.whl

---
+++ packages/trevorspray/trevorspray-2.0.3-py3-none-any.whl/trevorspray/lib/sprayers/base.py
@@ -0,0 +1,130 @@
+import logging
+import requests
+from urllib.parse import quote
+from ..util import windows_user_agent
+from ..errors import TREVORSprayError
+
+log = logging.getLogger('trevorspray.sprayers.base')
+
+class BaseSprayModule:
+
+    # HTTP method
+    method = 'POST'
+    # default target URL
+    default_url = None
+    # alternative IPv6 URL
+    ipv6_url = None
+    # name of username parameter
+    userparam = 'username'
+    # name of password parameter
+    passparam = 'password'
+    # other global parameters
+    globalparams = {}
+    # body of request
+    request_data = None
+    request_json = None
+    # HTTP headers
+    headers = {}
+    # HTTP cookies
+    cookies = {}
+    # Don't count nonexistent accounts as failed logons
+    fail_nonexistent = False
+    # Module for looting after successful login
+    looter = None
+
+    def __init__(self, trevor):
+
+        self.trevor = trevor
+        if self.trevor.options.url is not None:
+            self.url = str(self.trevor.options.url)
+        elif self.default_url is not None:
+            self.url = str(self.default_url)
+        else:
+            raise TREVORSprayError('Please specify a --url to spray against')
+
+        if self.ipv6_url and self.url == self.default_url and self.trevor.options.prefer_ipv6:
+            self.url = self.ipv6_url
+
+        # make sure we have a user-agent
+        if not self.headers.get('User-Agent', ''):
+            self.headers['User-Agent'] = windows_user_agent
+
+
+    def initialize(self):
+        return True
+
+
+    def create_params(self, username, password):
+
+        return {
+            self.userparam: username,
+            self.passparam: password
+        }
+
+
+    def create_request(self, username, password):
+        '''
+        Returns request.Request() object
+        '''
+
+        runtimeparams = self.create_params(username, password)
+
+        url = self.url.format(**self.globalparams, **runtimeparams)
+        if not url.lower().startswith('http'):
+            url = f'https://{url}'
+
+        data = None
+        params = dict(self.globalparams)
+        params.update(runtimeparams)
+        params.update(self.trevor.runtimeparams)
+        if type(self.request_data) == dict:
+            data = dict(self.request_data)
+            data.update(params)
+        elif type(self.request_data) == str:
+            params = {k: quote(v) for k,v in params.items()}
+            data = self.request_data.format(**params)
+
+        json = None
+        if type(self.request_json) == dict:
+            json = dict(self.request_json)
+            json.update(params)
+
+        return requests.Request(
+            method=self.method,
+            url=url,
+            headers=self.headers,
+            cookies=self.cookies,
+            data=data,
+            json=json
+        )
+
+
+    def check_response(self, response):
+        '''
+        returns (valid, exists, locked, msg)
+        '''
+
+        valid = False
+        exists = None
+        locked = None
+        msg = ''
+
+        if getattr(response, 'status_code', 0) == 200:
+            valid = True
+            exists = True
+            msg = 'Valid cred'
+
+        return (valid, exists, locked, msg)
+
+
+    def loot(self, credential):
+
+        if self.looter is not None:
+            looter = self.looter(self, credential)
+            looter.run()
+
+
+    @property
+    def id(self):
+
+        return f'{self.__class__.__name__}|{self.url}'
---
+++ packages/trevorspray/trevorspray-2.0.3-py3-none-any.whl/trevorspray/lib/util.py
@@ -0,0 +1,158 @@
+import json
+import logging
+import requests
+import tldextract
+import subprocess as sp
+from pathlib import Path
+import lxml.etree as etree
+from pygments import highlight
+from contextlib import suppress
+from urllib.parse import urlparse
+from pygments.lexers.html import XmlLexer
+from pygments.lexers.data import JsonLexer
+from pygments.formatters import TerminalFormatter
+
+log = logging.getLogger('trevorspray.util')
+
+
+windows_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36 Edg/96.0.1054.43'
+
+
+def highlight_json(j):
+
+    if type(j) in [dict, list, set, tuple]:
+        j = json.dumps(j, indent=4)
+
+    return highlight(j, JsonLexer(), TerminalFormatter())
+
+
+def highlight_xml(x):
+
+    if type(x) == str:
+        x = str.encode()
+
+    with suppress(Exception):
+        x = etree.tostring(etree.fromstring(x), pretty_print=True)
+
+    return highlight(x, XmlLexer(), TerminalFormatter())
+
+
+def files_to_list(l):
+
+    new_list = dict()
+    for entry in l:
+        entry = str(entry)
+        try:
+            with open(entry) as f:
+                for line in f.readlines():
+                    entry = line.strip('\r\n').lower()
+                    new_list[entry] = True
+        except OSError:
+            if entry and entry not in new_list:
+                new_list[entry] = True
+
+    return new_list
+
+
+def update_file(filename, l):
+    '''
+    Update file "filename" with entries from list "l"
+    Only unique entries are added
+    '''
+
+    final_list = dict()
+    try:
+        with open(str(filename)) as f:
+            for line in f:
+                final_list[line.strip()] = True
+    except OSError as e:
+        log.debug(f'Could not read file {filename}: {e}')
+    for entry in l:
+        final_list[entry] = True
+    with open(filename, 'w') as f:
+        f.writelines([f'{e}\n' for e in final_list])
+
+
+def read_file(filename, key=lambda x: True):
+
+    final_list = dict()
+    try:
+        with open(str(filename)) as f:
+            for line in f.readlines():
+                entry = line.strip()
+                if key(entry):
+                    final_list[entry] = True
+    except OSError as e:
+        log.debug(f'Could not read file {filename}: {e}')
+
+    return final_list
+
+
+def ssh_key_encrypted(f=None):
+
+    if f is None:
+        f = Path.home() / '.ssh/id_rsa'
+
+    try:
+        p = sp.run(['ssh-keygen', '-y', '-P', '', '-f', str(f)], stdout=sp.DEVNULL, stderr=sp.PIPE)
+        if not 'incorrect' in p.stderr.decode():
+            return False
+    except:
+        pass
+    return True
+
+
+def is_domain(d):
+
+    extracted = tldextract.extract(d)
+    if extracted.domain and not extracted.subdomain:
+        return True
+    return False
+
+
+def is_subdomain(d):
+
+    extracted = tldextract.extract(d)
+    if extracted.domain and extracted.subdomain:
+        return True
+    return False
+
+
+def is_url(d):
+
+    parsed = urlparse(d)
+    if parsed.scheme or '/' in parsed.path or parsed.query:
+        return True
+    return False
+
+
+def download_file(url, filename, **kwargs):
+
+    log.debug(f'Downloading file from {url} to {filename}, {kwargs}')
+    with request('GET', url, stream=True, **kwargs) as response:
+        log.debug(f'Download result: HTTP {response.status_code}, Size: {len(response.text)}')
+        response.raise_for_status()
+        with open(filename, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=8192):  
+                f.write(chunk)
+
+
+def request(*args, **kwargs):
+
+    if not args and 'method' not in kwargs:
+        kwargs['method'] = 'GET'
+
+    headers = kwargs.get('headers', {})
+    if 'User-Agent' not in headers:
+        headers.update({
+            'User-Agent': windows_user_agent
+        })
+    kwargs['headers'] = headers
+
+    if not 'verify' in kwargs:
+        kwargs['verify'] = False
+
+    log.debug(f'Web request: {str(args)}, {str(kwargs)}')
+    response = requests.request(*args, **kwargs)
+    log.debug(f'{response} (Length: {len(response.content)})')
+    return response
