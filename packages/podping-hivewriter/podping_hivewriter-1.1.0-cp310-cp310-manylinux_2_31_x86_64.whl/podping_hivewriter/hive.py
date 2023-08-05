6e9562db7fd184b7258be3f75f9b68a98aa853f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:16 +0000

    Add package podping-hivewriter/1.1.0/podping_hivewriter-1.1.0-cp310-cp310-manylinux_2_31_x86_64.whl

---
+++ packages/podping-hivewriter/podping_hivewriter-1.1.0-cp310-cp310-manylinux_2_31_x86_64.whl/podping_hivewriter/hive.py
@@ -0,0 +1,40 @@
+import logging
+from typing import List, Optional, Set
+
+from lighthive.client import Client
+
+
+def get_client(
+    posting_keys: Optional[List[str]] = None,
+    nodes=None,
+    connect_timeout=3,
+    read_timeout=30,
+    loglevel=logging.ERROR,
+    chain=None,
+) -> Client:
+    try:
+        client = Client(
+            keys=posting_keys,
+            nodes=nodes,
+            connect_timeout=connect_timeout,
+            read_timeout=read_timeout,
+            loglevel=loglevel,
+            chain=chain,
+            automatic_node_selection=False,
+        )
+        return client
+    except Exception as ex:
+        raise ex
+
+
+def get_allowed_accounts(
+    client: Client = None, account_name: str = "podping"
+) -> Set[str]:
+    """get a list of all accounts allowed to post by acc_name (podping)
+    and only react to these accounts"""
+
+    if not client:
+        client = get_client()
+
+    master_account = client.account(account_name)
+    return set(master_account.following())
