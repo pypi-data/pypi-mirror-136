4ee29e3698ed205986cb3ec56ae566d240b5c9d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:50 +0000

    Add package podunk/0.9.1/podunk-0.9.1.tar.gz

---
+++ packages/podunk/podunk-0.9.1.tar.gz/podunk-0.9.1/podunk/prefab/paper.py
@@ -0,0 +1,11 @@
+#------------------------------------------------------------------------------
+#   file:       podunk/prefab/paper.py
+#   author:     Jim Storch
+#------------------------------------------------------------------------------
+
+LETTER_PORTRAIT = (612, 792)
+LETTER_LANDSCAPE = (792, 612)
+LEGAL_PORTRAIT = (612, 1008)
+LEGAL_LANDSCAPE = (1008, 612)
+A4_PORTRAIT = (595, 842)
+A4_LANDSCAPE = (842, 595)
