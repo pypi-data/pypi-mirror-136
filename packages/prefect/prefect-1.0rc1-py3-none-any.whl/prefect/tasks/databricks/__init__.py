0b68fd5031ae2f020e871712c52e39dc5d96ac4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:40 +0000

    Add package prefect/1.0rc1/prefect-1.0rc1-py3-none-any.whl

---
+++ packages/prefect/prefect-1.0rc1-py3-none-any.whl/prefect/tasks/databricks/__init__.py
@@ -0,0 +1,8 @@
+"""
+This module contains a collection of tasks for interacting with Databricks resources.
+"""
+
+from prefect.tasks.databricks.databricks_submitjob import DatabricksSubmitRun
+from prefect.tasks.databricks.databricks_submitjob import DatabricksRunNow
+
+__all__ = ["DatabricksRunNow", "DatabricksSubmitRun"]
