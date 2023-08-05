d25ad321476b9bc71b19f19c69f1d01130932fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:34 +0000

    Add package opensarlab-notifications/0.1.4/opensarlab_notifications-0.1.4-py3-none-any.whl

---
+++ packages/opensarlab-notifications/opensarlab_notifications-0.1.4-py3-none-any.whl/opensarlab_notifications/handlers.py
@@ -0,0 +1,39 @@
+import os
+import json
+
+from notebook.base.handlers import APIHandler
+from notebook.utils import url_path_join
+
+import tornado
+from tornado.web import StaticFileHandler
+
+from .notifications import notes
+
+class RouteHandler(APIHandler):
+    # The following decorator should be present on all verb methods (head, get, post,
+    # patch, put, delete, options) to ensure only authorized user can request the
+    # Jupyter server
+    @tornado.web.authenticated
+    def get(self):
+        profile_name = os.environ.get('OPENSARLAB_PROFILE_NAME', '')
+        ical_url = os.environ.get('ICAL_URL', '')
+        events = notes(profile_name, ical_url)
+        self.finish(json.dumps({"data": events}))
+
+def setup_handlers(web_app, url_path):
+    host_pattern = ".*$"
+    base_url = web_app.settings["base_url"]
+
+    # Prepend the base_url so that it works in a JupyterHub setting
+    route_pattern = url_path_join(base_url, url_path, "notifications")
+    handlers = [(route_pattern, RouteHandler)]
+    web_app.add_handlers(host_pattern, handlers)
+
+    # Prepend the base_url so that it works in a JupyterHub setting
+    doc_url = url_path_join(base_url, url_path, "public")
+    doc_dir = os.getenv(
+        "JLAB_SERVER_EXAMPLE_STATIC_DIR",
+        os.path.join(os.path.dirname(__file__), "public"),
+    )
+    handlers = [("{}/(.*)".format(doc_url), StaticFileHandler, {"path": doc_dir})]
+    web_app.add_handlers(".*$", handlers)
