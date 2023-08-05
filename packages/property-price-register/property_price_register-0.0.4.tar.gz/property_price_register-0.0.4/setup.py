e4510ed9c19fcc600848a203abbf0f403a2ae929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:41 +0000

    Add package property-price-register/0.0.4/property_price_register-0.0.4.tar.gz

---
+++ packages/property-price-register/property_price_register-0.0.4.tar.gz/property_price_register-0.0.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import (
+    find_packages,
+    setup
+)
+
+INSTALL_REQUIRES = (
+    'cached_property',
+    'requests',
+    'pandas'
+)
+
+setup(
+    name='property_price_register',
+    version='0.0.4',
+    python_requires='>=3.6',
+    author='Robert Lucey',
+    url='https://github.com/RobertLucey/property-price-register',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=INSTALL_REQUIRES,
+    package_data={
+        'property_price_register': [
+            'resources/PPR.csv.tar.gz',
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'load_property_price_register = property_price_register.bin.load:main',
+        ]
+    }
+)
