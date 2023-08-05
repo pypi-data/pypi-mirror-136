f233d5468d4d61226a27ef2364cf5945c9c9118c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:23 +0000

    Add package imio.directory.policy/1.0a1/imio.directory.policy-1.0a1.tar.gz

---
+++ packages/imio.directory.policy/imio.directory.policy-1.0a1.tar.gz/imio.directory.policy-1.0a1/src/imio/directory/policy/setuphandlers.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from imio.directory.policy.utils import remove_unused_contents
+from plone import api
+from Products.CMFPlone.interfaces import INonInstallable
+from Products.CMFQuickInstallerTool import interfaces as quiskinstallinterfaces
+from zope.interface import implementer
+
+
+@implementer(INonInstallable)
+class HiddenProfiles(object):
+    def getNonInstallableProfiles(self):
+        """Hide uninstall profile from site-creation and quickinstaller."""
+        return [
+            "imio.directory.core:default",
+            "imio.directory.policy:uninstall",
+        ]
+
+
+@implementer(quiskinstallinterfaces.INonInstallable)
+class HiddenProducts(object):
+    def getNonInstallableProducts(self):
+        """Hides profiles from QuickInstaller"""
+        return [
+            u"imio.directory.core",
+        ]
+
+
+def post_install(context):
+    """Post install script"""
+    portal = api.portal.get()
+    remove_unused_contents(portal)
+
+
+def uninstall(context):
+    """Uninstall script"""
+    # Do something at the end of the uninstallation of this package.
