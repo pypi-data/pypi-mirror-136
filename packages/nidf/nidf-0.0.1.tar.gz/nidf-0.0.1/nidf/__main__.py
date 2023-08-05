4e4e631f8465707a118f79ea88b158dae3950f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:51 +0000

    Add package nidf/0.0.1/nidf-0.0.1.tar.gz

---
+++ packages/nidf/nidf-0.0.1.tar.gz/nidf-0.0.1/nidf/__main__.py
@@ -0,0 +1,6 @@
+
+if __name__ == '__main__':
+    import curio
+    from nidf import main
+
+    curio.run(main)
---
+++ packages/nidf/nidf-0.0.1.tar.gz/nidf-0.0.1/setup.py
@@ -0,0 +1,28 @@
+   
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+long_description = """
+nidf is a simple, striped down `find` replacement for use on NAS or slow disk drives. Results  may be faster than `find` on SSDs for deep but not shallow searches.'
+"""
+
+
+setup(name="nidf",
+      description="nidf",
+      long_description=long_description,
+      license="MIT",
+      version="0.0.1",
+      author="Samuel Woodward",
+      author_email="sam@woodward.fyi",
+      maintainer="Samuel Woodward   ",
+      maintainer_email="sam@woodward.fyi",
+      url="https://github.com/PyWood/nidf",
+      packages=['nidf'],
+      python_requires='>= 3.7',
+      classifiers=[
+          'Programming Language :: Python :: 3',
+      ])
+
