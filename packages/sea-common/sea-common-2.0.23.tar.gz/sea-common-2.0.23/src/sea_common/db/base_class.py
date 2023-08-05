bd05c9ce57ae8c22a726706e489e5a04b538bd97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:38 +0000

    Add package sea-common/2.0.23/sea-common-2.0.23.tar.gz

---
+++ packages/sea-common/sea-common-2.0.23.tar.gz/sea-common-2.0.23/src/sea_common/db/base_class.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+# from typing import Any
+# from sqlalchemy import inspect
+# from sqlalchemy.ext.declarative import as_declarative, declared_attr
+#
+# @as_declarative()
+# class Base:
+#     id: Any
+#     __name__: str
+#
+#     # Generate __tablename__ automatically
+#     @declared_attr
+#     def __tablename__(cls) -> str:
+#         return cls.__name__.lower()
+#
+#     def to_dict(self):
+#         return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
