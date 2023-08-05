0464efb5e1f458807d71ff5f06383754a605c4f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:35 +0000

    Add package obs2mk/1.7.1/obs2mk-1.7.1.tar.gz

---
+++ packages/obs2mk/obs2mk-1.7.1.tar.gz/obs2mk-1.7.1/mkdocs_obsidian/common/convert_one.py
@@ -0,0 +1,54 @@
+"""
+Convert one file function.
+"""
+
+import os
+import sys
+from datetime import datetime
+from pathlib import Path
+from rich.console import Console
+import frontmatter
+import yaml
+
+from mkdocs_obsidian.common import (
+    file_checking as check,
+    conversion as convert,
+    config as setup,
+    global_value as value,
+)
+
+
+def convert_one(ori, git: bool, meta: int):
+    """
+    Function to start the conversion of *one* specified file.
+    :param ori: path to file to convert
+    :param git: if False, push to git
+    :param meta: If 1 update the metadata's source file
+    :return: None
+    """
+    file_name = os.path.basename(ori).upper()
+    console = Console()
+    try:
+        yaml_front = frontmatter.load(ori)
+        priv = Path(value.POST)
+        clipkey = "notes"
+        if yaml_front.get("category"):
+            priv = check.create_folder(yaml_front["category"])
+            clipkey = yaml_front["category"]
+        contents = convert.file_convert(ori, 1)
+        checkfile = convert.file_write(ori, contents, priv, 1, meta)
+        if checkfile and not git:
+            commit = f"Pushed {file_name.lower()} to blog"
+            setup.git_push(commit)
+            convert.clipboard(ori, clipkey)
+        elif checkfile and git:
+            console.print(
+                f"[{datetime.now().strftime('%H:%M:%S')}] 🎉 Successfully converted [u"
+                f" blue bold]{file_name.lower()}[/]"
+            )
+    except yaml.YAMLError:
+        sys.exit(
+            f"Error in {file_name} : Your YAML frontmatter doesn't seem valid! Use"
+            " https://jsonformatter.org/yaml-validator to correct it!"
+        )
+    sys.exit()
