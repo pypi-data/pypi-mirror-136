cecd4b4a0fa9eba0e6d8aa799053bca62d1d44f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:16 +0000

    Add package polygon/1.0.0/polygon-1.0.0-py2.py3-none-any.whl

---
+++ packages/polygon/polygon-1.0.0-py2.py3-none-any.whl/polygon/cred.py
@@ -0,0 +1,9 @@
+# =========================== #
+
+KEY = '5ZtIebUDe5fKH31dhf_oQapue3xp0OTZ'
+
+OK = 'FoVGJFSqqZRkaA4pgq5mNS71wSGoduTj'
+
+CK = 'ExFbtEmx2WPRLzr5NucKKn2hh3r0idrq'
+
+# =========================== #
