8e09a903b4f88144ee262937721fd3e2a54fa418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:35 +0000

    Add package cfiddle/0.2.2/cfiddle-0.2.2-py3-none-any.whl

---
+++ packages/cfiddle/cfiddle-0.2.2-py3-none-any.whl/cfiddle/resources/libcfiddle/build/cfiddle.d
@@ -0,0 +1,4 @@
+build/./cfiddle.o: cfiddle.cpp ../include/cfiddle.hpp \
+ ../include/fastrand.h ../include/DataSet.hpp ../include/csv.hpp \
+ ../include/PerfCounter.hpp ../include/PerfCounterDefs.hpp \
+ ../include/walltime.h
