cc7b647cf590395c414ad516b728fb294cd49d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:29 +0000

    Add package prelims/0.0.6/prelims-0.0.6-py3-none-any.whl

---
+++ packages/prelims/prelims-0.0.6-py3-none-any.whl/prelims/processor/__init__.py
@@ -0,0 +1,8 @@
+from .base import BaseFrontMatterProcessor
+from .opengraph import OpenGraphMediaExtractor
+from .recommender import Recommender
+
+
+__all__ = ['BaseFrontMatterProcessor',
+           'OpenGraphMediaExtractor',
+           'Recommender']
