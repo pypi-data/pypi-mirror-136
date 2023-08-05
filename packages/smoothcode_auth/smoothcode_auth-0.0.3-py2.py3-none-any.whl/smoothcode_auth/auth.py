5a22298ec9837b6aad001834f77932df043e9f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:44 +0000

    Add package smoothcode_auth/0.0.3/smoothcode_auth-0.0.3-py2.py3-none-any.whl

---
+++ packages/smoothcode_auth/smoothcode_auth-0.0.3-py2.py3-none-any.whl/smoothcode_auth/auth.py
@@ -0,0 +1,16 @@
+import json
+
+from .utils import generate_hmac
+
+
+class SmoothCodeAuth:
+    def __init__(self, request_hmac: str, client_secret: str):
+        self.hmac = request_hmac
+        self.client_secret = client_secret
+
+    def is_dashboard_request(self, shop: str):
+        return generate_hmac(self.client_secret, shop) == self.hmac
+
+    def is_webhook_request(self, webhook_data: dict):
+        webhook_id = webhook_data.get('id')
+        return generate_hmac(self.client_secret, str(webhook_id)) == self.hmac
