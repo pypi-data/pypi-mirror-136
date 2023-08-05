4124d6dbd855699381158c104de98a839cd4c9c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:16 +0000

    Add package esrinlutils/0.6/esrinlutils-0.6.tar.gz

---
+++ packages/esrinlutils/esrinlutils-0.6.tar.gz/esrinlutils-0.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = 'esrinlutils',
+    packages = ['esrinlutils'],
+    version = '0.6',
+    description = 'A set of common functions to be used in scripts',
+    author = 'EsriNL DevTeam',
+    author_email = 'developers@esri.nl',
+    license='MIT',
+    url = 'https://github.com/esrinederland/PythonUtils',
+    download_url = 'https://github.com/esrinederland/PythonUtils/archive/refs/tags/0.6.tar.gz',
+    setup_requires=['wheel']
+)
