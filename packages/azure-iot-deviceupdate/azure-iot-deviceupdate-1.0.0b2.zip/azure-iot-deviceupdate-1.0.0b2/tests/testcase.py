e3e4dc13e2acb1ae0f2e97babba1085f1feecc14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:10 +0000

    Add package azure-iot-deviceupdate/1.0.0b2/azure-iot-deviceupdate-1.0.0b2.zip

---
+++ packages/azure-iot-deviceupdate/azure-iot-deviceupdate-1.0.0b2.zip/azure-iot-deviceupdate-1.0.0b2/tests/testcase.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+import functools
+from devtools_testutils import AzureTestCase, PowerShellPreparer
+from azure.iot.deviceupdate import DeviceUpdateClient
+
+
+class DeviceUpdateTest(AzureTestCase):
+    def __init__(self, method_name, **kwargs):
+        super(DeviceUpdateTest, self).__init__(method_name, **kwargs)
+
+    def create_client(self, endpoint, instance_id):
+        credential = self.get_credential(DeviceUpdateClient)
+        return self.create_client_from_credential(
+            DeviceUpdateClient,
+            endpoint=endpoint,
+            instance_id=instance_id,
+            credential=credential
+        )
+
+
+DeviceUpdatePowerShellPreparer = functools.partial(
+    PowerShellPreparer,
+    "deviceupdate",
+    deviceupdate_endpoint="https://fake_account.account.purview.azure.com",
+    deviceupdate_instance_id="instance"
+)
