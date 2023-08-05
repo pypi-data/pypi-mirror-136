58464e2ac4dc5e6b4ff1d971b839f4398ec55ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:56 +0000

    Add package views-schema/2.1.0/views_schema-2.1.0.tar.gz

---
+++ packages/views-schema/views_schema-2.1.0.tar.gz/views_schema-2.1.0/views_schema/__init__.py
@@ -0,0 +1,16 @@
+"""
+views_schema
+============
+
+The classes in this module define the (json) schema which services and tools
+use to communicate.
+
+Having these classes in one place makes it possible to share schema definitions
+across services, so that services may speak the same "language".
+"""
+
+from .queryset_manager import *
+from .partitioning import *
+from .docs import *
+from .base_data_retriever import *
+from .generic import *
