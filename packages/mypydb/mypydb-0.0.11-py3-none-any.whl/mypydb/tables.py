0c284209398f13f9c35abd0ab6b878328c683791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:59 +0000

    Add package mypydb/0.0.11/mypydb-0.0.11-py3-none-any.whl

---
+++ packages/mypydb/mypydb-0.0.11-py3-none-any.whl/mypydb/tables.py
@@ -0,0 +1,4 @@
+from mypydb.lib.tables import Table
+from mypydb.lib.tables.users import UsersTable
+from mypydb.lib.tables.brands import BrandsTable
+from mypydb.lib.tables.categories import CategoriesTable
