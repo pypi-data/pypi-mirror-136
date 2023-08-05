8e9b176be60a446f5173e582ba2d8cf6fec88f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:31 +0000

    Add package eyja-internal/0.4.1/eyja_internal-0.4.1-py3-none-any.whl

---
+++ packages/eyja-internal/eyja_internal-0.4.1-py3-none-any.whl/eyja/interfaces/db/base_storage_client.py
@@ -0,0 +1,32 @@
+from typing import Any
+
+
+class BaseStorageClient:
+    _config_cls = dict
+    _config: Any
+    _buckets: list
+
+    def __init__(self, config: dict):
+        self._config = self._config_cls(**config)
+        self._buckets = []
+
+    async def init(self):
+        pass
+
+    async def save(self, obj, object_space, object_type):
+        pass
+
+    async def delete(self, obj, object_space, object_type):
+        pass
+
+    async def delete_all(self, obj, object_space, object_type, filter):
+        pass
+
+    async def get(self, obj_cls, object_space, object_type, object_id):
+        pass
+
+    async def find(self, obj_cls, object_space, object_type, filter):
+        pass
+
+    async def count(self, obj_cls, object_space, object_type, filter):
+        pass
