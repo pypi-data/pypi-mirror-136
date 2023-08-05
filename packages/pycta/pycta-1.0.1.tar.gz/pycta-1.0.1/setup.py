d0908e7482b92b6c6be40ac5b0912a0b340f87f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:03 +0000

    Add package pycta/1.0.1/pycta-1.0.1.tar.gz

---
+++ packages/pycta/pycta-1.0.1.tar.gz/pycta-1.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pycta',
+    version='1.0.1',
+    author='Joe Rechenmacher',
+    author_email='joe.rechenmacher@gmail.com',
+    description='Unofficial Python Wrapper for CTA Transit Feed',
+    url='https://github.com/joerex1418/cta',
+    project_urls = {
+        "Issues": "https://github.com/joerex1418/cta/issues",
+        "Projects": "https://github.com/joerex1418/cta/projects"
+    },
+    license='GPU',
+    packages=setuptools.find_packages(where='pycta',include=["__init__"],exclude=["cta","constants","utils","utils_cta"]),
+    install_requires=[
+        'requests',
+        'pandas',
+        'beautifulsoup4',
+        'polars',
+        'haversine',
+        'lxml',
+        'html5lib',
+        'tabulate'],
+)
