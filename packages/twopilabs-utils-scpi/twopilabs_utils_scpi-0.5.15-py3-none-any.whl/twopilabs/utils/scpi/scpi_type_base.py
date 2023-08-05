ae9928da0d4e3f78b47b2c5af04d9820080846e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:00 +0000

    Add package twopilabs-utils-scpi/0.5.15/twopilabs_utils_scpi-0.5.15-py3-none-any.whl

---
+++ packages/twopilabs-utils-scpi/twopilabs_utils_scpi-0.5.15-py3-none-any.whl/twopilabs/utils/scpi/scpi_type_base.py
@@ -0,0 +1,28 @@
+import abc
+from typing import *
+from typing import IO
+
+
+class ScpiTypeBase(object):
+    """Base class for SCPI types"""
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def __str__(self) -> str:
+        """A textual representation of the object"""
+        raise NotImplementedError("Please Implement this method")
+
+    @abc.abstractmethod
+    def compose(self) -> str:
+        """Converts the given data into SCPI "Device listening" format"""
+        raise NotImplementedError("Please Implement this method")
+
+    @classmethod
+    @abc.abstractmethod
+    def parse(cls, stream: IO):
+        """Parses SCPI "Device talking Elements" into an internal representation"""
+        raise NotImplementedError("Please Implement this method")
+
+
+# TypeVar for type hinting subclasses of ScpiTypeBase
+ScpiTypes = TypeVar('ScpiTypes', bound=ScpiTypeBase)
