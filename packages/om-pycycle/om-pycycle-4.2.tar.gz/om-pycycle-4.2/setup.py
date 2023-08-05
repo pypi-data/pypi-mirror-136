71e318e4fe244440dabd0a2ab8ce3491f65c1c89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:38 +0000

    Add package om-pycycle/4.2/om-pycycle-4.2.tar.gz

---
+++ packages/om-pycycle/om-pycycle-4.2.tar.gz/om-pycycle-4.2/setup.py
@@ -0,0 +1,43 @@
+from distutils.core import setup
+
+# optional dependencies, by category (currently just 'test')
+optional_dependencies = {
+    'test': [
+        'testflo>=1.3.6',
+    ]
+}
+
+# Add an optional dependency that concatenates all others
+optional_dependencies['all'] = sorted([
+    dependency
+    for dependencies in optional_dependencies.values()
+    for dependency in dependencies
+])
+
+setup(name='om-pycycle',
+      version='4.2',
+      packages=[
+          'pycycle',
+          'pycycle.elements',
+          'pycycle.elements.test',
+          'pycycle.maps',
+          'pycycle.maps.test',
+          'pycycle.thermo',
+          'pycycle.thermo.cea',
+          'pycycle.thermo.cea.test',
+          'pycycle.thermo.cea.thermo_data',
+          'pycycle.thermo.tabular',
+          'pycycle.thermo.tabular.test',
+          'pycycle.thermo.test',
+          'pycycle.tests',
+      ],
+      install_requires=[
+        'openmdao>=3.10.0',
+      ],
+    package_data={
+        'pycycle.elements.test': ['reg_data/*.csv'],
+        'pycycle.thermo.test': ['*.csv'],
+        'pycycle.thermo.tabular': ['*.pkl'],
+    },
+    extras_require=optional_dependencies,
+)
