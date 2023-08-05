a21d2ddefef93dfda2b6eb653c78c6b17b4a7a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:46 +0000

    Add package tesliper/0.9.0/tesliper-0.9.0-py3-none-any.whl

---
+++ packages/tesliper/tesliper-0.9.0-py3-none-any.whl/tesliper/__init__.py
@@ -0,0 +1,8 @@
+from .datawork import *
+from .extraction import *
+from .glassware import *
+from .tesliper import Tesliper
+from .writing import *
+
+__author__ = "Michał M. Więcław"
+__version__ = "0.9.0"
