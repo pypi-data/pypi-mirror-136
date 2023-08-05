9f0e316075efa96b3d8407a3b02661f2bcae370e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:45 +0000

    Add package dicebear/0.2.5/dicebear-0.2.5.tar.gz

---
+++ packages/dicebear/dicebear-0.2.5.tar.gz/dicebear-0.2.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+__version__ = "0.2.5"
+DESCRIPTION = "A python API wrapper for DiceBear's avatar generating API."
+
+setup(
+    name="dicebear",
+    version=__version__,
+    license="MIT License",
+    author="jvherck",
+    author_email="<jvh.discord@gmail.com>",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['requests', 'urllib3'],
+    keywords=['python', 'dicebear', 'avatar', 'generating', 'API', 'wrapper', 'image', 'svg'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Other Audience",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Artistic Software",
+        "Topic :: Games/Entertainment",
+        "Topic :: Multimedia :: Graphics"
+    ]
+)
