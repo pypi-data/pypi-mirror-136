ecf375d1f10a1d5f03f5649b4f818ae4665ead0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:40 +0000

    Add package mypydb/0.0.10/mypydb-0.0.10.tar.gz

---
+++ packages/mypydb/mypydb-0.0.10.tar.gz/mypydb-0.0.10/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+	name="mypydb",
+	version="0.0.10",
+	author="Gregory Bockus",
+	author_email="gregory.bockus@gmail.com",
+	description="A small MySQL database helper package for making databases, tables, and columns.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/gurgy11/mypydb",
+	project_urls={
+		"Bug Tracker": "https://github.com/gurgy11/mypydb/issues",
+	},
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"Operating System :: OS Independent",
+	],
+	license="GNU General Public",
+	package_dir={"": "src"},
+	packages=setuptools.find_packages(where="src"),
+	python_requires=">=3.6",
+)
