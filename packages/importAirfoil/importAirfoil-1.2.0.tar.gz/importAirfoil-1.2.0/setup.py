0e0d382e22d7270744d13f5e768a4d952d47fe70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:14 +0000

    Add package importAirfoil/1.2.0/importAirfoil-1.2.0.tar.gz

---
+++ packages/importAirfoil/importAirfoil-1.2.0.tar.gz/importAirfoil-1.2.0/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+print(long_description)
+
+setup(
+    name='importAirfoil',
+    version='1.2.0',
+    packages=['src'],
+    url='https://github.com/VAZMFB/Python-importAirfoil',
+    download_url = 'https://github.com/VAZMFB/Python-importAirfoil/archive/refs/tags/1.2.0.tar.gz',
+    keywords = ['airfoil', 'import-airfoil'],
+    license='GPL-3.0-or-later',
+    author='Miloš Petrašinović',
+    author_email='mpetrasinovic@mas.bg.ac.rs',
+    description='Airfoil coordinates import function',
+    classifiers = [],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
