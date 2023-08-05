bfb4b54dfdd060161ce530470226d57241f5273d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:46 +0000

    Add package zensols.deeplearn/0.1.8/zensols.deeplearn-0.1.8-py3-none-any.whl

---
+++ packages/zensols.deeplearn/zensols.deeplearn-0.1.8-py3-none-any.whl/zensols/deeplearn/__init__.py
@@ -0,0 +1,9 @@
+"""A deep learning framework to make training, validating and testing models
+with PyTorch easier.
+
+"""
+
+from .torchtype import *
+from .torchconfig import *
+from .domain import *
+from .plot import *
