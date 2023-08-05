6a82dbb93aff0ca649a28779108904b6a27109fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:15 +0000

    Add package BUCTOJ/0.0.3/BUCTOJ-0.0.3.tar.gz

---
+++ packages/BUCTOJ/BUCTOJ-0.0.3.tar.gz/BUCTOJ-0.0.3/BUCTOJ/FromPidToChar.py
@@ -0,0 +1,35 @@
+'''
+Author: LetMeFly
+Date: 2022-01-24 15:18:26
+LastEditors: LetMeFly
+LastEditTime: 2022-01-24 15:43:35
+'''
+"""
+从题目编号(数字)到题目编号(字母)
+"""
+def i2s(num: int) -> str:
+    """
+    0: A
+    1: B
+    2: C
+    ...
+    25: Z
+    26: AA
+    27: AB
+    ...
+    51: AZ
+    52: BA
+    ...
+    701: ZZ
+    702: ''
+    703: ''
+    
+    只有前701道题有编号，也就是说字母编号只有两位
+    低位从A~Z，相当于26进制
+    高位从''(空)到A~Z，相当于27进制
+    表达范围：[0, 27 * 26) = [0, 701]
+    """
+    if num < 26:
+        return chr(ord('A') + num)
+    else:
+        return chr(ord('A') + num // 26 - 1) + chr(ord('A') + num % 26)
---
+++ packages/BUCTOJ/BUCTOJ-0.0.3.tar.gz/BUCTOJ-0.0.3/BUCTOJ/__init__.py
@@ -0,0 +1,7 @@
+'''
+Author: LetMeFly
+Date: 2022-01-25 18:07:26
+LastEditors: LetMeFly
+LastEditTime: 2022-01-25 18:08:30
+'''
+__all__ = ['AutoGetAndSubmit', 'FromPidToChar', 'GetASourceCodeAndChange', 'GetContestProblemList', 'Login', 'SubmitOneCode']
