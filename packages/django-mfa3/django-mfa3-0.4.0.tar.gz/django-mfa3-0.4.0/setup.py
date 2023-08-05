b85159e4a145772cb95b145a9c4fc1b71e863b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:18 +0000

    Add package django-mfa3/0.4.0/django-mfa3-0.4.0.tar.gz

---
+++ packages/django-mfa3/django-mfa3-0.4.0.tar.gz/django-mfa3-0.4.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='django-mfa3',
+    description='multi factor authentication for django',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/xi/django-mfa3',
+    author='Tobias Bengfort',
+    author_email='tobias.bengfort@posteo.de',
+    version='0.4.0',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    install_requires=['pyotp', 'fido2', 'qrcode', 'django>=2.2'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
