21c2fdd645ef8c5e2e4d0321d81dd176b2d2c9d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:18:52 +0000

    Add package psmpy/0.1.6/psmpy-0.1.6.tar.gz

---
+++ packages/psmpy/psmpy-0.1.6.tar.gz/psmpy-0.1.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+dependencies = ['matplotlib','numpy','pandas','seaborn','sklearn','scipy','sklearn']
+
+# read the contents of README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='psmpy',
+    packages=find_packages(include=['matplotlib','seaborn','sklearn']),
+    version='0.1.6',
+    description='Propensity score matching for python and graphical plots',
+    author='Adrienne Kline',
+    author_email='askline1@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    install_requires = dependencies,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
