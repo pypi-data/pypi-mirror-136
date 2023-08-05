08a04de6ab9c3f6213be6d47346834f055022d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:18:41 +0000

    Add package cdktf-cdktf-provider-aws/4.0.18/cdktf-cdktf-provider-aws-4.0.18.tar.gz

---
+++ packages/cdktf-cdktf-provider-aws/cdktf-cdktf-provider-aws-4.0.18.tar.gz/cdktf-cdktf-provider-aws-4.0.18/src/cdktf_cdktf_provider_aws/datapipeline/__init__.py
@@ -0,0 +1,297 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from .._jsii import *
+
+import cdktf
+import constructs
+
+
+class DatapipelinePipeline(
+    cdktf.TerraformResource,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="@cdktf/provider-aws.datapipeline.DatapipelinePipeline",
+):
+    '''Represents a {@link https://www.terraform.io/docs/providers/aws/r/datapipeline_pipeline aws_datapipeline_pipeline}.'''
+
+    def __init__(
+        self,
+        scope: constructs.Construct,
+        id: builtins.str,
+        *,
+        name: builtins.str,
+        description: typing.Optional[builtins.str] = None,
+        tags: typing.Optional[typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]]] = None,
+        tags_all: typing.Optional[typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]]] = None,
+        count: typing.Optional[typing.Union[jsii.Number, cdktf.IResolvable]] = None,
+        depends_on: typing.Optional[typing.Sequence[cdktf.ITerraformDependable]] = None,
+        lifecycle: typing.Optional[cdktf.TerraformResourceLifecycle] = None,
+        provider: typing.Optional[cdktf.TerraformProvider] = None,
+    ) -> None:
+        '''Create a new {@link https://www.terraform.io/docs/providers/aws/r/datapipeline_pipeline aws_datapipeline_pipeline} Resource.
+
+        :param scope: The scope in which to define this construct.
+        :param id: The scoped construct ID. Must be unique amongst siblings in the same scope
+        :param name: Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/aws/r/datapipeline_pipeline#name DatapipelinePipeline#name}.
+        :param description: Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/aws/r/datapipeline_pipeline#description DatapipelinePipeline#description}.
+        :param tags: Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/aws/r/datapipeline_pipeline#tags DatapipelinePipeline#tags}.
+        :param tags_all: Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/aws/r/datapipeline_pipeline#tags_all DatapipelinePipeline#tags_all}.
+        :param count: 
+        :param depends_on: 
+        :param lifecycle: 
+        :param provider: 
+        '''
+        config = DatapipelinePipelineConfig(
+            name=name,
+            description=description,
+            tags=tags,
+            tags_all=tags_all,
+            count=count,
+            depends_on=depends_on,
+            lifecycle=lifecycle,
+            provider=provider,
+        )
+
+        jsii.create(self.__class__, self, [scope, id, config])
+
+    @jsii.member(jsii_name="resetDescription")
+    def reset_description(self) -> None:
+        return typing.cast(None, jsii.invoke(self, "resetDescription", []))
+
+    @jsii.member(jsii_name="resetTags")
+    def reset_tags(self) -> None:
+        return typing.cast(None, jsii.invoke(self, "resetTags", []))
+
+    @jsii.member(jsii_name="resetTagsAll")
+    def reset_tags_all(self) -> None:
+        return typing.cast(None, jsii.invoke(self, "resetTagsAll", []))
+
+    @jsii.member(jsii_name="synthesizeAttributes")
+    def _synthesize_attributes(self) -> typing.Mapping[builtins.str, typing.Any]:
+        return typing.cast(typing.Mapping[builtins.str, typing.Any], jsii.invoke(self, "synthesizeAttributes", []))
+
+    @jsii.python.classproperty # type: ignore[misc]
+    @jsii.member(jsii_name="tfResourceType")
+    def TF_RESOURCE_TYPE(cls) -> builtins.str:
+        return typing.cast(builtins.str, jsii.sget(cls, "tfResourceType"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="id")
+    def id(self) -> builtins.str:
+        return typing.cast(builtins.str, jsii.get(self, "id"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="descriptionInput")
+    def description_input(self) -> typing.Optional[builtins.str]:
+        return typing.cast(typing.Optional[builtins.str], jsii.get(self, "descriptionInput"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="nameInput")
+    def name_input(self) -> typing.Optional[builtins.str]:
+        return typing.cast(typing.Optional[builtins.str], jsii.get(self, "nameInput"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="tagsAllInput")
+    def tags_all_input(
+        self,
+    ) -> typing.Optional[typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]]]:
+        return typing.cast(typing.Optional[typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]]], jsii.get(self, "tagsAllInput"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="tagsInput")
+    def tags_input(
+        self,
+    ) -> typing.Optional[typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]]]:
+        return typing.cast(typing.Optional[typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]]], jsii.get(self, "tagsInput"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="description")
+    def description(self) -> builtins.str:
+        return typing.cast(builtins.str, jsii.get(self, "description"))
+
+    @description.setter
+    def description(self, value: builtins.str) -> None:
+        jsii.set(self, "description", value)
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="name")
+    def name(self) -> builtins.str:
+        return typing.cast(builtins.str, jsii.get(self, "name"))
+
+    @name.setter
+    def name(self, value: builtins.str) -> None:
+        jsii.set(self, "name", value)
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="tags")
+    def tags(
+        self,
+    ) -> typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]]:
+        return typing.cast(typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]], jsii.get(self, "tags"))
+
+    @tags.setter
+    def tags(
+        self,
+        value: typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]],
+    ) -> None:
+        jsii.set(self, "tags", value)
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="tagsAll")
+    def tags_all(
+        self,
+    ) -> typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]]:
+        return typing.cast(typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]], jsii.get(self, "tagsAll"))
+
+    @tags_all.setter
+    def tags_all(
+        self,
+        value: typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]],
+    ) -> None:
+        jsii.set(self, "tagsAll", value)
+
+
+@jsii.data_type(
+    jsii_type="@cdktf/provider-aws.datapipeline.DatapipelinePipelineConfig",
+    jsii_struct_bases=[cdktf.TerraformMetaArguments],
+    name_mapping={
+        "count": "count",
+        "depends_on": "dependsOn",
+        "lifecycle": "lifecycle",
+        "provider": "provider",
+        "name": "name",
+        "description": "description",
+        "tags": "tags",
+        "tags_all": "tagsAll",
+    },
+)
+class DatapipelinePipelineConfig(cdktf.TerraformMetaArguments):
+    def __init__(
+        self,
+        *,
+        count: typing.Optional[typing.Union[jsii.Number, cdktf.IResolvable]] = None,
+        depends_on: typing.Optional[typing.Sequence[cdktf.ITerraformDependable]] = None,
+        lifecycle: typing.Optional[cdktf.TerraformResourceLifecycle] = None,
+        provider: typing.Optional[cdktf.TerraformProvider] = None,
+        name: builtins.str,
+        description: typing.Optional[builtins.str] = None,
+        tags: typing.Optional[typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]]] = None,
+        tags_all: typing.Optional[typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]]] = None,
+    ) -> None:
+        '''AWS Data Pipeline.
+
+        :param count: 
+        :param depends_on: 
+        :param lifecycle: 
+        :param provider: 
+        :param name: Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/aws/r/datapipeline_pipeline#name DatapipelinePipeline#name}.
+        :param description: Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/aws/r/datapipeline_pipeline#description DatapipelinePipeline#description}.
+        :param tags: Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/aws/r/datapipeline_pipeline#tags DatapipelinePipeline#tags}.
+        :param tags_all: Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/aws/r/datapipeline_pipeline#tags_all DatapipelinePipeline#tags_all}.
+        '''
+        if isinstance(lifecycle, dict):
+            lifecycle = cdktf.TerraformResourceLifecycle(**lifecycle)
+        self._values: typing.Dict[str, typing.Any] = {
+            "name": name,
+        }
+        if count is not None:
+            self._values["count"] = count
+        if depends_on is not None:
+            self._values["depends_on"] = depends_on
+        if lifecycle is not None:
+            self._values["lifecycle"] = lifecycle
+        if provider is not None:
+            self._values["provider"] = provider
+        if description is not None:
+            self._values["description"] = description
+        if tags is not None:
+            self._values["tags"] = tags
+        if tags_all is not None:
+            self._values["tags_all"] = tags_all
+
+    @builtins.property
+    def count(self) -> typing.Optional[typing.Union[jsii.Number, cdktf.IResolvable]]:
+        '''
+        :stability: experimental
+        '''
+        result = self._values.get("count")
+        return typing.cast(typing.Optional[typing.Union[jsii.Number, cdktf.IResolvable]], result)
+
+    @builtins.property
+    def depends_on(self) -> typing.Optional[typing.List[cdktf.ITerraformDependable]]:
+        '''
+        :stability: experimental
+        '''
+        result = self._values.get("depends_on")
+        return typing.cast(typing.Optional[typing.List[cdktf.ITerraformDependable]], result)
+
+    @builtins.property
+    def lifecycle(self) -> typing.Optional[cdktf.TerraformResourceLifecycle]:
+        '''
+        :stability: experimental
+        '''
+        result = self._values.get("lifecycle")
+        return typing.cast(typing.Optional[cdktf.TerraformResourceLifecycle], result)
+
+    @builtins.property
+    def provider(self) -> typing.Optional[cdktf.TerraformProvider]:
+        '''
+        :stability: experimental
+        '''
+        result = self._values.get("provider")
+        return typing.cast(typing.Optional[cdktf.TerraformProvider], result)
+
+    @builtins.property
+    def name(self) -> builtins.str:
+        '''Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/aws/r/datapipeline_pipeline#name DatapipelinePipeline#name}.'''
+        result = self._values.get("name")
+        assert result is not None, "Required property 'name' is missing"
+        return typing.cast(builtins.str, result)
+
+    @builtins.property
+    def description(self) -> typing.Optional[builtins.str]:
+        '''Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/aws/r/datapipeline_pipeline#description DatapipelinePipeline#description}.'''
+        result = self._values.get("description")
+        return typing.cast(typing.Optional[builtins.str], result)
+
+    @builtins.property
+    def tags(
+        self,
+    ) -> typing.Optional[typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]]]:
+        '''Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/aws/r/datapipeline_pipeline#tags DatapipelinePipeline#tags}.'''
+        result = self._values.get("tags")
+        return typing.cast(typing.Optional[typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]]], result)
+
+    @builtins.property
+    def tags_all(
+        self,
+    ) -> typing.Optional[typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]]]:
+        '''Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/aws/r/datapipeline_pipeline#tags_all DatapipelinePipeline#tags_all}.'''
+        result = self._values.get("tags_all")
+        return typing.cast(typing.Optional[typing.Union[cdktf.IResolvable, typing.Mapping[builtins.str, builtins.str]]], result)
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "DatapipelinePipelineConfig(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+__all__ = [
+    "DatapipelinePipeline",
+    "DatapipelinePipelineConfig",
+]
+
+publication.publish()
