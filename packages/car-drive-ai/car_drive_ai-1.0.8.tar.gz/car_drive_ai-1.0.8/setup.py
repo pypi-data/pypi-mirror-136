70c45dee95945f2480c2784821af78af5a8bc98f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:46 +0000

    Add package car-drive-ai/1.0.8/car_drive_ai-1.0.8.tar.gz

---
+++ packages/car-drive-ai/car_drive_ai-1.0.8.tar.gz/car_drive_ai-1.0.8/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="car_drive_ai",
+	version="1.0.8",
+	description="with this package you can easily setup a car drive ai with pygame on you own raceing track",
+	author="Timo Pickelmann",
+	author_email="timo.pickelmann.opensource@gmail.com",
+	url="https://github.com/timo42453189/car_drive_ai",
+	install_requires=["pygame", "neat-python", "numpy", "matplotlib", "wget"],
+	packages=find_packages(),
+)
