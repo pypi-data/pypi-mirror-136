02198cc8d11014c3d7d8eb42e4be98ada56a2f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:17 +0000

    Add package biotas/0.0.8/biotas-0.0.8.tar.gz

---
+++ packages/biotas/biotas-0.0.8.tar.gz/biotas-0.0.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name = 'biotas',
+      version = '0.0.8',
+      description = '''BIO-Telemetry Analysis Software (BIOTAS) for use in
+      removing false positive and overlap detections from radio telemetry projects.''',
+      url = 'https://github.com/knebiolo/biotas',
+      author = 'Kevin P. Nebiolo and Theodore Castro-Santos',
+      author_email = 'kevin.nebiolo@kleinschmidtgroup.com',
+      license = 'MIT',
+      packages = ['biotas',],
+      python_requires= '>=3.5',
+      install_requires=["numpy >= 1.17.4",
+                        "pandas >= 0.25.3",
+                        "matplotlib >= 3.1.1",
+                        "statsmodels >= 0.10.1",
+                        "networkx >= 2.2",
+                        "scipy >= 1.7.1",
+                        "sklearn >= 1.0"],
+      zip_safe = False
+      )
