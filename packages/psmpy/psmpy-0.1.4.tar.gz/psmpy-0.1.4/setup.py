372a9ff90c41911db99f3dc8838e83e4a8ad9787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:08 +0000

    Add package psmpy/0.1.4/psmpy-0.1.4.tar.gz

---
+++ packages/psmpy/psmpy-0.1.4.tar.gz/psmpy-0.1.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+dependencies = ['matplotlib','numpy','pandas','seaborn','sklearn','scipy','sklearn']
+
+setup(
+    name='psmpy',
+    packages=find_packages(include=['matplotlib','seaborn','sklearn']),
+    version='0.1.4',
+    description='Propensity score matching for python and graphical plots',
+    author='Adrienne Kline',
+    author_email='askline1@gmail.com',
+    license='MIT',
+    install_requires = dependencies,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
