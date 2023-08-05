c8ccfadd56462da60bb0e9e6dc0b3f98a851a50c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:15 +0000

    Add package pytmpfile/0.1/pytmpfile-0.1.tar.gz

---
+++ packages/pytmpfile/pytmpfile-0.1.tar.gz/pytmpfile-0.1/pytmpfile/__init__.py
@@ -0,0 +1,22 @@
+from memory_tempfile import MemoryTempfile
+import contextlib
+from typing import Optional
+
+__all__ = ['get_tmp_path']
+__version__ = '0.1'
+
+# Credits: https://pypi.org/project/memory-tempfile/
+tempfile = MemoryTempfile(preferred_paths=['/run/user/{uid}'], remove_paths=['/dev/shm', '/run/shm'],
+                          additional_paths=['/var/run'], filesystem_types=['tmpfs'], fallback=True)
+
+if not tempfile.found_mem_tempdir():
+    raise RuntimeError('No tmp directory found')
+
+
+@contextlib.contextmanager
+def get_tmp_path(content: Optional[str] = None, suffix: Optional[str] = None, delete=True) -> str:
+    with tempfile.NamedTemporaryFile(suffix=suffix, delete=delete, mode='w+') as ntf:
+        if content:
+            ntf.write(content)
+            ntf.flush()
+        yield ntf.name
