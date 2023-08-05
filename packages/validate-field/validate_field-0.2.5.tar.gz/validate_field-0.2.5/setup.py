27434a987709fefe9dbc14634efeab4ca05316f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:39 +0000

    Add package validate-field/0.2.5/validate_field-0.2.5.tar.gz

---
+++ packages/validate-field/validate_field-0.2.5.tar.gz/validate_field-0.2.5/setup.py
@@ -0,0 +1,12 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.rst").read_text()
+setup(
+	long_description=README,
+    long_description_content_type="text/markdown",
+    )
