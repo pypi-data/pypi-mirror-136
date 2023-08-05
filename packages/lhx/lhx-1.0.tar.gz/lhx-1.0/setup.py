a8833c836f9f8b85f2c90c2428aed787388a8e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:15 +0000

    Add package lhx/1.0/lhx-1.0.tar.gz

---
+++ packages/lhx/lhx-1.0.tar.gz/lhx-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name='lhx',
+    version='1.0',
+    description='用作数据分析的便捷指令',
+    author='lyr',
+    author_email='lhx17614480838@163.com',
+    py_modules=['lhx.SQL_DateFream','lhx.SJ']
+)
