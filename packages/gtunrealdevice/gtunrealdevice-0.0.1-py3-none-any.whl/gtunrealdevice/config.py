2c566c7fb10353004ae1b12b442572b7b31ee49b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:27 +0000

    Add package gtunrealdevice/0.0.1/gtunrealdevice-0.0.1-py3-none-any.whl

---
+++ packages/gtunrealdevice/gtunrealdevice-0.0.1-py3-none-any.whl/gtunrealdevice/config.py
@@ -0,0 +1,11 @@
+"""Module containing the attributes for gtunrealdevice."""
+
+__version__ = '0.0.1'
+version = __version__
+__edition__ = 'Community'
+edition = __edition__
+
+__all__ = [
+    'version',
+    'edition'
+]
