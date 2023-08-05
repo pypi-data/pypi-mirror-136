c2804503bcc3daaba4e3175610353d2a4ecc2e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:42 +0000

    Add package databasemodels/1.7.0/databasemodels-1.7.0-py3-none-any.whl

---
+++ packages/databasemodels/databasemodels-1.7.0-py3-none-any.whl/databasemodels/__init__.py
@@ -0,0 +1,12 @@
+from .wrapper import *
+from . import datatypes
+from .connection import *
+from .columns import *
+from .protocols import *
+from .exceptions import *
+from .version import *
+
+# TODO: Implement Interval
+# TODO: Additional SQL parameter to createTable
+
+version = Version(1, 7, 0)
