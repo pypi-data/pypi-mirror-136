9553bcbc3faf34b21ba5a780433e049c40dc7043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:35 +0000

    Add package views-schema/2.1.1/views_schema-2.1.1.tar.gz

---
+++ packages/views-schema/views_schema-2.1.1.tar.gz/views_schema-2.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['views_schema']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.8.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'views-schema',
+    'version': '2.1.1',
+    'description': 'A package containing pydantic models used throughout ViEWS 3 for communication between services',
+    'long_description': None,
+    'author': 'peder2911',
+    'author_email': 'pglandsverk@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
