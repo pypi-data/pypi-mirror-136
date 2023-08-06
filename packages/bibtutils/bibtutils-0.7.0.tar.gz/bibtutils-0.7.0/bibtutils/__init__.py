d4ac6b7ca1aedfec30ff4b10d1535c1f6b86d42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:50 +0000

    Add package bibtutils/0.7.0/bibtutils-0.7.0.tar.gz

---
+++ packages/bibtutils/bibtutils-0.7.0.tar.gz/bibtutils-0.7.0/bibtutils/__init__.py
@@ -0,0 +1,10 @@
+from bibtutils.version import __version__
+__all__ = [
+    'gcp',
+    'slack'
+]
+
+from bibtutils import gcp
+from bibtutils import slack
+
+'''A Python package featuring functionality often used by BITS Blue Team (BIBT).'''
