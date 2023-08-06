c40a68c9e058ad96182f40aadfe7cf38f9f19f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:16 +0000

    Add package historydag/0.1/historydag-0.1.tar.gz

---
+++ packages/historydag/historydag-0.1.tar.gz/historydag-0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="historydag",
+    version=0.1,
+    author="Will Dumm",
+    author_email="wdumm88@gmail.com",
+    description="Basic history DAG implementation",
+    packages=['historydag'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "six",
+        "PyQt5",
+        "ete3",
+        "biopython",
+        "graphviz"
+    ],
+)
