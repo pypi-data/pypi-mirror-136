3d77bc57800eab6847793d9a43409bf7ac455bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:37 +0000

    Add package pyqtCuWi/1.4.1/pyqtCuWi-1.4.1-py3-none-any.whl

---
+++ packages/pyqtCuWi/pyqtCuWi-1.4.1-py3-none-any.whl/pyqtCuWi/pyqtCuWiModules/__init__.py
@@ -0,0 +1,7 @@
+#          Custom Widgets For PyQt5 Module           #
+#          GPL 3.0 - myygunduz - pyqtCuWi            #
+#        https://github.com/myygunduz/pyqtCuWi       #
+
+from .jsonHelper import readJ,writeJ
+from .progressBar import ProgressBar
+from .clearList import clearOfList
