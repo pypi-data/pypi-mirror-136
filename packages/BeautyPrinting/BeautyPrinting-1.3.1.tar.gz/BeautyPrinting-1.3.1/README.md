f514654d39921ec26176165dff681ebafd0b0e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:39 +0000

    Add package BeautyPrinting/1.3.1/BeautyPrinting-1.3.1.tar.gz

---
+++ packages/BeautyPrinting/BeautyPrinting-1.3.1.tar.gz/BeautyPrinting-1.3.1/README.md
@@ -0,0 +1,28 @@
+# 欢迎下载BeautyPrinting！
+## 你是否想要拥有幻彩的打印输出？
+```python
+import BeautyPrinting
+
+BeautyPrinting.BtPrint.btprint('Colorful Hello World!', color='cyan')
+```
+###### 两行代码，就可以拥有炫彩的“Hello World”
+## 你是否想要拥有炫酷的加载进度条？
+```python
+import BeautyPrinting
+
+BeautyPritning.ProgressBar.loading_bar('Loading......', items=50, color='cyan')
+```
+###### 两行代码，轻松拥有多彩的加载条
+## 新功能：enlighten progress bar
+```python
+import BeautyPrinting
+
+BeautyPrinting.ProgressBar.enlighten_loading_bar(text='Loading', items=200, sleep=0.01, color='cyan')
+```
+```python
+import BeautyPrinting
+
+BeautyPrinting.ProgressBar.enlighten_loading_bars(5, use_list=False, text='Loading......', items=200, sleep=0.01, color='cyan')
+```
+###### 抽象Enlighten接口，摆脱复杂的Enlighten API
+## 赶紧下载我们BeautyPrinting吧！一定能满足您的美学需求！
---
+++ packages/BeautyPrinting/BeautyPrinting-1.3.1.tar.gz/BeautyPrinting-1.3.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as file:
+    fh = file.read()
+
+
+setup(
+    name='BeautyPrinting',
+    version='1.3.1',
+    author='YunDuanModule',
+    description='BeautyPrinting · 一个专为美观打印而设计的模块',
+    long_description=fh,
+    long_description_content_type='text/markdown',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.0',
+    install_requires=['colorama', 'enlighten'],
+    py_modules=['BeautyPrinting', 'BeautyPrinting.BtPrint', 'BeautyPrinting.ProgressBar'],
+    keywords=['BeautyPrinting', 'Beauty', 'Printing', 'Print', 'beautiful', 'colorful', 'loading', 'bar']
+)
