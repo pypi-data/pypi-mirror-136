b6ab3dcd89d424f30d4f41dc937cdd48b5824f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:40 +0000

    Add package throws/1.0.6/throws-1.0.6.tar.gz

---
+++ packages/throws/throws-1.0.6.tar.gz/throws-1.0.6/README.md
@@ -0,0 +1,58 @@
+# Python: throws
+
+Small but simple library providing a function decorator similar to Kotlins *@throws(...)*
+decorator with the small difference that multiple exceptions or errors can be combined in just one
+single annotation. Should maily used for documentation and debugging purposes.
+
+## Installation
+
+Installation is possible using the Python *pip* command line tool. For you the command to install
+this library may look like this:
+
+```bash
+pip3 install throws
+```
+
+## Usage
+
+To use the *@throw* decorator simply place it before the function declaration providing every
+possible exception raised by the function. For a quick example see below:
+
+```python
+from throws import throws
+
+@throws(IOError, ValueError)
+def check_version(version_file: str) -> bool:
+    with open(version_file, "r", encoding = "utf-8") as vf:
+        if (float(vf.read() > 1.0):
+            return true
+        return false
+```
+
+The library provides two Exceptions by itself, the *EmptyListException* and the
+*InvalidRaisedException*. The first one is raised when there are no parameters provided to
+*@throw* and the function decorated is run. The second one occours when the function raises an
+exception which is not provided to the decorator, providing the developer with feedback that he
+might have forgotten about handling this specific exception!
+
+## Uploading a new version
+
+To create and upload a new version to PyPI use the following commands:
+
+```bash
+# 1) Check for correctness
+python3 setup.py check
+
+# 2) Create distributable files
+python3 setup.py sdist bdist_wheel
+
+# 3) Check distributable files
+python3 -m twine check dist/*
+
+# 4) Upload distributable files
+python3 -m twine upload dist/*
+```
+
+## Links
+
+Library at the Python Package Index (PyPI): https://pypi.org/project/throws/
---
+++ packages/throws/throws-1.0.6.tar.gz/throws-1.0.6/throws/throws.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from functools import wraps
+from typing import Any
+
+
+class EmptyListException(Exception):
+    """
+    Exception which is thrown when no exception or error is provided to the decorator @throws(...)
+    and therefor the list is empty!
+    """
+    pass
+
+
+class InvalidRaisedException(Exception):
+    """
+    Exception which is thrown when decorator @throws(...) does not contain all throwable
+    exceptions for decorated function!
+    """
+    pass
+
+
+def throws(*errors: Any):
+    """
+    Decorator for functions equal to the "@throws(...)" decorator provided by Kotlin to specify
+    possible exceptions or errors thrown!
+
+    :param errors: list of possible exceptions
+    :return: function with decorator wrapped around
+    :exception EmptyListException: when no exception or error is provided in decorator
+    :exception InvalidRaisedException: when an exception or error is thrown which is not provided
+    """
+    
+    def decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            if len(errors) <= 0:
+                raise EmptyListException(
+                    "List of possible exceptions provided to decorator can not be empty!"
+                )
+
+            try:
+                return function(*args, **kwargs)
+            except Exception as err:
+                for exception in errors:
+                    if isinstance(err, exception):
+                        raise err
+
+                if not isinstance(err, InvalidRaisedException) or not isinstance(err, EmptyListException):
+                    raise InvalidRaisedException(
+                        f"Exception {err.__class__.__name__} missing in list for decorator at "
+                        + f"function '{function.__name__}'!"
+                    ) from None
+        
+        return wrapper
+    return decorator
