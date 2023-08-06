2f0ce13c9c64e82bf99cf215597df7751337fa75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:49 +0000

    Add package fava-review/0.8.0/fava-review-0.8.0.tar.gz

---
+++ packages/fava-review/fava-review-0.8.0.tar.gz/fava-review-0.8.0/fava_review/__init__.py
@@ -0,0 +1,24 @@
+from typing import Optional, Any
+
+from fava.core import FavaLedger
+from fava.ext import FavaExtensionBase
+
+from fava_review import pivot_review
+from fava_review.pivot_review import PivotReview
+
+
+class FavaIncomeExpenseReview(FavaExtensionBase):
+    report_title = "Income/Expense Review"
+
+    def __init__(self, ledger: FavaLedger, config: Optional[str] = None) -> None:
+        super().__init__(ledger, config)
+        self.review = PivotReview(self.ledger)
+
+    def get_income_expenses_review_by_month(self) -> list[dict[str, Any]]:
+        try:
+            return self.review.income_and_expense_by_month()
+        except Exception:
+            raise
+
+    def current_operating_currency(self) -> str:
+        return self.review.current_operating_currency()
