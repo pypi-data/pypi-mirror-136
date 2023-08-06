7e75313b3c75523edda01404e14e375f6e152608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:24 +0000

    Add package wg-utilities/2.6.2/wg_utilities-2.6.2-py3-none-any.whl

---
+++ packages/wg-utilities/wg_utilities-2.6.2-py3-none-any.whl/wg_utilities/clients/__init__.py
@@ -0,0 +1,5 @@
+"""Useful clients for commonly accessed APIs/services"""
+
+from .google_client import GoogleClient, MediaType, MediaItem, Album
+from .spotify_client import SpotifyClient
+from .truelayer_client import TrueLayerClient, TrueLayerBank, Account, Card, Transaction
