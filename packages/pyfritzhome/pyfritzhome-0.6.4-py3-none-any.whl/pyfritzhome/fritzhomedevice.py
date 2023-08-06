e7b1f20803007360f13b4f20ff099ba0ab07f449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:59 +0000

    Add package pyfritzhome/0.6.4/pyfritzhome-0.6.4-py3-none-any.whl

---
+++ packages/pyfritzhome/pyfritzhome-0.6.4-py3-none-any.whl/pyfritzhome/fritzhomedevice.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from .devicetypes import (
+    FritzhomeDeviceAlarm,
+    FritzhomeDeviceButton,
+    FritzhomeDevicePowermeter,
+    FritzhomeDeviceRepeater,
+    FritzhomeDeviceSwitch,
+    FritzhomeDeviceTemperature,
+    FritzhomeDeviceThermostat,
+    FritzhomeDeviceLightBulb,
+    FritzhomeTemplate,
+)
+
+
+class FritzhomeDevice(
+    FritzhomeDeviceAlarm,
+    FritzhomeDeviceButton,
+    FritzhomeDevicePowermeter,
+    FritzhomeDeviceRepeater,
+    FritzhomeDeviceSwitch,
+    FritzhomeDeviceTemperature,
+    FritzhomeDeviceThermostat,
+    FritzhomeDeviceLightBulb,
+):
+    """The Fritzhome Device class."""
+
+    def __init__(self, fritz=None, node=None):
+        super().__init__(fritz, node)
+
+    def _update_from_node(self, node):
+        super()._update_from_node(node)
