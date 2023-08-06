8a82dd381ceed02ba66f02d12423c9d715ca8226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:50 +0000

    Add package footil/0.35.0/footil-0.35.0.tar.gz

---
+++ packages/footil/footil-0.35.0.tar.gz/footil-0.35.0/README.rst
@@ -0,0 +1,28 @@
+Various Helper Modules
+######################
+
+- **date**, **datetime**, **time** - date/datetime/time manipulation helpers.
+- **decor** - helper decorators.
+- **file** - file manipulation helpers.
+- **formatting** - string formatting.
+- **log** - helpers with stdout, stderr management, logging setup.
+- **parsing** - parse arbitrary data using various helpers.
+- **path** - functions for paths manipulation.
+- **patterns** - design pattern helper classes for better re-usability.
+- **sorting** - sorting helpers.
+- **version** - manage version validation and bumping.
+- **xcollections** - functions for collections.
+- **xdata** - data manipulation helpers.
+- **xiter** - iteration helpers.
+- **xtyping** - type checking.
+
+Source code in:
+
+* `github <https://github.com/focusate/footil/>`_.
+* `pypi <https://pypi.org/project/footil/>`_.
+
+Contributors
+============
+
+* Silvija Butko (Focusate)
+* Andrius Laukavičius (Focusate)
---
+++ packages/footil/footil-0.35.0.tar.gz/footil-0.35.0/setup.py
@@ -0,0 +1,45 @@
+from distutils.core import setup
+
+from footil import __version__
+
+test_deps = [
+    "pytest",
+    "mock",
+]
+
+extras = {
+    'test': test_deps,
+}
+
+setup(
+    name='footil',
+    version=__version__,
+    packages=['footil', 'footil.tools'],
+    license='LGPLv3',
+    url='https://github.com/focusate/footil',
+    description="Various Python helpers for other projects",
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'yattag',
+        'python-dateutil',
+        'verboselogs',
+        # TODO: upgrade to 3.*.* >= version once it is released.
+        'semver <= 2.13.0',
+        'natsort',
+    ],
+    tests_require=test_deps,
+    extras_require=extras,
+    maintainer='Andrius Laukavičius',
+    maintainer_email='dev@focusate.eu',
+    python_requires='>=3.5',
+    classifiers=[
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+    ],
+)
