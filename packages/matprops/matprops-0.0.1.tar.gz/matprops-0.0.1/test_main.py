30452c0c954e94e1acd5084757510e30bbcee31c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:45 +0000

    Add package matprops/0.0.1/matprops-0.0.1.tar.gz

---
+++ packages/matprops/matprops-0.0.1.tar.gz/matprops-0.0.1/test_main.py
@@ -0,0 +1,9 @@
+from matprops import say_hello
+
+
+def test_main_no_params():
+    assert say_hello() == "Hello World!"
+
+
+def test_main_with_params():
+    assert say_hello("Everybody") == "Hello Everybody!"
