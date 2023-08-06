12f26a66bffe4259b50e965282312a775b618420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:39 +0000

    Add package zntrack/0.3.3/zntrack-0.3.3.tar.gz

---
+++ packages/zntrack/zntrack-0.3.3.tar.gz/zntrack-0.3.3/zntrack/utils/utils.py
@@ -0,0 +1,94 @@
+"""
+This program and the accompanying materials are made available under the terms of the
+Eclipse Public License v2.0 which accompanies this distribution, and is available at
+https://www.eclipse.org/legal/epl-v20.html
+SPDX-License-Identifier: EPL-2.0
+
+Copyright Contributors to the Zincware Project.
+
+Description:
+"""
+
+import json
+import logging
+import os
+import shutil
+import tempfile
+
+import znjson
+
+from zntrack.utils.config import config
+
+log = logging.getLogger(__name__)
+
+
+# https://stackoverflow.com/questions/42033142/is-there-an-easy-way-to-check-if-an-object-is-json-serializable-in-python
+def is_jsonable(x: dict) -> bool:
+    """
+
+    Parameters
+    ----------
+    x: dict
+        Dictionary to check, if it is json serializable.
+
+    Returns
+    -------
+    bool: Whether the dict was serializable or not.
+
+    """
+    try:
+        json.dumps(x)
+        return True
+    except (TypeError, OverflowError):
+        return False
+
+
+def cwd_temp_dir(required_files=None) -> tempfile.TemporaryDirectory:
+    """Change into a temporary directory
+
+    Helper for e.g. the docs to quickly change into a temporary directory
+    and copy all files, e.g. the Notebook into that directory.
+
+    Parameters
+    ----------
+    required_files: list, optional
+        A list of optional files to be copied
+
+    Returns
+    -------
+    temp_dir:
+        The temporary  directory file. Close with temp_dir.cleanup() at the end.
+
+    """
+    temp_dir = tempfile.TemporaryDirectory()  # add ignore_cleanup_errors=True in Py3.10?
+
+    if config.nb_name is not None:
+        shutil.copy(config.nb_name, temp_dir.name)
+    if required_files is not None:
+        for file in required_files:
+            shutil.copy(file, temp_dir.name)
+
+    os.chdir(temp_dir.name)
+
+    return temp_dir
+
+
+def deprecated(reason, version="v0.0.0"):
+    """Depreciation Warning"""
+
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            log.critical(
+                f"DeprecationWarning for {func.__name__}: {reason} (Deprecated since"
+                f" {version})"
+            )
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def decode_dict(value):
+    """Decode dict that was loaded without znjson"""
+    return json.loads(json.dumps(value), cls=znjson.ZnDecoder)
