aad780699639b1fde5e7838773a55c84fc199b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:07 +0000

    Add package flake8_ruler/0.1.0/flake8_ruler-0.1.0.tar.gz

---
+++ packages/flake8_ruler/flake8_ruler-0.1.0.tar.gz/flake8_ruler-0.1.0/flake8_ruler/__init__.py
@@ -0,0 +1,7 @@
+"""More powerful configs for flake8.
+"""
+
+from ._cli import main
+
+__version__ = '0.1.0'
+__all__ = ['__version__', 'main']
---
+++ packages/flake8_ruler/flake8_ruler-0.1.0.tar.gz/flake8_ruler-0.1.0/tests/test_cli.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+import subprocess
+import sys
+from textwrap import dedent
+from typing import Tuple
+from flake8_ruler import main
+from io import StringIO
+
+INPUT_CONFIG = """
+    [tool.flake8_ruler]
+    max_line_length = 90
+
+    [tool.flake8_ruler.plugins]
+    pyflakes = ["-*", "+F62*"]
+"""
+
+OUTPUT_CONFIG = """
+    [flake8]
+    max_line_length = 90
+    select =
+        # pycodestyle
+        E902,  # *s
+        # pyflakes
+        F621,  # too many expressions in star-unpacking assignment
+        F622,  # two starred expressions in assignment
+"""
+
+
+def run(*args) -> Tuple[int, str]:
+    stream = StringIO()
+    code = main(argv=[str(arg) for arg in args], stream=stream)
+    stream.seek(0)
+    return code, stream.read()
+
+
+def test_main(tmp_path: Path):
+    input_path = tmp_path / 'pyproject.toml'
+    output_path = tmp_path / 'setup.cfg'
+    input_path.write_text(dedent(INPUT_CONFIG))
+    cmd = [
+        sys.executable,
+        '-m', 'flake8_ruler',
+        '--input', str(input_path),
+        '--output', str(output_path),
+    ]
+    result = subprocess.run(cmd)
+    assert result.returncode == 0
+    actual = output_path.read_text().replace('\t', '    ').strip()
+    exp = dedent(OUTPUT_CONFIG).strip().replace('select =', 'select = ')
+    assert actual == exp
+
+
+def test_warn_unknown_rule(tmp_path: Path):
+    input_path = tmp_path / 'pyproject.toml'
+    output_path = tmp_path / 'setup.cfg'
+    input_path.write_text(dedent("""
+        [tool.flake8_ruler.plugins]
+        unknown_plugin = ["+*"]
+    """))
+    code, output = run('--input', input_path, '--output', output_path)
+    assert code == 1
+    exp = 'unknown_plugin is expected but not found\n'
+    assert exp in output
