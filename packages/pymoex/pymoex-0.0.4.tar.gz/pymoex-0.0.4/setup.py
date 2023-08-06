a92f9170bf20723519fec008b90cfe9153ff24b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:33 +0000

    Add package pymoex/0.0.4/pymoex-0.0.4.tar.gz

---
+++ packages/pymoex/pymoex-0.0.4.tar.gz/pymoex-0.0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    version='0.0.4',
+    name='pymoex',
+    description='Unofficial ISS MOEX API on Python',
+    url='https://github.com/ntcny/moex',
+    author='Anton Parshutin',
+    author_email='n.tcnie@gmail.com',
+    license='GNU',
+    install_requires=['pandas>=0.18.0'],
+    packages=['moex'],
+    zip_safe=False)
