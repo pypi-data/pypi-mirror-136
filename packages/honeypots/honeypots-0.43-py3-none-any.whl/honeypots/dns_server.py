d349454014eb69cbf5688c9df5c844cca3909c85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:59 +0000

    Add package honeypots/0.43/honeypots-0.43-py3-none-any.whl

---
+++ packages/honeypots/honeypots-0.43-py3-none-any.whl/honeypots/dns_server.py
@@ -0,0 +1,132 @@
+'''
+//  -------------------------------------------------------------
+//  author        Giga
+//  project       qeeqbox/honeypots
+//  email         gigaqeeq@gmail.com
+//  description   app.py (CLI)
+//  licensee      AGPL-3.0
+//  -------------------------------------------------------------
+//  contributors list qeeqbox/honeypots/graphs/contributors
+//  -------------------------------------------------------------
+'''
+
+from warnings import filterwarnings
+filterwarnings(action='ignore', module='.*OpenSSL.*')
+
+from twisted.names import dns, error, client
+from twisted.names.server import DNSServerFactory
+from twisted.internet import defer, reactor
+from twisted.python import log as tlog
+from subprocess import Popen
+from os import path
+from honeypots.helper import close_port_wrapper, get_free_port, kill_server_wrapper, server_arguments, setup_logger, disable_logger, set_local_vars, check_if_server_is_running
+from uuid import uuid4
+
+
+class QDNSServer():
+    def __init__(self, ip=None, port=None, username=None, password=None, resolver_addresses=None, mocking=False, config=''):
+        self.auto_disabled = None
+        self.resolver_addresses = resolver_addresses or [('8.8.8.8', 53)]
+        self.process = None
+        self.uuid = 'honeypotslogger' + '_' + __class__.__name__ + '_' + str(uuid4())[:8]
+        self.config = config
+        self.mocking = mocking or ''
+        self.ip = None
+        self.port = None
+        self.username = None
+        self.password = None
+        if config:
+            self.logs = setup_logger(__class__.__name__, self.uuid, config)
+            set_local_vars(self, config)
+        else:
+            self.logs = setup_logger(__class__.__name__, self.uuid, None)
+        self.ip = ip or self.ip or '0.0.0.0'
+        self.port = port or self.port or 53
+        self.username = username or self.username or 'test'
+        self.password = password or self.password or 'test'
+        disable_logger(1, tlog)
+
+    def dns_server_main(self):
+        _q_s = self
+
+        class CustomCilentResolver(client.Resolver):
+            def queryUDP(self, queries, timeout=2):
+                res = client.Resolver.queryUDP(self, queries, timeout)
+
+                def queryFailed(reason):
+                    return defer.fail(error.DomainError())
+                res.addErrback(queryFailed)
+                return res
+
+        class CustomDNSServerFactory(DNSServerFactory):
+            def gotResolverResponse(self, response, protocol, message, address):
+                args = (self, response, protocol, message, address)
+                _q_s.logs.info({'server': 'dns_server', 'action': 'connection', 'src_ip': address[0], 'src_port': address[1], 'dest_ip': _q_s.ip, 'dest_port': _q_s.port})
+                try:
+                    for items in response:
+                        for item in items:
+                            _q_s.logs.info({'server': 'dns_server', 'action': 'query', 'src_ip': address[0], 'src_port': address[1], 'dest_ip': _q_s.ip, 'dest_port': _q_s.port, 'payload': item.payload})
+                except Exception as e:
+                    pass
+                return DNSServerFactory.gotResolverResponse(*args)
+
+        self.resolver = CustomCilentResolver(servers=self.resolver_addresses)
+        self.factory = CustomDNSServerFactory(clients=[self.resolver])
+        self.protocol = dns.DNSDatagramProtocol(controller=self.factory)
+        reactor.listenUDP(self.port, self.protocol, interface=self.ip)
+        reactor.listenTCP(self.port, self.factory, interface=self.ip)
+        reactor.run()
+
+    def run_server(self, process=False, auto=False):
+        status = 'error'
+        run = False
+        if process:
+            if auto and not self.auto_disabled:
+                port = get_free_port()
+                if port > 0:
+                    self.port = port
+                    run = True
+            elif self.close_port() and self.kill_server():
+                run = True
+
+            if run:
+                self.process = Popen(['python3', path.realpath(__file__), '--custom', '--ip', str(self.ip), '--port', str(self.port), '--mocking', str(self.mocking), '--config', str(self.config), '--uuid', str(self.uuid)])
+                if self.process.poll() is None and check_if_server_is_running(self.uuid):
+                    status = 'success'
+
+            self.logs.info({'server': 'dns_server', 'action': 'process', 'status': status, 'src_ip': self.ip, 'src_port': self.port, 'dest_ip': self.ip, 'dest_port': self.port})
+
+            if status == 'success':
+                return True
+            else:
+                self.kill_server()
+                return False
+        else:
+            self.dns_server_main()
+        return None
+
+    def close_port(self):
+        ret = close_port_wrapper('dns_server', self.ip, self.port, self.logs)
+        return ret
+
+    def kill_server(self):
+        ret = kill_server_wrapper('dns_server', self.uuid, self.process)
+        return ret
+
+    def test_server(self, ip=None, port=None, domain=None):
+        try:
+            from dns.resolver import Resolver
+            res = Resolver(configure=False)
+            res.nameservers = [self.ip]
+            res.port = self.port
+            temp_domain = domain or 'example.org'
+            r = res.query(temp_domain, 'a')
+        except BaseException:
+            pass
+
+
+if __name__ == '__main__':
+    parsed = server_arguments()
+    if parsed.docker or parsed.aws or parsed.custom:
+        qdnsserver = QDNSServer(ip=parsed.ip, port=parsed.port, resolver_addresses=parsed.resolver_addresses, config=parsed.config)
+        qdnsserver.run_server()
