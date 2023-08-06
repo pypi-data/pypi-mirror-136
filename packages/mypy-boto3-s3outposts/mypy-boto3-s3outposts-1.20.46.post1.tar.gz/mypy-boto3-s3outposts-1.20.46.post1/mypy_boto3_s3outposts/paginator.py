8cc2cb7962d12785c086879d77453631ff301a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:49 +0000

    Add package mypy-boto3-s3outposts/1.20.46.post1/mypy-boto3-s3outposts-1.20.46.post1.tar.gz

---
+++ packages/mypy-boto3-s3outposts/mypy-boto3-s3outposts-1.20.46.post1.tar.gz/mypy-boto3-s3outposts-1.20.46.post1/mypy_boto3_s3outposts/paginator.py
@@ -0,0 +1,53 @@
+"""
+Type annotations for s3outposts service client paginators.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_s3outposts/paginators.html)
+
+Usage::
+
+    ```python
+    import boto3
+
+    from mypy_boto3_s3outposts import S3OutpostsClient
+    from mypy_boto3_s3outposts.paginator import (
+        ListEndpointsPaginator,
+    )
+
+    client: S3OutpostsClient = boto3.client("s3outposts")
+
+    list_endpoints_paginator: ListEndpointsPaginator = client.get_paginator("list_endpoints")
+    ```
+"""
+from typing import Generic, Iterator, TypeVar
+
+from botocore.paginate import PageIterator
+from botocore.paginate import Paginator as Boto3Paginator
+
+from .type_defs import ListEndpointsResultTypeDef, PaginatorConfigTypeDef
+
+__all__ = ("ListEndpointsPaginator",)
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListEndpointsPaginator(Boto3Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.20.46/reference/services/s3outposts.html#S3Outposts.Paginator.ListEndpoints)
+    [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_s3outposts/paginators.html#listendpointspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListEndpointsResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.20.46/reference/services/s3outposts.html#S3Outposts.Paginator.ListEndpoints.paginate)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_s3outposts/paginators.html#listendpointspaginator)
+        """
