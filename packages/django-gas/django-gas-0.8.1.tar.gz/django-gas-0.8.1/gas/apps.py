c2efbe66e7755c4e8b2979de526815a4ca2b5afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:19 +0000

    Add package django-gas/0.8.1/django-gas-0.8.1.tar.gz

---
+++ packages/django-gas/django-gas-0.8.1.tar.gz/django-gas-0.8.1/gas/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class GASConfig(AppConfig):
+    name = 'gas'
+    verbose_name = _('GAS')
+
+    def ready(self):
+        from .sites import site
+        super().ready()
+        site.autodiscover()
