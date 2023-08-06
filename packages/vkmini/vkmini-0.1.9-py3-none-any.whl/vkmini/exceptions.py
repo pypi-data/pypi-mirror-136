e92f38ea88767af7198881304e69b65be30bc02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:37 +0000

    Add package vkmini/0.1.9/vkmini-0.1.9-py3-none-any.whl

---
+++ packages/vkmini/vkmini-0.1.9-py3-none-any.whl/vkmini/exceptions.py
@@ -0,0 +1,39 @@
+class VkResponseException(Exception):
+    request_data: dict
+    error_code: int
+    error_msg: str
+    error_data: dict
+
+    def __init__(self, error: dict, request_data: dict = {}):
+        self.error_code = error['error_code']
+        self.error_msg = error['error_msg']
+        self.request_data = request_data
+        self.error_data = error
+
+    def __str__(self) -> str:
+        info = f'Ошибка ВК #{self.error_code}: {self.error_msg}'
+        if self.request_data:
+            info += f'\nПараметры запроса: {self.request_data}'
+        return info
+    
+    def get_short_str(self) -> str:
+        return f'#{self.error_code}: {self.error_msg}'
+
+
+class NetworkError(Exception):
+    code: int
+
+    def __init__(self, code: int):
+        self.code = code
+
+
+class CaptchaNeeded(VkResponseException):
+    """Captcha needed (14) error"""
+
+
+class TooManyRequests(VkResponseException):
+    """Too many requests (6) error"""
+
+
+class TokenInvalid(VkResponseException):
+    """Token is not valid (5) error"""
