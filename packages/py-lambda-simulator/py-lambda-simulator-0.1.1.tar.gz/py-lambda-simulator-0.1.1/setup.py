db4e185654236ade10ee2b4885aeaf9133506ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:09 +0000

    Add package py-lambda-simulator/0.1.1/py-lambda-simulator-0.1.1.tar.gz

---
+++ packages/py-lambda-simulator/py-lambda-simulator-0.1.1.tar.gz/py-lambda-simulator-0.1.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+modules = \
+['py_lambda_simulator']
+install_requires = \
+['aiohttp>=3.8.1,<4.0.0',
+ 'asyncer>=0.0.1,<0.0.2',
+ 'boto3>=1.20.46,<2.0.0',
+ 'moto>=3.0.2,<4.0.0',
+ 'typing-extensions>=4.0.1,<5.0.0']
+
+setup_kwargs = {
+    'name': 'py-lambda-simulator',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Johan Bothin',
+    'author_email': 'johan.bothin@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/hemma/py-lambda-simulator',
+    'package_dir': package_dir,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
