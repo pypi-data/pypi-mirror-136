5c3225aeb6f19993ed572eee0a90b2f2648c4512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:05 +0000

    Add package ytdl-gui/0.1.5/ytdl_gui-0.1.5-py3-none-any.whl

---
+++ packages/ytdl-gui/ytdl_gui-0.1.5-py3-none-any.whl/ytdl_gui/const.py
@@ -0,0 +1,7 @@
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+WIDTH = 600
+HEIGHT = 800
+TITLE = 'ytdl_gui'
+APPID = 'org.sheep.youtube-downloader'
+CSSPATH = f'{dir_path}/css/style.css'
