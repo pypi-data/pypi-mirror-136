921950ca262a4b347ac60dc757d4b7d8a50a30c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:56 +0000

    Add package sqlcli/0.1.0/sqlcli-0.1.0.tar.gz

---
+++ packages/sqlcli/sqlcli-0.1.0.tar.gz/sqlcli-0.1.0/sqlcli/_help.py
@@ -0,0 +1,25 @@
+"""
+A module to store help strings that are shared by multiple commands.
+"""
+
+database_url_help = """
+A database connection string. If no connection string is provided sqlcli will
+check for a connection string in the environment variable `DATABASE_URL`.
+""".strip().replace(
+    "\n", ""
+)
+
+models_path_help = """
+The location of the python script(s) that contain the SQLModels. If no argument
+is provided sqlcli will check for a path in the environment variable
+`MODELS_PATH`.
+""".strip().replace(
+    "\n", ""
+)
+
+table_name_help = """
+The name of the table to query. If no name is provided the interactive prompt
+will ask for a table name.
+""".strip().replace(
+    "\n", ""
+)
