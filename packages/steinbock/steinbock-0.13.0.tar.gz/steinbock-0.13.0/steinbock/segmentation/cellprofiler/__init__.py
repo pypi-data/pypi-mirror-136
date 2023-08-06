07d771daaa7df5ac13a12eab3fccfff094a4f90a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:54 +0000

    Add package steinbock/0.13.0/steinbock-0.13.0.tar.gz

---
+++ packages/steinbock/steinbock-0.13.0.tar.gz/steinbock-0.13.0/steinbock/segmentation/cellprofiler/__init__.py
@@ -0,0 +1,6 @@
+from steinbock.segmentation.cellprofiler._cellprofiler import (
+    create_and_save_segmentation_pipeline,
+    try_segment_objects,
+)
+
+__all__ = ["create_and_save_segmentation_pipeline", "try_segment_objects"]
