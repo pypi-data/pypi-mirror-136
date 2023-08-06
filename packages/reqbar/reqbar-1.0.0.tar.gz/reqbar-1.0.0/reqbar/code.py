a431ffc173b3117f60ebc3abf10ae6145dbf1197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:30 +0000

    Add package reqbar/1.0.0/reqbar-1.0.0.tar.gz

---
+++ packages/reqbar/reqbar-1.0.0.tar.gz/reqbar-1.0.0/reqbar/code.py
@@ -0,0 +1,24 @@
+from subprocess import call
+call("pip install -q tqdm", shell=True)
+import os
+from tqdm.notebook import tqdm
+
+class install:
+    def __init__(self):
+        self._install_code()
+    def _install_code(self):
+        if os.path.exists('i.txt'):
+            filename = "i.txt"
+        elif os.path.exists('requirements.txt'):
+            filename = "requirements.txt"
+        
+
+        f = open(filename, "r")
+        lst = [i[:-1] for i in f.readlines()]
+        f.close()
+
+        for i in tqdm(lst):
+            if i[:5] == "npm:":
+                call(f"npm install -g --silent {i[5:]}", shell=True)
+            else:  
+                call(f"pip install -q {i}", shell=True)    
