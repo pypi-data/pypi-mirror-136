effd47fe2569d77f0a2af29c53e4e2aab7604e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:34 +0000

    Add package example-package-fabiog1901/0.0.4/example-package-fabiog1901-0.0.4.tar.gz

---
+++ packages/example-package-fabiog1901/example-package-fabiog1901-0.0.4.tar.gz/example-package-fabiog1901-0.0.4/README.md
@@ -0,0 +1,1 @@
+# example-package-fabiog1901
---
+++ packages/example-package-fabiog1901/example-package-fabiog1901-0.0.4.tar.gz/example-package-fabiog1901-0.0.4/src/example_package/example.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def add_one(number):
+    return number + 1
+
+def add_two(n):
+    return n + 2
+
+def add_three(n):
+    return n + 3
+
+def add_random():
+    rng = np.random.default_rng()
+    return rng.integers(0, 100)
+
+def welcome():
+    print("Welcome!")
