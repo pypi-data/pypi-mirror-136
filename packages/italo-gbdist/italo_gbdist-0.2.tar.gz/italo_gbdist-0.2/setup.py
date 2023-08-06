2c4e15fc4af7e2f03fbc13a6f03587ab6339075d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:01 +0000

    Add package italo-gbdist/0.2/italo_gbdist-0.2.tar.gz

---
+++ packages/italo-gbdist/italo_gbdist-0.2.tar.gz/italo_gbdist-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='italo_gbdist',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['italo_gbdist'],
+      author = 'Italo Castilho',
+      zip_safe=False)
