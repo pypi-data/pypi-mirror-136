73994cb8c0b896c17ccc988e00e79a5f20d10745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:48 +0000

    Add package fred-matt-merman/0.8/fred_matt_merman-0.8-py3-none-any.whl

---
+++ packages/fred-matt-merman/fred_matt_merman-0.8-py3-none-any.whl/fred_matt_merman/request.py
@@ -0,0 +1,27 @@
+import requests
+
+def request_json(url):
+    
+    try:
+        
+        request = requests.get(url)
+        check = request.status_code
+    
+        if check != 200: return -1
+    
+    except requests.ConnectionError: return -1
+        
+    return request
+    
+    
+def get(url_base, url_key, type, id):
+            
+    url = ""
+    
+    if type == "category_children": url = url_base + "category/children?category_id=" + id + url_key
+            
+    elif type == "series": url = url_base + "category/series?category_id=" + str(id) + url_key
+    
+    elif type == "observation": url = url_base + "series/observations?series_id=" + id + url_key
+        
+    return request_json(url)
