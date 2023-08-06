1455cf53d2a33009753f434946775ca7a603aedf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:28 +0000

    Add package espial/0.1.0/espial-0.1.0-py3-none-any.whl

---
+++ packages/espial/espial-0.1.0-py3-none-any.whl/espial/cli.py
@@ -0,0 +1,34 @@
+import click
+from espial.config import Config
+from espial import create_app
+from pathlib import Path
+
+
+@click.group()
+def espial():
+    """Build a second brain, with a sane amount of effort :)"""
+    pass
+
+
+@espial.command("run")
+@click.argument("data-dir", type=click.Path(exists=True))
+@click.option("--rerun", help="Regenerate existing concept graph", is_flag=True)
+@click.option("--port", type=int, help="Port to run server on.", default=5002)
+@click.option("--host", type=str, help="Host to run server on.", default="127.0.0.1")
+# @click.option("--build", type=bool, help="If enabled will build the concept graph as a static website")
+def run(data_dir, rerun, port, host):
+    config_path = Path(data_dir) / "espial.py"
+    if config_path:
+        contents = open(config_path)
+        config_locals = {}
+        exec(contents.read(), globals(), config_locals)
+        contents.close()
+        config = config_locals.get("Config", Config)()
+    else:
+        config = Config()
+    config.data_dir = data_dir
+    config.port = port
+    config.host = host
+    config.ANALYSIS["rerun"] = rerun
+    app = create_app(config)
+    app.run(port=port, host=host)
