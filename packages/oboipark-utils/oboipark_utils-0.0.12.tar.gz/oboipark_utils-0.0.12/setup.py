2f1ec7c4346e0e16e3083f05d10417a6c37c63c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:18 +0000

    Add package oboipark-utils/0.0.12/oboipark_utils-0.0.12.tar.gz

---
+++ packages/oboipark-utils/oboipark_utils-0.0.12.tar.gz/oboipark_utils-0.0.12/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='oboipark_utils',
+    version='0.0.12',
+    packages=['oboipark_utils'],
+    url='https://gitlab.com/oboipark/utils',
+    license='python',
+    author='VVD-byte',
+    author_email='vovavoronin1999@gmail.com',
+    description='утилиты для обоипарк'
+)
