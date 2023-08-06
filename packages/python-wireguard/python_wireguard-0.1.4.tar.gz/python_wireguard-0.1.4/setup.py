5c2f6ad93e26ebe0d9780c1fbcda88745b754d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:50 +0000

    Add package python-wireguard/0.1.4/python_wireguard-0.1.4.tar.gz

---
+++ packages/python-wireguard/python_wireguard-0.1.4.tar.gz/python_wireguard-0.1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from pathlib import Path
+import os
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+version = os.environ.get('RELEASE_VERSION')
+
+setup(name='python_wireguard',
+      version=version,
+      description='A python wrapper for controlling Wireguard',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/jarnoaxel/python-wireguard',
+      author='jarnoaxel',
+      license='GPL-3.0-or-later',
+      packages=['python_wireguard'],
+      include_package_data=True,
+      keywords=['vpn', 'wireguard'],
+      zip_safe=False)
