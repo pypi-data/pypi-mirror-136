814c330871cc48f7e787bd9963cea362cd2e7e29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:04 +0000

    Add package aiohttp-swagger3/0.7.1/aiohttp-swagger3-0.7.1.tar.gz

---
+++ packages/aiohttp-swagger3/aiohttp-swagger3-0.7.1.tar.gz/aiohttp-swagger3-0.7.1/aiohttp_swagger3/__init__.py
@@ -0,0 +1,19 @@
+__all__ = (
+    "swagger_doc",
+    "RapiDocUiSettings",
+    "ReDocUiSettings",
+    "RequestValidationFailed",
+    "SwaggerDocs",
+    "SwaggerFile",
+    "SwaggerUiSettings",
+    "ValidatorError",
+    "__version__",
+)
+__version__ = "0.7.1"
+__author__ = "Valetov Konstantin"
+
+from .exceptions import ValidatorError
+from .swagger_docs import SwaggerDocs, swagger_doc
+from .swagger_file import SwaggerFile
+from .swagger_route import RequestValidationFailed
+from .ui_settings import RapiDocUiSettings, ReDocUiSettings, SwaggerUiSettings
