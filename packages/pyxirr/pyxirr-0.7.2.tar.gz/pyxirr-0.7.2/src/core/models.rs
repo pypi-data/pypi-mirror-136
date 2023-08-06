2753e85d846e5b290de4739d084e9b2d97e29c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:29 +0000

    Add package pyxirr/0.7.2/pyxirr-0.7.2.tar.gz

---
+++ packages/pyxirr/pyxirr-0.7.2.tar.gz/pyxirr-0.7.2/src/core/models.rs
@@ -0,0 +1,81 @@
+use chrono::prelude::*;
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug, PartialEq, Eq, Hash, PartialOrd, Ord, Clone, Copy)]
+pub struct DateLike(i32);
+
+impl From<i32> for DateLike {
+    fn from(value: i32) -> Self {
+        Self(value)
+    }
+}
+
+impl From<NaiveDate> for DateLike {
+    fn from(value: NaiveDate) -> Self {
+        Self(value.num_days_from_ce())
+    }
+}
+
+impl std::ops::Sub for DateLike {
+    type Output = i32;
+
+    fn sub(self, other: DateLike) -> Self::Output {
+        self.0 - other.0
+    }
+}
+
+impl std::ops::Sub for &DateLike {
+    type Output = i32;
+
+    fn sub(self, other: &DateLike) -> Self::Output {
+        self.0 - other.0
+    }
+}
+
+impl std::str::FromStr for DateLike {
+    type Err = chrono::ParseError;
+
+    fn from_str(s: &str) -> chrono::ParseResult<Self> {
+        // get only date part: yyyy-mm-dd
+        // this allows to parse datetime strings
+        let s = if s.len() > 10 { &s[0..10] } else { s };
+
+        // try %Y-%m-%d
+        if let Ok(d) = NaiveDate::parse_from_str(s, "%Y-%m-%d") {
+            return Ok(d.into());
+        }
+
+        // try %m/%d/%Y
+        Ok(NaiveDate::parse_from_str(s, "%m/%d/%Y")?.into())
+    }
+}
+
+/// An error returned when the payments do not contain both negative and positive payments.
+#[derive(Debug)]
+pub struct InvalidPaymentsError(String);
+
+impl fmt::Display for InvalidPaymentsError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl Error for InvalidPaymentsError {}
+
+pub fn validate(payments: &[f64], dates: Option<&[DateLike]>) -> Result<(), InvalidPaymentsError> {
+    if dates.is_some() && payments.len() != dates.unwrap_or_default().len() {
+        return Err(InvalidPaymentsError(
+            "the amounts and dates arrays are of different lengths".into(),
+        ));
+    }
+
+    let positive = payments.iter().any(|&p| p > 0.0);
+    let negative = payments.iter().any(|&p| p < 0.0);
+
+    if positive && negative {
+        Ok(())
+    } else {
+        Err(InvalidPaymentsError("negative and positive payments are required".into()))
+    }
+}
