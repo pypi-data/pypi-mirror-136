a714cb57c13a487fd8a8e90f5b43f14015afff32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:00 +0000

    Add package uni-say/0.1.2/uni-say-0.1.2.tar.gz

---
+++ packages/uni-say/uni-say-0.1.2.tar.gz/uni-say-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['uni_say']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.3,<9.0.0']
+
+entry_points = \
+{'console_scripts': ['uni-say = uni_say.cli:cli']}
+
+setup_kwargs = {
+    'name': 'uni-say',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Eyal Levin',
+    'author_email': 'eyalev@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
