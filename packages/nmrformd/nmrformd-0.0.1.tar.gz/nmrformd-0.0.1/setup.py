d8cfdbd545da26bdd46dd6db898c737bf6c167fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:36 +0000

    Add package nmrformd/0.0.1/nmrformd-0.0.1.tar.gz

---
+++ packages/nmrformd/nmrformd-0.0.1.tar.gz/nmrformd-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='nmrformd',
+      version='v0.0.1',
+      description='Calculate NMR relaxation time from molecular dynamics trajectory file',
+      long_description=open('PyPI/README.rst').read(),
+      url='https://github.com/simongravelle/nmrformd',
+      download_url='https://github.com/simongravelle/nmrformd/archive/refs/tags/v0.0.1.tar.gz',
+      author='Simon Gravelle',
+      author_email='simon.gravelle@live.fr',
+      license='GNU GENERAL PUBLIC LICENSE',
+      packages=['nmrformd'],
+      zip_safe=False,
+         install_requires=[
+       "mdanalysis",
+       "pytest",
+       "numpy",
+       "scipy",
+      ]      
+      )
