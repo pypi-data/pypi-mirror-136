6081c55cf3aca6a7971df35a9cbe5ef0d043c6ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:10 +0000

    Add package logfly/1.7/logfly-1.7.tar.gz

---
+++ packages/logfly/logfly-1.7.tar.gz/logfly-1.7/src/logfly/soundfly/__init__.py
@@ -0,0 +1,7 @@
+# Sound analysis lib
+# Author: Yuan Sui
+# Date: 2022-1-16
+# TODO: add code. may add wav file wave,
+#  spectrum and limit check(displayed via matplotlib) --Yuan
+
+"""I love you, Pidan"""
