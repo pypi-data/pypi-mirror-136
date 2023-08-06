2a82102b7e013718580e2c9cfd327d237d05cd88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:44 +0000

    Add package SexyThumbnailGenerator/1.0.3/SexyThumbnailGenerator-1.0.3.tar.gz

---
+++ packages/SexyThumbnailGenerator/SexyThumbnailGenerator-1.0.3.tar.gz/SexyThumbnailGenerator-1.0.3/src/SexyThumbnailGenerator/__init__.py
@@ -0,0 +1,4 @@
+from .classifier import Classifier as NudeClassifier
+from .lite_classifier import LiteClassifier as NudeClassifierLite
+from .detector import Detector as NudeDetector
+from .thumbnails import Generator
