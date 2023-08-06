042182ab6bc4e73bd0b86a675b2f6f6f2c2eaea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:47 +0000

    Add package mypdfpkg1/2022.1/mypdfpkg1-2022.1-py3-none-any.whl

---
+++ packages/mypdfpkg1/mypdfpkg1-2022.1-py3-none-any.whl/mypdfpkg/pdf2text.py
@@ -0,0 +1,21 @@
+""" 
+short summery of module
+
+A more detailes explanation.
+"""
+
+
+def convert(Path):
+    """
+    Convert the given PDF to text.(a short summery of object)
+
+    more details about function is represented here.
+    Parameters:
+    (What Path is, its type, what we use it for)
+    Path(str): The path to a PDF file.
+    ...
+
+    Returns:
+    str: The content of the PDF file as text.
+    """
+    print("convert function: Pdf2Text")
