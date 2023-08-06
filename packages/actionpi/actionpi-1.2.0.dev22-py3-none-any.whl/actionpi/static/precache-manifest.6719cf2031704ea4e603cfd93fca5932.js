7dbef82a01787f57e859117a492b96ffaa32d40a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:08 +0000

    Add package actionpi/1.2.0.dev22/actionpi-1.2.0.dev22-py3-none-any.whl

---
+++ packages/actionpi/actionpi-1.2.0.dev22-py3-none-any.whl/actionpi/static/precache-manifest.6719cf2031704ea4e603cfd93fca5932.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74cdf85775ecedc288792f8178816fbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88af1e82dbe511aab0fa",
+    "url": "/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "0b999f215fd073ff61d8",
+    "url": "/static/js/2.4064d2ac.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.4064d2ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88af1e82dbe511aab0fa",
+    "url": "/static/js/main.a065b716.chunk.js"
+  },
+  {
+    "revision": "98c1327435369df6cc45",
+    "url": "/static/js/runtime-main.c63fbe4a.js"
+  },
+  {
+    "revision": "cabc4b0d6882524fc0e7872c7fc50e0d",
+    "url": "/static/media/logo.cabc4b0d.png"
+  }
+]);
