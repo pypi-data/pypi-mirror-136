9c11059ac52f964fae7ecf62a161331ce795d1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:05 +0000

    Add package codeseeker/0.0.2/codeseeker-0.0.2.tar.gz

---
+++ packages/codeseeker/codeseeker-0.0.2.tar.gz/codeseeker-0.0.2/codeseeker/utils.py
@@ -0,0 +1,14 @@
+from typing import List, Dict
+
+WILDCARDS: str = ".,:;/\`'\"=*!?#$&+^|~<>()\{\}[]@"  # noqa: W605
+
+
+def show(data: List[Dict[str, str]], tag: str = "path") -> None:
+    """Display the data.
+
+    Args:
+        data (Dict[str, str]): The data to be displayed.
+    """
+    for item in data:
+        print(item[tag])
+    print(f"\n{len(data)} file(s) found(s).")
