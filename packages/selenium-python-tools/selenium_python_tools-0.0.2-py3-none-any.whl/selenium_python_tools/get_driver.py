5ace3120c23777b3970527cf20d2ff13b386a054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:30 +0000

    Add package selenium-python-tools/0.0.2/selenium_python_tools-0.0.2-py3-none-any.whl

---
+++ packages/selenium-python-tools/selenium_python_tools-0.0.2-py3-none-any.whl/selenium_python_tools/get_driver.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def set_chrome_options() -> None:
+    """Sets chrome options for Selenium.
+    Chrome options for headless browser is enabled.
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    return chrome_options
+
+def get_driver():
+    return webdriver.Chrome(options=set_chrome_options())
