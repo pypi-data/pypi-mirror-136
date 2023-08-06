44ddd9af226b5a9bbef04d61bc2251a6a13e5854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:34 +0000

    Add package TN2601/0.57/TN2601-0.57.tar.gz

---
+++ packages/TN2601/TN2601-0.57.tar.gz/TN2601-0.57/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup 
+
+setup(
+    name="TN2601",
+    version="0.57",
+    description="Librería para poder chequear ejercicios propuestos del libro numérico.",
+    author="Axel Osses A. & Diego Olguín W.",
+    packages=["TN"],
+    zip_safe=False
+)
