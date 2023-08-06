d9a91dd9433dab9e674aafd2966f32e07f344c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:15 +0000

    Add package BeastX/1.0/BeastX-1.0.tar.gz

---
+++ packages/BeastX/BeastX-1.0.tar.gz/BeastX-1.0/beastx/__init__.py
@@ -0,0 +1,74 @@
+"""
+# -*- coding: utf-8 -*-
+
+__author__ = "Akash"
+__email__ = "akashjio6666@gmail.com"
+__version__ = 1.0.0"
+__copyright__ = "Copyright (c) 2004-2020 Leonard Richardson"
+# Use of this source code is governed by the MIT license.
+__license__ = "MIT"
+
+Description:
+            Py-Insta Is A Python Library
+            Created By Akash Pattnaik From
+            India..
+            Py-Insta Helps Users To Easily
+            Scrape Instagram Data
+            And Print It Or You Can Define It Into A Variable...
+            If You Find Bugs Then Please Report To
+            @AKASH_AM1 On Telegram...
+
+Pre-Requests:
+            from bs4 import BeautifulSoup
+            import requests
+
+Documentation:
+            Github: https://github.com/BLUE-DEVIL1134/Py-Insta
+            PyPi: https://pypi.org/user/AkashPattnaik/
+"""
+__version__ = 1.0
+import requests
+from bs4 import BeautifulSoup
+from colorama import Fore, Back, Style, init
+
+init()
+n = Fore.RESET
+lg = Fore.LIGHTGREEN_EX
+r = Fore.RED
+w = Fore.WHITE
+cy = Fore.CYAN
+ye = Fore.YELLOW
+colors = [lg, r, w, cy, ye]
+
+
+def bannerred():
+    os.system('clear')
+    print(f"""
+{r}
+ ██████╗ ███████╗ █████╗ ███████╗████████╗{r}  
+ ██╔══██╗██╔════╝██╔══██╗██╔════╝╚══██╔══╝{r}
+ ██████╔╝█████╗  ███████║███████╗   ██║    {r}
+ ██╔══██╗██╔══╝  ██╔══██║╚════██║   ██║   {r}
+ ██████╔╝███████╗██║  ██║███████║   ██║    {r}
+ ╚═╝  ╚═╝╚═════╝ ╚═════╝ ╚══════╝   ╚═╝   {r}
+            Author: github.com/msy1717{r}
+            Developer : @Godmrunal{r}
+
+
+""")
+
+def bannerwhite():
+    os.system('clear')
+    print(f"""
+{r}
+ ██████╗ ███████╗ █████╗ ███████╗████████╗
+ ██╔══██╗██╔════╝██╔══██╗██╔════╝╚══██╔══╝
+ ██████╔╝█████╗  ███████║███████╗   ██║    
+ ██╔══██╗██╔══╝  ██╔══██║╚════██║   ██║   
+ ██████╔╝███████╗██║  ██║███████║   ██║    
+ ╚═╝  ╚═╝╚═════╝ ╚═════╝ ╚══════╝   ╚═╝   
+            Author: github.com/msy1717
+            Developer : @Godmrunal
+
+
+""")
---
+++ packages/BeastX/BeastX-1.0.tar.gz/BeastX-1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+fh.close()
+
+setuptools.setup(
+    name="BeastX",
+    version="1.0",
+    author="@Godmrunal",
+    author_email="mrunalgaming7@gmail.com",
+    description="A Python Package ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/msy1717",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
