124e17d22a0e66edee5bce3fcd69a4de72fe212b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:30 +0000

    Add package mypy-boto3-amplify/1.20.45/mypy_boto3_amplify-1.20.45-py3-none-any.whl

---
+++ packages/mypy-boto3-amplify/mypy_boto3_amplify-1.20.45-py3-none-any.whl/mypy_boto3_amplify/__main__.py
@@ -0,0 +1,39 @@
+"""
+Main CLI entrypoint.
+"""
+import sys
+
+
+def print_info() -> None:
+    """
+    Print package info to stdout.
+    """
+    print(
+        "Type annotations for boto3.Amplify 1.20.45\n"
+        "Version:         1.20.45\n"
+        "Builder version: 6.4.0\n"
+        "Docs:            https://pypi.org/project/mypy-boto3-amplify/\n"
+        "Boto3 docs:      https://boto3.amazonaws.com/v1/documentation/api/1.20.45/reference/services/amplify.html#Amplify\n"
+        "Other services:  https://pypi.org/project/boto3-stubs/\n"
+        "Changelog:       https://github.com/vemel/mypy_boto3_builder/releases"
+    )
+
+
+def print_version() -> None:
+    """
+    Print package version to stdout.
+    """
+    print("1.20.45")
+
+
+def main() -> None:
+    """
+    Main CLI entrypoint.
+    """
+    if "--version" in sys.argv:
+        return print_version()
+    print_info()
+
+
+if __name__ == "__main__":
+    main()
