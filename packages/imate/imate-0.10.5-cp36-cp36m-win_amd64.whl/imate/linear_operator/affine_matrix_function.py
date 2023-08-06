3150e4af0c87ed5742b33420754d7fbf538dccf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:49 +0000

    Add package imate/0.10.5/imate-0.10.5-cp36-cp36m-win_amd64.whl

---
+++ packages/imate/imate-0.10.5-cp36-cp36m-win_amd64.whl/imate/linear_operator/affine_matrix_function.py
@@ -0,0 +1,163 @@
+# SPDX-FileCopyrightText: Copyright 2021, Siavash Ameli <sameli@berkeley.edu>
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileType: SOURCE
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the license found in the LICENSE.txt file in the root
+# directory of this source tree.
+
+
+# =======
+# Imports
+# =======
+
+from .linear_operator import LinearOperator
+from scipy.sparse import isspmatrix
+
+
+# ======================
+# Affine Matrix Function
+# ======================
+
+class AffineMatrixFunction(LinearOperator):
+    """
+    """
+
+    # ====
+    # init
+    # ====
+
+    def __init__(self, A_, B_=None):
+        """
+        """
+
+        # Calling base class to initialize member data
+        super(AffineMatrixFunction, self).__init__()
+
+        # A reference to numpy or scipt sparse matrices
+        self.A = A_
+        self.B = B_
+        self.num_parameters = 1
+        self.set_data_type_name(self.A)
+
+    # ==========
+    # initialize
+    # ==========
+
+    def initialize(self, gpu, num_gpu_devices):
+        """
+        """
+
+        if gpu:
+
+            if not self.initialized_on_gpu:
+
+                # Create a linear operator on GPU
+                from .._cu_linear_operator import pycuAffineMatrixFunction
+                self.gpu_Aop = pycuAffineMatrixFunction(self.A, self.B,
+                                                        num_gpu_devices)
+                self.initialized_on_gpu = True
+
+            elif num_gpu_devices != self.num_gpu_devices:
+
+                # Get the actual (all) number of gpu devices
+                device_properties_dict = self.gpu_Aop.get_device_properties()
+                num_all_gpu_devices = device_properties_dict['num_devices']
+
+                # If number of gpu devices is zero, it means all gpu devices
+                # were used. Exclude cases when one of num_gpu_devices and
+                # self.num_gpu_devices is zero and the other is equal to all
+                # gpu devices to avoid unnecessary reallocation.
+                reallocate = True
+                if (num_gpu_devices == 0) and \
+                   (self.num_gpu_devices == num_all_gpu_devices):
+                    reallocate = False
+                elif (num_gpu_devices == num_all_gpu_devices) and \
+                     (self.num_gpu_devices == 0):
+                    reallocate = False
+
+                if reallocate:
+
+                    # Create a linear operator on GPU
+                    from .._cu_linear_operator import pycuAffineMatrixFunction
+
+                    # If allocated before, deallocate. This occurs then the
+                    # number of gpu devices changes and gpu_Aop needs to be
+                    # reallocated.
+                    if self.gpu_Aop is not None:
+                        del self.gpu_Aop
+
+                    self.gpu_Aop = pycuAffineMatrixFunction(self.A, self.B,
+                                                            num_gpu_devices)
+                    self.initialized_on_gpu = True
+
+        else:
+
+            if not self.initialized_on_cpu:
+
+                # Create a linear operator on CPU
+                from .._c_linear_operator import pycAffineMatrixFunction
+                self.cpu_Aop = pycAffineMatrixFunction(self.A, self.B)
+                self.initialized_on_cpu = True
+
+        self.gpu = gpu
+        self.num_gpu_devices = num_gpu_devices
+
+    # ============
+    # get num rows
+    # ============
+
+    def get_num_rows(self):
+        """
+        """
+
+        return self.A.shape[0]
+
+    # ===============
+    # get num columns
+    # ===============
+
+    def get_num_columns(self):
+        """
+        """
+
+        return self.A.shape[1]
+
+    # =========
+    # is sparse
+    # =========
+
+    def is_sparse(self):
+        """
+        """
+
+        return isspmatrix(self.A)
+
+    # =======
+    # get nnz
+    # =======
+
+    def get_nnz(self):
+        """
+        Returns the number of non-zero elements of a matrix.
+        """
+
+        if self.is_sparse():
+            return self.A.nnz
+        else:
+            return self.get_num_rows() * self.get_num_columns()
+
+    # ===========
+    # get density
+    # ===========
+
+    def get_density(self):
+        """
+        Returns the density of non-zero elements of a matrix.
+        """
+
+        if self.is_sparse():
+            return self.get_nnz() / \
+                (self.get_num_rows() * self.get_num_columns())
+        else:
+            return 1.0
---
+++ packages/imate/imate-0.10.5-cp36-cp36m-win_amd64.whl/imate/logdet/_cholesky_method.py
@@ -0,0 +1,226 @@
+# SPDX-FileCopyrightText: Copyright 2021, Siavash Ameli <sameli@berkeley.edu>
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileType: SOURCE
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the license found in the LICENSE.txt file in the root directory
+# of this source tree.
+
+
+# =======
+# Imports
+# =======
+
+import time
+import numpy
+import scipy
+import scipy.linalg
+from scipy.sparse import isspmatrix
+import multiprocessing
+from .._linear_algebra.matrix_utilities import get_data_type_name, get_nnz, \
+        get_density
+
+try:
+    from sksparse.cholmod import cholesky as sk_cholesky
+    suitesparse_installed = True
+except ImportError:
+    suitesparse_installed = False
+
+from .._linear_algebra import sparse_cholesky
+from ..__version__ import __version__
+
+
+# ===============
+# cholesky method
+# ===============
+
+def cholesky_method(
+        A,
+        gram=False,
+        exponent=1.0,
+        cholmod=None):
+    """
+    Computes log-determinant using Cholesky decomposition.
+
+    This function is essentially a wrapper for the Choleksy function of the
+    scipy and scikit-sparse packages and primarily used for testing and
+    comparison (benchmarking)  against the randomized methods that are
+    implemented in this package.
+
+    :param A: A full-rank matrix.
+    :type A: numpy.ndarray
+
+    The log-determinant is computed from the Cholesky decomposition
+    :math:`\\mathbf{A} = \\mathbf{L} \\mathbf{L}^{\\intercal}` as
+
+    .. math::
+
+        \\log | \\mathbf{A} | =
+        2 \\mathrm{trace}( \\log \\mathrm{diag}(\\mathbf{L})).
+
+    .. note::
+        This function uses the `Suite Sparse
+        <https://people.engr.tamu.edu/davis/suitesparse.html>`_
+        package to compute the Cholesky decomposition. See the
+        :ref:`installation <InstallScikitSparse>`.
+
+    The result is exact (no approximation) and should be used as benchmark to
+    test other methods.
+
+    :param A: A square matrix
+    :type A: numpy.ndarray
+
+    :param exponent: Exponent :math:`p` in :math:`\\mathbf{A}^{p}`.
+    :param exponent: float
+
+    :param cholmod: If set to ``True``, it uses cholmod library from
+        scikit-sparse package to compute the Chlesky decomposition. If set to
+        ``False``, it uses `scipy.sparse.cholesky`` method. If set to ``None``,
+        first, it tries to use cholmod library,  but if cholmod is not
+        available, it uses ``scipy.sparse.cholesky`` method without raising any
+        warning.
+
+    :return: log-determinant of matrix ``A``.
+    :rtype: float
+    """
+
+    # Check input arguments
+    check_arguments(A, gram, exponent, cholmod)
+
+    # Determine to use Sparse
+    sparse = False
+    if scipy.sparse.isspmatrix(A):
+        sparse = True
+
+    # Determine to use suitesparse or scipy.sparse to compute cholesky
+    if suitesparse_installed and cholmod is not False and sparse:
+        use_cholmod = True
+    else:
+        use_cholmod = False
+
+    init_tot_wall_time = time.perf_counter()
+    init_cpu_proc_time = time.process_time()
+
+    if exponent == 0:
+
+        # determinant is 1. Logdet is zero
+        trace = 0.0
+
+    else:
+
+        # Compute logdet of A without the exponent
+        if sparse:
+
+            # Sparse matrix
+            if use_cholmod:
+                # Use Suite Sparse
+                Factor = sk_cholesky(A)
+                trace = Factor.logdet()
+            else:
+                # Use scipy
+                diag_L = sparse_cholesky(
+                        A, diagonal_only=True).astype(numpy.complex128)
+                logdet_L = numpy.real(numpy.sum(numpy.log(diag_L)))
+                trace = 2.0*logdet_L
+
+        else:
+
+            # Dense matrix. Use scipy
+            L = scipy.linalg.cholesky(A, lower=True)
+            diag_L = numpy.diag(L).astype(numpy.complex128)
+            logdet_L = numpy.real(numpy.sum(numpy.log(diag_L)))
+            trace = 2.0*logdet_L
+
+        # Taking into account of the exponent
+        trace = trace*exponent
+
+    # Gramian matrix
+    if gram:
+        trace = 2.0 * trace
+
+    tot_wall_time = time.perf_counter() - init_tot_wall_time
+    cpu_proc_time = time.process_time() - init_cpu_proc_time
+
+    # Dictionary of output info
+    info = {
+        'matrix':
+        {
+            'data_type': get_data_type_name(A),
+            'gram': gram,
+            'exponent': exponent,
+            'size': A.shape[0],
+            'sparse': isspmatrix(A),
+            'nnz': get_nnz(A),
+            'density': get_density(A),
+            'num_inquiries': 1
+        },
+        'device':
+        {
+            'num_cpu_threads': multiprocessing.cpu_count(),
+            'num_gpu_devices': 0,
+            'num_gpu_multiprocessors': 0,
+            'num_gpu_threads_per_multiprocessor': 0
+        },
+        'time':
+        {
+            'tot_wall_time': tot_wall_time,
+            'alg_wall_time': tot_wall_time,
+            'cpu_proc_time': cpu_proc_time,
+        },
+        'solver':
+        {
+            'version': __version__,
+            'method': 'cholesky',
+            'cholmod_used': use_cholmod
+        }
+    }
+
+    return trace, info
+
+
+# ===============
+# check arguments
+# ===============
+
+def check_arguments(
+        A,
+        gram,
+        exponent,
+        cholmod):
+    """
+    Checks the type and value of the parameters.
+    """
+
+    # Check A
+    if (not isinstance(A, numpy.ndarray)) and (not scipy.sparse.issparse(A)):
+        raise TypeError('Input matrix should be either a "numpy.ndarray" or ' +
+                        'a "scipy.sparse" matrix.')
+    elif A.shape[0] != A.shape[1]:
+        raise ValueError('Input matrix should be a square matrix.')
+
+    # Check gram
+    if gram is None:
+        raise TypeError('"gram" cannot be None.')
+    elif not numpy.isscalar(gram):
+        raise TypeError('"gram" should be a scalar value.')
+    elif not isinstance(gram, bool):
+        raise TypeError('"gram" should be boolean.')
+
+    # Check exponent
+    if exponent is None:
+        raise TypeError('"exponent" cannot be None.')
+    elif not numpy.isscalar(exponent):
+        raise TypeError('"exponent" should be a scalar value.')
+    elif isinstance(exponent, complex):
+        TypeError('"exponent" cannot be an integer or a float number.')
+
+    # Check cholmod
+    if cholmod is not None:
+        if not isinstance(cholmod, bool):
+            raise TypeError('"cholmod" should be either "None", or boolean.')
+        elif cholmod is True and suitesparse_installed is False:
+            print(cholmod)
+            print(suitesparse_installed)
+            raise RuntimeError('"cholmod" method is not available. Either ' +
+                               'install "scikit-sparse" package, or set ' +
+                               '"cholmod" to "False" or "None".')
