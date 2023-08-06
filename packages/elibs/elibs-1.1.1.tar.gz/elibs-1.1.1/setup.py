ed61b552dcd26b7a74d4561c9bb7e9ba37739b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:21 +0000

    Add package elibs/1.1.1/elibs-1.1.1.tar.gz

---
+++ packages/elibs/elibs-1.1.1.tar.gz/elibs-1.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='elibs',
+    version='1.1.1',
+    packages=['elibs', 'elibs.utils', 'elibs.tom_ticket', 'elibs.tom_ticket.models'],
+    url='',
+    license='',
+    author='ellitedev',
+    author_email='osni@sotech.xyz',
+    description='Biblioteca de integração com o TomTicket'
+)
