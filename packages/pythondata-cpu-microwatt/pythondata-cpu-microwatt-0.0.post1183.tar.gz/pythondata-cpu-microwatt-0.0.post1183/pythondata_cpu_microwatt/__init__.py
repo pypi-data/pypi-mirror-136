29039d631392d52252494bbbd6103c62c598fde4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:56 +0000

    Add package pythondata-cpu-microwatt/0.0.post1183/pythondata-cpu-microwatt-0.0.post1183.tar.gz

---
+++ packages/pythondata-cpu-microwatt/pythondata-cpu-microwatt-0.0.post1183.tar.gz/pythondata-cpu-microwatt-0.0.post1183/pythondata_cpu_microwatt/__init__.py
@@ -0,0 +1,55 @@
+import os.path
+__dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
+data_location = os.path.join(__dir__, "vhdl")
+src = "https://github.com/antonblanchard/microwatt"
+
+# Module version
+version_str = "0.0.post1183"
+version_tuple = (0, 0, 1183)
+try:
+    from packaging.version import Version as V
+    pversion = V("0.0.post1183")
+except ImportError:
+    pass
+
+# Data version info
+data_version_str = "0.0.post1059"
+data_version_tuple = (0, 0, 1059)
+try:
+    from packaging.version import Version as V
+    pdata_version = V("0.0.post1059")
+except ImportError:
+    pass
+data_git_hash = "6ff3b2499ceaf911252f634d44469fefb58952ae"
+data_git_describe = "v0.0-1059-g6ff3b24"
+data_git_msg = """\
+commit 6ff3b2499ceaf911252f634d44469fefb58952ae
+Merge: fda8879 cdd661d
+Author: Michael Neuling <mikey@neuling.org>
+Date:   Tue Jan 18 13:27:27 2022 +1100
+
+    Merge pull request #342 from mkj/orangecrab-merge
+    
+    Orangecrab working with litedram
+    
+    Fixed up a few simple merge conflicts in the Makefile.
+
+"""
+
+# Tool version info
+tool_version_str = "0.0.post124"
+tool_version_tuple = (0, 0, 124)
+try:
+    from packaging.version import Version as V
+    ptool_version = V("0.0.post124")
+except ImportError:
+    pass
+
+
+def data_file(f):
+    """Get absolute path for file inside pythondata_cpu_microwatt."""
+    fn = os.path.join(data_location, f)
+    fn = os.path.abspath(fn)
+    if not os.path.exists(fn):
+        raise IOError("File {f} doesn't exist in pythondata_cpu_microwatt".format(f))
+    return fn
