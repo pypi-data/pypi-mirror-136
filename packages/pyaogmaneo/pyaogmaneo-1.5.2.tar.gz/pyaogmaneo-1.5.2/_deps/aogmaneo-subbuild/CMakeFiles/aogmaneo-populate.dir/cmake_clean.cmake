25c1049bf1499354aae9126091c6262844e51fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:06 +0000

    Add package pyaogmaneo/1.5.2/pyaogmaneo-1.5.2.tar.gz

---
+++ packages/pyaogmaneo/pyaogmaneo-1.5.2.tar.gz/pyaogmaneo-1.5.2/_deps/aogmaneo-subbuild/CMakeFiles/aogmaneo-populate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aogmaneo-populate"
+  "CMakeFiles/aogmaneo-populate-complete"
+  "aogmaneo-populate-prefix/src/aogmaneo-populate-stamp/aogmaneo-populate-build"
+  "aogmaneo-populate-prefix/src/aogmaneo-populate-stamp/aogmaneo-populate-configure"
+  "aogmaneo-populate-prefix/src/aogmaneo-populate-stamp/aogmaneo-populate-download"
+  "aogmaneo-populate-prefix/src/aogmaneo-populate-stamp/aogmaneo-populate-install"
+  "aogmaneo-populate-prefix/src/aogmaneo-populate-stamp/aogmaneo-populate-mkdir"
+  "aogmaneo-populate-prefix/src/aogmaneo-populate-stamp/aogmaneo-populate-patch"
+  "aogmaneo-populate-prefix/src/aogmaneo-populate-stamp/aogmaneo-populate-test"
+  "aogmaneo-populate-prefix/src/aogmaneo-populate-stamp/aogmaneo-populate-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/aogmaneo-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
