7901733c30bc4fec9e51ea30f2aa8b2b15ff6778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:25 +0000

    Add package Lateksii-invoicegen/2.1.1/Lateksii-invoicegen-2.1.1.tar.gz

---
+++ packages/Lateksii-invoicegen/Lateksii-invoicegen-2.1.1.tar.gz/Lateksii-invoicegen-2.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Lateksii-invoicegen',
+    version='2.1.1',
+    packages = find_packages(),
+    data_files = ['lateksii-white-bg.png','template.tex'],
+    author='antto',)
