99528e9eebd80490e595698f15f317856b3a9d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:43 +0000

    Add package mcda/0.3.1/mcda-0.3.1.tar.gz

---
+++ packages/mcda/mcda-0.3.1.tar.gz/mcda-0.3.1/mcda/core/__init__.py
@@ -0,0 +1,15 @@
+"""This subpackage contains all core functionalities, especially data
+structures.
+
+
+References
+----------
+
+.. [2] Grabisch, M., Kojadinovic, I., & Meyer, P. (2008).
+   A review of methods for capacity identification in Choquet integral based multi-attribute utility theory: Applications of the Kappalab R package.
+   European journal of operational research, 186(2), 766-785.
+
+.. [3] Meyer, P., Bigaret, S., Hodgett, R., & Olteanu A-L. (2021).
+   MCDA: Support for the Multicriteria Decision Aiding Process.
+   R package version 0.0.21. https://github.com/paterijk/MCDA.
+"""  # noqa: E501
---
+++ packages/mcda/mcda-0.3.1.tar.gz/mcda-0.3.1/mcda/mavt/aggregators.py
@@ -0,0 +1,102 @@
+__all__ = [
+    "weighted_sum",
+    "choquet_integral_capacity",
+    "choquet_integral_mobius",
+]
+
+from typing import List, cast
+
+from ..core.aliases import (
+    NumericPerformanceTable,
+    NumericValue,
+    PerformanceTable,
+)
+from ..core.performance_table import (
+    apply_criteria_weights,
+    normalize,
+    sum_table,
+)
+from ..core.scales import Scale
+from ..core.sets import difference, index_to_set
+from ..core.sorting import sort_elements_by_values
+
+
+def normalized_weighted_sum(
+    performance_table: NumericPerformanceTable,
+    criteria_weights: List[NumericValue],
+) -> List[NumericValue]:
+    """Compute alternatives values as weighted sum of normalized alternatives'
+    performances.
+
+    :param performance_table:
+    :param criteria_weights:
+    :return: alternatives values
+    """
+    weighted_table = cast(
+        PerformanceTable,
+        apply_criteria_weights(performance_table, criteria_weights),
+    )
+    res = sum_table(weighted_table, axis=1)
+    res = cast(List[NumericValue], res)
+    return res
+
+
+def weighted_sum(
+    performance_table: PerformanceTable,
+    criteria_scales: List[Scale],
+    criteria_weights: List[NumericValue],
+) -> List[NumericValue]:
+    """Compute alternatives values as weighted sum of alternatives'
+    performances.
+
+    :param performance_table:
+    :param criteria_scales:
+    :param criteria_weights:
+    :return: alternatives values
+    """
+    normalized_table = normalize(performance_table, criteria_scales)
+    weighted_table = cast(
+        PerformanceTable,
+        apply_criteria_weights(normalized_table, criteria_weights),
+    )
+    res = sum_table(weighted_table, axis=1)
+    res = cast(List[NumericValue], res)
+    return res
+
+
+def choquet_integral_capacity(
+    values: List[NumericValue], capacity: List[NumericValue]
+) -> NumericValue:
+    """Return Choquet integral using a capacity.
+
+    :param values:
+    :param capacity:
+    :return:
+
+    .. note:: Implementation is based on [2]_.
+    """
+    permutation = [*range(len(values))]
+    sort_elements_by_values(values, permutation, reverse=True)
+    index = len(capacity) - 1
+    res = 0
+    for i in permutation:
+        next_index = difference(index, 2 ** i)
+        res += values[i] * (capacity[index] - capacity[next_index])
+        index = next_index
+    return res
+
+
+def choquet_integral_mobius(
+    values: List[NumericValue], mobius: List[NumericValue]
+) -> NumericValue:
+    """Return Choquet integral using a möbius.
+
+    :param values:
+    :param mobius:
+    :return:
+
+    .. note:: Implementation is based on [2]_.
+    """
+    return sum(
+        mobius[t] * min(index_to_set(t, values)) for t in range(1, len(mobius))
+    )
