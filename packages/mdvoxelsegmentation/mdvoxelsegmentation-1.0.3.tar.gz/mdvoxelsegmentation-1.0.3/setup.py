7ca8a5c5edaf1537e834db98bae7bc93e83c5915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:40 +0000

    Add package mdvoxelsegmentation/1.0.3/mdvoxelsegmentation-1.0.3.tar.gz

---
+++ packages/mdvoxelsegmentation/mdvoxelsegmentation-1.0.3.tar.gz/mdvoxelsegmentation-1.0.3/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import setuptools
+
+setuptools.setup(
+    packages=['mdvoxelsegmentation'],
+    entry_points = {
+        'console_scripts' : ['mdvseg=mdvoxelsegmentation.do_segmentaton:main'],
+        },
+)
