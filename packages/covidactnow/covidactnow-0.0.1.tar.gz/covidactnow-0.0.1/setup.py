697c2814a8f3f2202353a9d7006c190cc9e474b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:59 +0000

    Add package covidactnow/0.0.1/covidactnow-0.0.1.tar.gz

---
+++ packages/covidactnow/covidactnow-0.0.1.tar.gz/covidactnow-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A wrapper allowing users to get specific state-based COVID information from the Covid Act Now database.'
+
+# Setting up
+setup(
+    name="covidactnow",
+    version=VERSION,
+    author="Prerit Das",
+    author_email="<preritdas@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
