811c418d22d2f641ed5d698cf65983afd4f910f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:17 +0000

    Add package libvoiced/0.23.0/libvoiced-0.23.0-py3-none-any.whl

---
+++ packages/libvoiced/libvoiced-0.23.0-py3-none-any.whl/libvoiced/putup.py
@@ -0,0 +1,31 @@
+import logging
+import subprocess
+
+
+def putup(project_path):
+    cmd = [
+        "putup",
+        "--pre-commit",
+        "--url",
+        f"https://github.com/TaylorMonacelli/{project_path.name}.git",
+        "--venv",
+        ".venv",
+        f"{project_path.resolve()}",
+    ]
+    logging.debug(cmd)
+
+    proc = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    try:
+        outs, errs = proc.communicate(timeout=15)
+        logging.debug(outs.decode())
+    except subprocess.TimeoutExpired:
+        proc.kill()
+        outs, errs = proc.communicate()
+        logging.warning(errs.decode())
+
+    logging.debug(errs.decode())
