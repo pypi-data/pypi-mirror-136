2a4ef624bcee6c1ebd0acf06214e9ee83a9e9304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:43 +0000

    Add package antistasi_logbook/0.4.1/antistasi_logbook-0.4.1.tar.gz

---
+++ packages/antistasi_logbook/antistasi_logbook-0.4.1.tar.gz/antistasi_logbook-0.4.1/antistasi_logbook/gui/models/log_files_model.py
@@ -0,0 +1,135 @@
+"""
+WiP.
+
+Soon.
+"""
+
+# region [Imports]
+
+# * Standard Library Imports ---------------------------------------------------------------------------->
+from typing import TYPE_CHECKING, Any, Optional
+from pathlib import Path
+from datetime import datetime
+
+# * Third Party Imports --------------------------------------------------------------------------------->
+from peewee import JOIN, Field, Query
+
+# * Qt Imports --------------------------------------------------------------------------------------->
+import PySide6
+from PySide6 import (QtCore, QtGui, QtWidgets, Qt3DAnimation, Qt3DCore, Qt3DExtras, Qt3DInput, Qt3DLogic, Qt3DRender, QtAxContainer, QtBluetooth,
+                     QtCharts, QtConcurrent, QtDataVisualization, QtDesigner, QtHelp, QtMultimedia, QtMultimediaWidgets, QtNetwork, QtNetworkAuth,
+                     QtOpenGL, QtOpenGLWidgets, QtPositioning, QtPrintSupport, QtQml, QtQuick, QtQuickControls2, QtQuickWidgets, QtRemoteObjects,
+                     QtScxml, QtSensors, QtSerialPort, QtSql, QtStateMachine, QtSvg, QtSvgWidgets, QtTest, QtUiTools, QtWebChannel, QtWebEngineCore,
+                     QtWebEngineQuick, QtWebEngineWidgets, QtWebSockets, QtXml)
+
+from PySide6.QtCore import (QByteArray, QCoreApplication, QDate, QDateTime, QEvent, QLocale, QMetaObject, QModelIndex, QModelRoleData, QMutex,
+                            QMutexLocker, QObject, QPoint, QRect, QRecursiveMutex, QRunnable, QSettings, QSize, QThread, QThreadPool, QTime, QUrl,
+                            QWaitCondition, Qt, QAbstractItemModel, QAbstractListModel, QAbstractTableModel, Signal, Slot, QIdentityProxyModel, QSortFilterProxyModel)
+
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QFontMetrics, QGradient, QIcon, QImage,
+                           QKeySequence, QLinearGradient, QPainter, QPalette, QPixmap, QRadialGradient, QTransform)
+
+from PySide6.QtWidgets import (QApplication, QBoxLayout, QCheckBox, QColorDialog, QColumnView, QComboBox, QDateTimeEdit, QDialogButtonBox,
+                               QDockWidget, QDoubleSpinBox, QFontComboBox, QFormLayout, QFrame, QGridLayout, QGroupBox, QHBoxLayout, QHeaderView,
+                               QLCDNumber, QLabel, QLayout, QLineEdit, QListView, QListWidget, QMainWindow, QMenu, QMenuBar, QMessageBox,
+                               QProgressBar, QProgressDialog, QPushButton, QSizePolicy, QSpacerItem, QSpinBox, QStackedLayout, QStackedWidget,
+                               QStatusBar, QStyledItemDelegate, QSystemTrayIcon, QTabWidget, QTableView, QTextEdit, QTimeEdit, QToolBox, QTreeView,
+                               QVBoxLayout, QWidget, QAbstractItemDelegate, QAbstractItemView, QAbstractScrollArea, QRadioButton, QFileDialog, QButtonGroup)
+
+
+# * Gid Imports ----------------------------------------------------------------------------------------->
+from gidapptools import get_logger
+
+# * Local Imports --------------------------------------------------------------------------------------->
+from antistasi_logbook.storage.models.models import Server, GameMap, LogFile
+from antistasi_logbook.storage.models.custom_fields import FakeField
+from antistasi_logbook.gui.models.base_query_data_model import INDEX_TYPE, BaseQueryDataModel, ModelContextMenuAction
+from antistasi_logbook.gui.models.proxy_models.base_proxy_model import BaseProxyModel
+# * Type-Checking Imports --------------------------------------------------------------------------------->
+if TYPE_CHECKING:
+    from antistasi_logbook.storage.models.models import BaseModel
+    from antistasi_logbook.gui.views.base_query_tree_view import CustomContextMenu
+# endregion[Imports]
+
+# region [TODO]
+
+
+# endregion [TODO]
+
+# region [Logging]
+
+
+# endregion[Logging]
+
+# region [Constants]
+
+THIS_FILE_DIR = Path(__file__).parent.absolute()
+log = get_logger(__name__)
+# endregion[Constants]
+
+
+class LogFilesModel(BaseQueryDataModel):
+    extra_columns = {FakeField(name="amount_log_records", verbose_name="Records"),
+                     FakeField("time_frame", "Time Frame"),
+                     FakeField(name="amount_errors", verbose_name="Errors"),
+                     FakeField(name="amount_warnings", verbose_name="Warnings")}
+    strict_exclude_columns = {"startup_text", "remote_path", "header_text"}
+
+    def __init__(self, parent: Optional[QtCore.QObject] = None, show_unparsable: bool = False) -> None:
+        self.show_unparsable = show_unparsable
+        super().__init__(LogFile, parent=parent)
+        self.ordered_by = (-LogFile.modified_at, LogFile.server)
+        self.filter_item = None
+
+    def change_show_unparsable(self, show_unparsable):
+        if show_unparsable and self.show_unparsable is False:
+            self.show_unparsable = True
+            self.refresh()
+
+        elif not show_unparsable and self.show_unparsable is True:
+            self.show_unparsable = False
+            self.refresh()
+
+    def on_display_data_bool(self, role: int, item: "BaseModel", column: "Field", value: bool) -> str:
+        if role == Qt.DisplayRole:
+            if column.name in {"is_new_campaign"}:
+                return ''
+
+            return super().on_display_data_bool(role=role, item=item, column=column, value=value)
+        if role == Qt.DecorationRole:
+            if column.name in {"is_new_campaign"}:
+                return self.bool_images[True] if value is True else None
+
+            return super().on_display_data_bool(role=role, item=item, column=column, value=value)
+
+    def get_query(self) -> "Query":
+        query = LogFile.select().join(GameMap, join_type=JOIN.LEFT_OUTER).switch(LogFile).join(Server).switch(LogFile)
+        if self.show_unparsable is False:
+            query = query.where(LogFile.unparsable == False)
+        if self.filter_item is not None:
+            query = query.where(self.filter_item)
+        return query.order_by(*self.ordered_by)
+
+    def get_content(self) -> "BaseQueryDataModel":
+        with self.backend.database:
+            self.content_items = list(self.get_query().execute())
+
+        return self
+
+    def _get_tool_tip_data(self, index: INDEX_TYPE) -> Any:
+        item = self.content_items[index.row()]
+        column = self.columns[index.column()]
+        if column.name == "marked":
+            if item.marked is True:
+                return "This Log-File is marked"
+            else:
+                return "This Log-File is not marked"
+
+        return super()._get_tool_tip_data(index)
+# region[Main_Exec]
+
+
+if __name__ == '__main__':
+    pass
+
+# endregion[Main_Exec]
