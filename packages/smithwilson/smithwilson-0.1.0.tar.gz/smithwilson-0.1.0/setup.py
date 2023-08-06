fbd0a7d7e97e5384cd119ccaf1184d4f4f1421c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:51 +0000

    Add package smithwilson/0.1.0/smithwilson-0.1.0.tar.gz

---
+++ packages/smithwilson/smithwilson-0.1.0.tar.gz/smithwilson-0.1.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="smithwilson",
+    version="0.1.0",
+    author="Dejan Simic",
+    # author_email="dejan.simic",
+    description=
+    "Implementation of the Smith-Wilson yield curve fitting algorithm in Python for interpolations and extrapolations of zero-coupon bond rates",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/simicd/smith-wilson-py.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=["numpy>=1.21.5"],
+)
