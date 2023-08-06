f795c89380727b926b5b7327fa3f4acddaf45830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:56 +0000

    Add package proxy.py/2.4.0rc9/proxy.py-2.4.0rc9.tar.gz

---
+++ packages/proxy.py/proxy.py-2.4.0rc9.tar.gz/proxy.py-2.4.0rc9/examples/tcp_echo_client.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+    proxy.py
+    ~~~~~~~~
+    ⚡⚡⚡ Fast, Lightweight, Pluggable, TLS interception capable proxy server focused on
+    Network monitoring, controls & Application development, testing, debugging.
+
+    :copyright: (c) 2013-present by Abhinav Singh and contributors.
+    :license: BSD, see LICENSE for more details.
+"""
+import logging
+
+from proxy.common.utils import socket_connection
+from proxy.common.constants import DEFAULT_BUFFER_SIZE
+
+
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    with socket_connection(('::', 12345)) as client:
+        while True:
+            client.send(b'hello')
+            data = client.recv(DEFAULT_BUFFER_SIZE)
+            if data is None:
+                break
+            logger.info(data)
---
+++ packages/proxy.py/proxy.py-2.4.0rc9.tar.gz/proxy.py-2.4.0rc9/proxy/plugin/modify_post_data.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+    proxy.py
+    ~~~~~~~~
+    ⚡⚡⚡ Fast, Lightweight, Pluggable, TLS interception capable proxy server focused on
+    Network monitoring, controls & Application development, testing, debugging.
+
+    :copyright: (c) 2013-present by Abhinav Singh and contributors.
+    :license: BSD, see LICENSE for more details.
+"""
+from typing import Optional
+
+from ..http import httpMethods
+from ..http.proxy import HttpProxyBasePlugin
+from ..http.parser import HttpParser, ChunkParser
+from ..common.utils import bytes_
+
+
+class ModifyPostDataPlugin(HttpProxyBasePlugin):
+    """Modify POST request body before sending to upstream server."""
+
+    MODIFIED_BODY = b'{"key": "modified"}'
+
+    def before_upstream_connection(
+            self, request: HttpParser,
+    ) -> Optional[HttpParser]:
+        return request
+
+    def handle_client_request(
+            self, request: HttpParser,
+    ) -> Optional[HttpParser]:
+        if request.method == httpMethods.POST:
+            # If request data is compressed, compress the body too
+            body = ModifyPostDataPlugin.MODIFIED_BODY
+            # If the request is of type chunked encoding
+            # add post data as chunk
+            if request.is_chunked_encoded:
+                body = ChunkParser.to_chunks(
+                    ModifyPostDataPlugin.MODIFIED_BODY,
+                )
+            else:
+                request.add_header(
+                    b'Content-Length',
+                    bytes_(len(body)),
+                )
+            request.body = body
+            # Enforce content-type json
+            if request.has_header(b'Content-Type'):
+                request.del_header(b'Content-Type')
+            request.add_header(b'Content-Type', b'application/json')
+        return request
