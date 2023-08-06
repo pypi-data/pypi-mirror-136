d320dd9683d5bc430bfb586d427042ff561d5971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:02 +0000

    Add package package-gen/0.0.2/package-gen-0.0.2.tar.gz

---
+++ packages/package-gen/package-gen-0.0.2.tar.gz/package-gen-0.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="package-gen",
+    version="0.0.2",
+    author="Zhang",
+    author_email="",
+    url = 'https://github.com/suifengpiaoyang/pk-gen',
+    description="Generate python package",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "pk-gen=packagegenerator:main",
+        ]
+    },
+    python_requires='>=3.6.0'
+)
