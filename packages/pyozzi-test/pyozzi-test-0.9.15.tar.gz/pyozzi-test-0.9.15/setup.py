92de35a57bda2fe283794ac37f45169ef13a0822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:01 +0000

    Add package pyozzi-test/0.9.15/pyozzi-test-0.9.15.tar.gz

---
+++ packages/pyozzi-test/pyozzi-test-0.9.15.tar.gz/pyozzi-test-0.9.15/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import os
+
+print("[*] PYOZZI PRINT")
+
+with open("README.md","r") as f:
+    longDesc = f.read()
+
+setuptools.setup(
+    name="pyozzi-test",
+    version="0.9.15",
+    author="pyozzi",
+    author_email="pyozzi@toss.im",
+    description="test library",
+    long_description=longDesc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pyozzi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.6"
+)
