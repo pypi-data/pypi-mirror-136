f027bc1ad0f66249214cbd053902073569d7d5da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:12 +0000

    Add package rcounting/0.4.2.dev0/rcounting-0.4.2.dev0-py3-none-any.whl

---
+++ packages/rcounting/rcounting-0.4.2.dev0-py3-none-any.whl/rcounting/scripts/cli.py
@@ -0,0 +1,22 @@
+"""
+The main entry point for the command line interface.
+See the subcommands for details on their behaviour.
+"""
+import click
+
+from .log_thread import log
+from .update_thread_directory import update_directory
+from .validate import validate
+
+
+@click.group(
+    commands=[log, validate, update_directory],
+    context_settings=dict(help_option_names=["-h", "--help"]),
+)
+@click.version_option()
+def cli():
+    pass
+
+
+if __name__ == "__main__":
+    cli()  # pylint: disable=no-value-for-parameter
