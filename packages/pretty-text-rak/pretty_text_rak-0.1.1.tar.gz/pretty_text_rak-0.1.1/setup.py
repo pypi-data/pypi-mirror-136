6fc61a7394acf832a7af8e8b674ee058912fb34a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:08 +0000

    Add package pretty-text-rak/0.1.1/pretty_text_rak-0.1.1.tar.gz

---
+++ packages/pretty-text-rak/pretty_text_rak-0.1.1.tar.gz/pretty_text_rak-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pretty_text_rak']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyfiglet>=0.8.post1,<0.9', 'termcolor>=1.1.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pretty-text-rak',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Rakesh Singh',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
