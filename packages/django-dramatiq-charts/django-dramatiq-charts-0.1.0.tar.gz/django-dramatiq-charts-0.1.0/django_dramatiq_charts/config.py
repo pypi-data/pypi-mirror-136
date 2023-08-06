88cee058086e4c45c5c651eaff6301ce7e8ed88c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:12 +0000

    Add package django-dramatiq-charts/0.1.0/django-dramatiq-charts-0.1.0.tar.gz

---
+++ packages/django-dramatiq-charts/django-dramatiq-charts-0.1.0.tar.gz/django-dramatiq-charts-0.1.0/django_dramatiq_charts/config.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+
+
+def _has_charts_perm_fn_default(request):
+    return request.user.is_superuser
+
+
+def get_perm_fn():
+    return getattr(settings, "DJANGO_DRAMATIQ_CHARTS_PERM_FN", _has_charts_perm_fn_default)
+
+
+def get_load_chart_qs():
+    return getattr(settings, "DJANGO_DRAMATIQ_CHARTS_LOAD_CHART_QS", None)
+
+
+def get_timeline_chart_qs():
+    return getattr(settings, "DJANGO_DRAMATIQ_CHARTS_TIMELINE_CHART_QS", None)
+
+
+def get_cache_form_data_sec():
+    return getattr(settings, "DJANGO_DRAMATIQ_CHARTS_CACHE_FORM_DATA_SEC", 60 * 60 * 4)
