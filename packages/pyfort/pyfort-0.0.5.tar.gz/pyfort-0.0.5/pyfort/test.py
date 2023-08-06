759af1e59b4e2b21bfc1b976e580e476ed3d9f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:22 +0000

    Add package pyfort/0.0.5/pyfort-0.0.5.tar.gz

---
+++ packages/pyfort/pyfort-0.0.5.tar.gz/pyfort-0.0.5/pyfort/test.py
@@ -0,0 +1,13 @@
+'''from save_retrive import *
+
+#search_display_database('asd')
+
+result = show('asd', 'EREW')
+for _ in result:
+    print('\n','User name:',_[0])
+    print(' password :',_[1])
+'''
+import getpass
+
+passwoed = getpass.getpass('password:')
+print(passwoed)
---
+++ packages/pyfort/pyfort-0.0.5.tar.gz/pyfort-0.0.5/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.5'
+DESCRIPTION = 'command-line password manager'
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+]
+
+# Setting up
+setup(
+    name="pyfort",
+    version=VERSION,
+    author="Atharva Bhandvalkar",
+    author_email="<atharv.bhandvalkar@gmail.com>",
+    license='MIT',
+    url='https://github.com/a-tharva/fort',
+    description=DESCRIPTION,
+    long_description=long_description + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=['colorama', 'termcolor', 'cryptography'],
+    keywords=['python'],
+    entry_points={
+        'console_scripts': [
+            'fort = pyfort.pyfort:main',
+        ],
+    },
+    classifiers=classifiers,
+)
