f9c495b66ebdab419e0eeb4da56f964b2f889173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:37 +0000

    Add package corpengine1/1.2/corpengine1-1.2.tar.gz

---
+++ packages/corpengine1/corpengine1-1.2.tar.gz/corpengine1-1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+changelog = 'v1.2\n\n-Some object & service naming fixes\n-Added back the version module\n-new colors methods: all, onlyFill, onlyEmpty\n-Fixed color range checking in colors.mix()\n-Optional Rectangle support for isCollidingWithMouse()\n-new setIcon() method'
+
+setup(
+    name='corpengine1',
+    version='1.2',
+    description='Fast, Organized, Object-Oriented Python Game Development',
+    long_description='github repository: https://github.com/corpengine/corpengine' + '\n\nChangelog:\n'+changelog,
+    url='https://corpengine.github.io/',
+    author='CORPEngine Organization + Contributors',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='corpengine',
+    packages=find_packages(),
+    install_requires=['easygui', 'pygame']
+)
