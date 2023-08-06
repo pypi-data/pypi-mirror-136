eeaa20f6e4569a4c724bf525128b1a624029d267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:43 +0000

    Add package Rahimcalc/0.3.6/Rahimcalc-0.3.6.tar.gz

---
+++ packages/Rahimcalc/Rahimcalc-0.3.6.tar.gz/Rahimcalc-0.3.6/Rahimcalc/comment.py
@@ -0,0 +1,20 @@
+import Rahimcalc as r
+import sys
+def main():
+    a=sys.argv[1]
+    b=sys.argv[2]
+    c=sys.argv[3]
+    if a=='sum':
+        r.sum(b,c)
+    if a=='sub':
+        r.sub(b,c)
+    if a=='mul':
+        r.mul(b,c)
+    if a=='div':
+        r.div(b,c)
+    if a=='exp':
+        r.exp(b,c)
+    if a=='complex':
+        r.complex(b,c)
+if __name__=='__main__':
+    main()
