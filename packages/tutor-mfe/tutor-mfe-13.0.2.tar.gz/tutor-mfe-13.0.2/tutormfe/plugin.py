708ca24fc40817f8208201280c9f8f8846c44af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:05 +0000

    Add package tutor-mfe/13.0.2/tutor-mfe-13.0.2.tar.gz

---
+++ packages/tutor-mfe/tutor-mfe-13.0.2.tar.gz/tutor-mfe-13.0.2/tutormfe/plugin.py
@@ -0,0 +1,64 @@
+from glob import glob
+import os
+
+from .__about__ import __version__
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+templates = os.path.join(HERE, "templates")
+
+config = {
+    "defaults": {
+        "VERSION": __version__,
+        "DOCKER_IMAGE": "{{ DOCKER_REGISTRY }}overhangio/openedx-mfe:{{ MFE_VERSION }}",
+        "HOST": "apps.{{ LMS_HOST }}",
+        "COMMON_VERSION": "{{ OPENEDX_COMMON_VERSION }}",
+        "CADDY_DOCKER_IMAGE": "{{ DOCKER_IMAGE_CADDY }}",
+        "ACCOUNT_MFE_APP": {
+            "name": "account",
+            "repository": "https://github.com/edx/frontend-app-account",
+            "port": 1997,
+            "env": {
+                "production": {
+                    "COACHING_ENABLED": "",
+                    "ENABLE_DEMOGRAPHICS_COLLECTION": "",
+                },
+            },
+        },
+        "GRADEBOOK_MFE_APP": {
+            "name": "gradebook",
+            "repository": "https://github.com/edx/frontend-app-gradebook",
+            "port": 1994,
+        },
+        "LEARNING_MFE_APP": {
+            "name": "learning",
+            "repository": "https://github.com/edx/frontend-app-learning",
+            "port": 2000,
+        },
+        "PROFILE_MFE_APP": {
+            "name": "profile",
+            "repository": "https://github.com/edx/frontend-app-profile",
+            "port": 1995,
+        },
+    },
+}
+
+hooks = {
+    "build-image": {
+        "mfe": "{{ MFE_DOCKER_IMAGE }}",
+    },
+    "remote-image": {
+        "mfe": "{{ MFE_DOCKER_IMAGE }}",
+    },
+    "init": ["lms"],
+}
+
+
+def patches():
+    all_patches = {}
+    for path in glob(os.path.join(HERE, "patches", "*")):
+        with open(path) as patch_file:
+            name = os.path.basename(path)
+            content = patch_file.read()
+            all_patches[name] = content
+    return all_patches
