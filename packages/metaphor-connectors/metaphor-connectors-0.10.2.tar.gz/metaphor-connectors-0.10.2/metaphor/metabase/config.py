5a3e77fee2f53b6ea1df1df513b62a9465653f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:08 +0000

    Add package metaphor-connectors/0.10.2/metaphor-connectors-0.10.2.tar.gz

---
+++ packages/metaphor-connectors/metaphor-connectors-0.10.2.tar.gz/metaphor-connectors-0.10.2/metaphor/metabase/config.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+from serde import deserialize
+
+from metaphor.common.base_config import BaseConfig
+
+
+@deserialize
+@dataclass
+class MetabaseRunConfig(BaseConfig):
+    server_url: str
+    username: str
+    password: str
