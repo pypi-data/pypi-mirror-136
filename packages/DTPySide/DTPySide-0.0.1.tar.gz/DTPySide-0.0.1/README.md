0bd40d135147152836828f3700f4be88a68a24c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:02 +0000

    Add package DTPySide/0.0.1/DTPySide-0.0.1.tar.gz

---
+++ packages/DTPySide/DTPySide-0.0.1.tar.gz/DTPySide-0.0.1/README.md
@@ -0,0 +1,11 @@
+# DongliTeahousePySideWheel
+
+## Try Demo
+
+Put the whole folder into your "site-packages" and run python:
+
+```python
+from DTPySide.demo import main
+
+main.run()
+```
---
+++ packages/DTPySide/DTPySide-0.0.1.tar.gz/DTPySide-0.0.1/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from DTPySide.DTFunction import *
+from DTPySide.DTStyleSheet import DTStyleSheet
+
+import DTPySide.DTIcon as DTIcon
+
+import DTPySide.DTWidget as DTWidget
+import DTPySide.DTFrame as DTFrame
+import DTPySide.DTModule as DTModule
+import DTPySide.DTTranslation as DTTranslation
+
+import DTPySide.DTSession as DTSession
+from DTPySide.DTAPP import DTAPP
