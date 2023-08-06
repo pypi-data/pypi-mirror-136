6257ad3e11ef313555502354df7ce0af013a510c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:10 +0000

    Add package demo-tools/0.0.2/demo_tools-0.0.2-py3-none-any.whl

---
+++ packages/demo-tools/demo_tools-0.0.2-py3-none-any.whl/demotools/__init__.py
@@ -0,0 +1,5 @@
+from .cast import int_char_int
+
+__all__ = [
+    'int_char_int',
+]
---
+++ packages/demo-tools/demo_tools-0.0.2-py3-none-any.whl/demotools/cast/pointer_cast.py
@@ -0,0 +1,16 @@
+def int_char_int(val1, val2, val3):
+    s1 = bin(val1)
+    s1 = (32 - len(s1)) * '0' + s1
+    s1 = s1[::-1]
+    s2 = bin(val2)
+    s2 = (32 - len(s2)) * '0' + s2
+    s2 = s2[::-1]
+    s3 = bin(val3)
+    s3 = (32 - len(s3)) * '0' + s3
+    s3 = s3[::-1]
+    s4 = s1[:8] + s3 + s2[8:]
+    s5 = s4[:32]
+    s6 = s4[32:]
+    r1 = int(s5[::-1], base=2)
+    r2 = int(s6[::-1], base=2)
+    print(val1, val2, val3, r1, r2)
