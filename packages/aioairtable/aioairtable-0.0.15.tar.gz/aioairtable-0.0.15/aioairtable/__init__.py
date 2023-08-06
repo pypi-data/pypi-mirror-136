4b0037af68463e77550e93861906cb792a75b116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:38 +0000

    Add package aioairtable/0.0.15/aioairtable-0.0.15.tar.gz

---
+++ packages/aioairtable/aioairtable-0.0.15.tar.gz/aioairtable-0.0.15/aioairtable/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '0.0.15'
+
+from .aioairtable import (DT_FORMAT, Airtable, AirtableBase, AirtableRecord,
+                          AirtableTable, Attachment, CellFormat, Collaborator,
+                          Fields, Method, RequestAttachment, SortDirection,
+                          Thumbnail)
+
+__all__ = ('__version__', 'DT_FORMAT', 'Method', 'Fields', 'Thumbnail',
+           'Attachment', 'RequestAttachment', 'Collaborator', 'SortDirection',
+           'CellFormat', 'Airtable', 'AirtableBase', 'AirtableTable',
+           'AirtableRecord')
