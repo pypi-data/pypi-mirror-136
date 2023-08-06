e1dc185efc9bd8054aaa26280636e1c466b011ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:00 +0000

    Add package scipy/1.8.0rc4/scipy-1.8.0rc4-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/scipy/scipy-1.8.0rc4-cp38-cp38-macosx_10_9_x86_64.whl/scipy/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '1.8.0rc4'
+version = '1.8.0rc4'
+full_version = '1.8.0rc4'
+git_revision = 'ea004bd'
+commit_count = '2283'
+release = True
+
+if not release:
+    version = full_version
