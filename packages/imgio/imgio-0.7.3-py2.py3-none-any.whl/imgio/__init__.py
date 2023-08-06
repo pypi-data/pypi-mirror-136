8672d31594c174b2ed2347ac0d5a3531e2225d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:21 +0000

    Add package imgio/0.7.3/imgio-0.7.3-py2.py3-none-any.whl

---
+++ packages/imgio/imgio-0.7.3-py2.py3-none-any.whl/imgio/__init__.py
@@ -0,0 +1,14 @@
+"""
+Easy image file reading & writing. Supports PGM/PPM/PNM/PFM/PNG/JPG/TIFF/EXR/RAW.
+
+Example:
+  image, maxval = imgio.imread("foo.png")
+  imgio.imwrite("foo.ppm", image, maxval)
+
+https://github.com/toaarnio/imgio
+"""
+
+from .imgio import *
+
+__version__ = "0.7.3"
+__all__ = ["imread", "imwrite", "selftest"]
