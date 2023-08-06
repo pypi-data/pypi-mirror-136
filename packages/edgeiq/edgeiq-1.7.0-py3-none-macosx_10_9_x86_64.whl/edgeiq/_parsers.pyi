6e98bc3583e52430c93cbd312c66df09124233eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:15 +0000

    Add package edgeiq/1.7.0/edgeiq-1.7.0-py3-none-macosx_10_9_x86_64.whl

---
+++ packages/edgeiq/edgeiq-1.7.0-py3-none-macosx_10_9_x86_64.whl/edgeiq/_parsers.pyi
@@ -0,0 +1,4 @@
+from edgeiq.bounding_box import BoundingBox as BoundingBox
+from edgeiq.object_detection import ObjectDetectionPrediction as ObjectDetectionPrediction
+
+def parse_cvat_annotations(path, start_frame, end_frame, new_id_for_occlusion: bool = ...): ...
