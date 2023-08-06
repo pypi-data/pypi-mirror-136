5378762a2a99c7f2a9f34f57b99cffd60af4f833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:58 +0000

    Add package aicore-hangman/0.0.1/aicore-hangman-0.0.1.tar.gz

---
+++ packages/aicore-hangman/aicore-hangman-0.0.1.tar.gz/aicore-hangman-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='aicore-hangman',
+    version='0.0.1',
+    long_description_content_type='text/markdown',
+    description='Correct the hangman code',
+    author='Ivan Ying',
+    packages=find_packages(),
+)
