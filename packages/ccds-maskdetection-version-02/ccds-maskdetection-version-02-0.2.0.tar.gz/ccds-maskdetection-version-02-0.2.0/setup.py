c507e0bedc82ae1e0d8b7a5887c5f2cc55e975d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:00 +0000

    Add package ccds-maskdetection-version-02/0.2.0/ccds-maskdetection-version-02-0.2.0.tar.gz

---
+++ packages/ccds-maskdetection-version-02/ccds-maskdetection-version-02-0.2.0.tar.gz/ccds-maskdetection-version-02-0.2.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ccds-maskdetection-version-02',
+    packages=find_packages('src'),
+    version='0.2.0',
+    description='Mask detection',
+    author='seema',
+    author_email='seema.timmawagol@kabam.ai',
+    license='BSD-3',
+    package_dir={'': 'src'},
+    url='https://github.com/gmyrianthous/example-publish-pypi'
+)
