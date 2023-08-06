71e0fe7e7fe876d2618e3f309b41c119d09f62ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:22 +0000

    Add package raincloudy/1.1.2.dev1/raincloudy-1.1.2.dev1.tar.gz

---
+++ packages/raincloudy/raincloudy-1.1.2.dev1.tar.gz/raincloudy-1.1.2.dev1/tests/test_base.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8 -*-
+"""Define basic data for unittests."""
+import sys
+
+# python 2.7 unicode compatibility
+try:
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+except NameError:
+    pass
+
+import unittest
+import requests_mock
+
+from tests.extras import load_fixture, USERNAME, PASSWORD
+
+from raincloudy.const import (
+    LOGIN_ENDPOINT, STATUS_ENDPOINT, HOME_ENDPOINT, SETUP_ENDPOINT)
+
+
+class UnitTestBase(unittest.TestCase):
+    """Top level test class for RainCloudy Core."""
+
+    @requests_mock.Mocker()
+    def setUp(self, mock):
+        """Initialize rdy object for unittests."""
+        from raincloudy.core import RainCloudy
+
+        mock.get(LOGIN_ENDPOINT,
+                 text=load_fixture('home.html'))
+        mock.get(SETUP_ENDPOINT,
+                 text=load_fixture('setup.html'))
+        mock.get(STATUS_ENDPOINT,
+                 text=load_fixture('get_cu_and_fu_status.json'))
+
+        mock.post(LOGIN_ENDPOINT,
+                  text=load_fixture('home.html'))
+        mock.get(HOME_ENDPOINT,
+                 text=load_fixture('home.html'))
+
+        # initialize self.rdy object
+        self.rdy = RainCloudy(USERNAME, PASSWORD, ssl_warnings=False)
+        self.rdy.update()
+
+    def cleanUp(self):
+        """Cleanup any data created from the tests."""
+        self.rdy = None
+
+    def tearDown(self):
+        """Stop everything initialized."""
+        self.cleanUp()
---
+++ packages/raincloudy/raincloudy-1.1.2.dev1.tar.gz/raincloudy-1.1.2.dev1/tests/test_controller_request.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Test raincloudy.controller post."""
+import requests_mock
+from tests.test_base import UnitTestBase
+from raincloudy.const import (
+    STATUS_ENDPOINT, HOME_ENDPOINT, SETUP_ENDPOINT)
+from tests.extras import load_fixture
+
+
+class TestRainCloudyController(UnitTestBase):
+    """Unit tests for controller attributes."""
+
+    @requests_mock.Mocker()
+    def test_request_actions(self, mock):
+        """Test post actions."""
+        mock.get(STATUS_ENDPOINT,
+                 text=load_fixture('get_cu_and_fu_status.json'))
+        mock.get(HOME_ENDPOINT,
+                 text=load_fixture('home.html'))
+        mock.post(SETUP_ENDPOINT)
+        mock.get(SETUP_ENDPOINT)
+
+        controller = self.rdy.controllers[0]
+        controller.update()
+        self.assertIsNone(setattr(controller, 'name', 'test'))
+
+# vim:sw=4:ts=4:et:
