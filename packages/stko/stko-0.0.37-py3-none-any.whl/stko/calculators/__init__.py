f4c2cde1ec2c17d17a1378ab3341161ebebc2c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:38 +0000

    Add package stko/0.0.37/stko-0.0.37-py3-none-any.whl

---
+++ packages/stko/stko-0.0.37-py3-none-any.whl/stko/calculators/__init__.py
@@ -0,0 +1,6 @@
+from .calculators import *  # noqa
+from .xtb_calculators import *  # noqa
+from .rdkit_calculators import *  # noqa
+from .torsion_calculators import *  # noqa
+from .shape_calculators import *  # noqa
+from .orca_calculators import *  # noqa
