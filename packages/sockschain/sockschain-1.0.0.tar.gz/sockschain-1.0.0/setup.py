78a81fe23398860bc5e8caa594442c407b77af83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:16 +0000

    Add package sockschain/1.0.0/sockschain-1.0.0.tar.gz

---
+++ packages/sockschain/sockschain-1.0.0.tar.gz/sockschain-1.0.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from setuptools import setup
+setup()
+
+'''OLD SETUP.PY
+VERSION = "2.1.2"
+
+setup(
+    name = "SocksipyChain",
+    version = VERSION,
+    description = "A Python SOCKS/HTTP Proxy module",
+    install_requires=["six"],
+    long_description = """\
+This Python module allows you to create TCP connections through a chain
+of SOCKS or HTTP proxies without any special effort. It also supports
+TLS/SSL encryption if the OpenSSL modules are installed.
+""",
+     url = "http://github.com/PageKite/SocksiPyChain",
+     author = "Bjarni R. Einarsson",
+     author_email="bre@pagekite.net",
+     license = "BSD",
+     packages=["sockschain"],
+     entry_points = {
+         'console_scripts': [
+             'sockschain = sockschain:Main'
+         ]
+     }
+)
+
+'''
