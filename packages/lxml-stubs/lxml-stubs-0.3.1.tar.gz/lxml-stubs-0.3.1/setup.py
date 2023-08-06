78121011c1edf5c5406aa9598c97aaf3ad406d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:40 +0000

    Add package lxml-stubs/0.3.1/lxml-stubs-0.3.1.tar.gz

---
+++ packages/lxml-stubs/lxml-stubs-0.3.1.tar.gz/lxml-stubs-0.3.1/setup.py
@@ -0,0 +1,33 @@
+import re
+
+from setuptools import setup
+
+tests_require = [
+    "pytest>=6.0.0",
+    "pytest-mypy-plugins==1.4.0",
+    "coverage[toml]==5.2",
+]
+
+with open("README.md") as fh:
+    long_description = re.sub(
+        "<!-- start-no-pypi -->.*<!-- end-no-pypi -->\n",
+        "",
+        fh.read(),
+        flags=re.M | re.S,
+    )
+
+setup(
+    name="lxml-stubs",
+    version="0.3.1",
+    description="Type annotations for the lxml package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_data={"lxml-stubs": ["*.pyi", "*/*.pyi"]},
+    packages=["lxml-stubs"],
+    tests_require=tests_require,
+    extras_require={"test": tests_require},
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    zip_safe=False
+)
