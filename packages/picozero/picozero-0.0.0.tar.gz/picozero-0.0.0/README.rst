3170d2b1af127d1df0286930a67ba3b96ece1fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:18 +0000

    Add package picozero/0.0.0/picozero-0.0.0.tar.gz

---
+++ packages/picozero/picozero-0.0.0.tar.gz/picozero-0.0.0/README.rst
@@ -0,0 +1,12 @@
+picozero
+========
+
+A beginner-friendly library for using common electronics components with the Raspberry Pi Pico.
+
+Contributors
+------------
+
+Tracy Gardner
+
+Martin O'Hanlon
+
---
+++ packages/picozero/picozero-0.0.0.tar.gz/picozero-0.0.0/picozero/__init__.py
@@ -0,0 +1,7 @@
+message = """picozero
+
+A beginner-friendly library for using common electronics components with the Raspberry Pi Pico. 
+
+This packaged is still in development and has not yet been released."""
+
+print(message)
