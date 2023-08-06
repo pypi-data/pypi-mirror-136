d7f5dfdd2b6078573698302cc952bbf6493814b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:13 +0000

    Add package susee/1.2.5/susee-1.2.5.tar.gz

---
+++ packages/susee/susee-1.2.5.tar.gz/susee-1.2.5/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open('README.md', "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="susee",
+    version='1.2.5',
+    author="ing. F.S. Lovecchio - Bari, Italy",
+    author_email="f.lovecchio@pugliautomazione.it",
+    description="suSEE - Energy Monitoring Platform",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/frlovecchio/susee',
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[
+                'numpy',
+                'pandas',
+                'mysql-connector',
+                'pymodbus',
+                'pytz',
+                'scipy',
+                'statsmodels',
+                'python-decouple',
+                'setuptools',
+                'wheel',
+    ],
+   
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: System :: Monitoring',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        "Topic :: Software Development :: User Interfaces",
+    ],
+
+    package_dir={"": "src"},
+    python_requires=">=3.9.0",
+)
