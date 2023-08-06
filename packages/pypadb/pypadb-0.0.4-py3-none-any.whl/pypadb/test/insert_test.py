ddf18cf42c01f4835da6522cce40053641db7a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:44 +0000

    Add package pypadb/0.0.4/pypadb-0.0.4-py3-none-any.whl

---
+++ packages/pypadb/pypadb-0.0.4-py3-none-any.whl/pypadb/test/insert_test.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+from conf.db_configurer import db_configurer
+from conf.table_configurer import tables
+
+
+class User(BaseModel):
+    id: Optional[int]
+    account: str
+    stuffs: list = []
+
+
+class Stuff(BaseModel):
+    user_id: int
+    name: str
+    count: int
+
+
+if __name__ == '__main__':
+    db_configurer \
+        .set_host('localhost') \
+        .set_user('root') \
+        .set_password('123456') \
+        .set_database('test') \
+        .end()
+    tables.init_tables(user=User, stuff=Stuff)
+    # tables.user.insert(User(id=None, account='czcz1'))
+    res2 = tables.stuff.insert_batch([
+        Stuff(user_id=1, name='2323dfdf23', count=0),
+        Stuff(user_id=2, name='343dfdf434', count=0)
+    ])
+    print(res2)
