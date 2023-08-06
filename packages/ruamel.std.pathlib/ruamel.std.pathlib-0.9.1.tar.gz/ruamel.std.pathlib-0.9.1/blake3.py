c66f6dcb4856c0c70796136efcf1e9e69abb810b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:33 +0000

    Add package ruamel.std.pathlib/0.9.1/ruamel.std.pathlib-0.9.1.tar.gz

---
+++ packages/ruamel.std.pathlib/ruamel.std.pathlib-0.9.1.tar.gz/ruamel.std.pathlib-0.9.1/blake3.py
@@ -0,0 +1,30 @@
+# this fails immediately on import of ruamel.std.pathlib.blake3 if blake3 not installed
+from blake3 import blake3
+
+from pathlib import Path
+
+if not hasattr(Path, 'blake3'):
+
+    def _blake3(self, size=-1, timeit=False):
+        """blake3 hash of the contents
+        if size is provided and non-negative only read that amount of bytes from
+        the start of the file
+        """
+        with self.open(mode='rb') as f:
+            data = f.read(size)
+        if timeit:
+            import time
+
+            start = time.time()
+            try:
+                res = blake3(data, max_threads=blake3.AUTO)
+            except TypeError:
+                res = blake3(data, multithreading=True)
+            return time.time() - start, res
+        try:
+            res = blake3(data, max_threads=blake3.AUTO)
+        except TypeError:
+            res = blake3(data, multithreading=True)
+        return res
+
+    Path.blake3 = _blake3
