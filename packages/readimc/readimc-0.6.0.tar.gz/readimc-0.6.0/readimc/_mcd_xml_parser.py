715bb557d801871a00272a3552e5930f9d75b81d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:23 +0000

    Add package readimc/0.6.0/readimc-0.6.0.tar.gz

---
+++ packages/readimc/readimc-0.6.0.tar.gz/readimc-0.6.0/readimc/_mcd_xml_parser.py
@@ -0,0 +1,201 @@
+import re
+
+from typing import Dict, List, Optional, Tuple
+from xml.etree import ElementTree as ET
+
+from .data import Slide, Panorama, Acquisition
+
+
+class MCDXMLParserError(Exception):
+    def __init__(self, *args) -> None:
+        """Error occurring when parsing well-formed but invalid .mcd metadata XML"""
+        super(MCDXMLParserError, self).__init__(*args)
+
+
+class MCDXMLParser:
+    _CHANNEL_REGEX = re.compile(r"^(?P<metal>[a-zA-Z]+)\((?P<mass>[0-9]+)\)$")
+
+    def __init__(
+        self, metadata_xml: ET.Element, metadata_xmlns: Optional[str] = None
+    ) -> None:
+        """A class for parsing XML metadata contained in .mcd files
+
+        :param metadata_xml: metadata from .mcd files in XML format
+        :param metadata_xmlns: metadata XML namespace
+        """
+        self._metadata_xml = metadata_xml
+        self._metadata_xmlns = metadata_xmlns
+
+    def parse_slides(self) -> List[Slide]:
+        """Extract slide information from the current metadata XML"""
+        slides = [
+            self._parse_slide(slide_elem) for slide_elem in self._find_elements("Slide")
+        ]
+        slides.sort(key=lambda slide: slide.id)
+        return slides
+
+    def _parse_slide(self, slide_elem: ET.Element) -> Slide:
+        slide = Slide(
+            self._get_text_as_int(slide_elem, "ID"),
+            self._get_metadata_dict(slide_elem),
+        )
+        panorama_elems = self._find_elements(f"Panorama[SlideID='{slide.id}']")
+        for panorama_elem in panorama_elems:
+            panorama = None
+            panorama_id = self._get_text_as_int(panorama_elem, "ID")
+            panorama_type = self._get_text_or_none(panorama_elem, "Type")
+            if panorama_type != "Default":  # ignore "virtual" Panoramas
+                panorama = self._parse_panorama(panorama_elem, slide)
+                slide.panoramas.append(panorama)
+            acquisition_roi_elems = self._find_elements(
+                f"AcquisitionROI[PanoramaID='{panorama_id}']"
+            )
+            for acquisition_roi_elem in acquisition_roi_elems:
+                acquisition_roi_id = self._get_text_as_int(acquisition_roi_elem, "ID")
+                roi_point_elems = self._find_elements(
+                    f"ROIPoint[AcquisitionROIID='{acquisition_roi_id}']"
+                )
+                roi_points_um = None
+                if len(roi_point_elems) == 4:
+                    roi_points_um = tuple(
+                        (
+                            self._get_text_as_float(roi_point_elem, "SlideXPosUm"),
+                            self._get_text_as_float(roi_point_elem, "SlideYPosUm"),
+                        )
+                        for roi_point_elem in sorted(
+                            roi_point_elems,
+                            key=lambda roi_point_elem: self._get_text_as_int(
+                                roi_point_elem, "OrderNumber"
+                            ),
+                        )
+                    )
+                acquisition_elems = self._find_elements(
+                    f"Acquisition[AcquisitionROIID='{acquisition_roi_id}']"
+                )
+                for acquisition_elem in acquisition_elems:
+                    acquisition = self._parse_acquisition(
+                        acquisition_elem, slide, panorama, roi_points_um
+                    )
+                    slide.acquisitions.append(acquisition)
+                    if panorama is not None:
+                        panorama.acquisitions.append(acquisition)
+        slide.panoramas.sort(key=lambda panorama: panorama.id)
+        slide.acquisitions.sort(key=lambda acquisition: acquisition.id)
+        return slide
+
+    def _parse_panorama(self, panorama_elem: ET.Element, slide: Slide) -> Panorama:
+        return Panorama(
+            slide,
+            self._get_text_as_int(panorama_elem, "ID"),
+            self._get_metadata_dict(panorama_elem),
+        )
+
+    def _parse_acquisition(
+        self,
+        acquisition_elem: ET.Element,
+        slide: Slide,
+        panorama: Optional[Panorama],
+        roi_points_um: Optional[
+            Tuple[
+                Tuple[float, float],
+                Tuple[float, float],
+                Tuple[float, float],
+                Tuple[float, float],
+            ]
+        ],
+    ) -> Acquisition:
+        acquisition_id = self._get_text_as_int(acquisition_elem, "ID")
+        acquisition_channel_elems = self._find_elements(
+            f"AcquisitionChannel[AcquisitionID='{acquisition_id}']"
+        )
+        acquisition_channel_elems.sort(
+            key=lambda acquisition_channel_elem: self._get_text_as_int(
+                acquisition_channel_elem, "OrderNumber"
+            )
+        )
+        acquisition = Acquisition(
+            slide,
+            panorama,
+            acquisition_id,
+            roi_points_um,
+            self._get_metadata_dict(acquisition_elem),
+            len(acquisition_channel_elems) - 3,
+        )
+        for i, acquisition_channel_elem in enumerate(acquisition_channel_elems):
+            channel_name = self._get_text(acquisition_channel_elem, "ChannelName")
+            if i == 0 and channel_name != "X":
+                raise MCDXMLParserError(
+                    f"First channel '{channel_name}' should be named 'X'"
+                )
+            if i == 1 and channel_name != "Y":
+                raise MCDXMLParserError(
+                    f"Second channel '{channel_name}' should be named 'Y'"
+                )
+            if i == 2 and channel_name != "Z":
+                raise MCDXMLParserError(
+                    f"Third channel '{channel_name}' should be named 'Z'"
+                )
+            if channel_name in ("X", "Y", "Z"):
+                continue
+            m = re.match(self._CHANNEL_REGEX, channel_name)
+            if m is None:
+                raise MCDXMLParserError(
+                    "Cannot extract channel information "
+                    f"from channel name '{channel_name}' "
+                    f"for acquisition {acquisition.id}"
+                )
+            channel_label = self._get_text(acquisition_channel_elem, "ChannelLabel")
+            acquisition.channel_metals.append(m.group("metal"))
+            acquisition.channel_masses.append(int(m.group("mass")))
+            acquisition.channel_labels.append(channel_label)
+        return acquisition
+
+    def _find_elements(self, path: str) -> List[ET.Element]:
+        namespaces = None
+        if self._metadata_xmlns is not None:
+            namespaces = {"": self._metadata_xmlns}
+        return self._metadata_xml.findall(path, namespaces=namespaces)
+
+    def _get_text_or_none(self, parent_elem: ET.Element, tag: str) -> Optional[str]:
+        namespaces = None
+        if self._metadata_xmlns is not None:
+            namespaces = {"": self._metadata_xmlns}
+        elem = parent_elem.find(tag, namespaces=namespaces)
+        return (elem.text or "") if elem is not None else None
+
+    def _get_text(self, parent_elem: ET.Element, tag: str) -> str:
+        text = self._get_text_or_none(parent_elem, tag)
+        if text is None:
+            raise MCDXMLParserError(
+                f"XML tag '{tag}' not found for parent XML tag '{parent_elem.tag}'"
+            )
+        return text
+
+    def _get_text_as_int(self, parent_elem: ET.Element, tag: str) -> int:
+        text = self._get_text(parent_elem, tag)
+        try:
+            return int(text)
+        except ValueError as e:
+            raise MCDXMLParserError(
+                f"Text '{text}' of XML tag '{tag}' cannot be converted to int "
+                f"for parent XML tag '{parent_elem.tag}'"
+            ) from e
+
+    def _get_text_as_float(self, parent_elem: ET.Element, tag: str) -> float:
+        text = self._get_text(parent_elem, tag)
+        try:
+            return float(text)
+        except ValueError as e:
+            raise MCDXMLParserError(
+                f"Text '{text}' of XML tag '{tag}' cannot be converted to "
+                f"float for parent XML tag '{parent_elem.tag}'"
+            ) from e
+
+    def _get_metadata_dict(self, parent_elem: ET.Element) -> Dict[str, str]:
+        metadata = {}
+        for elem in parent_elem:
+            tag = elem.tag
+            if self._metadata_xmlns is not None:
+                tag = tag.replace(f"{{{self._metadata_xmlns}}}", "")
+            metadata[tag] = elem.text or ""
+        return metadata
