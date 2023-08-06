62dd1d5672b65c590db4cfd7e2cce0067563070a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:16 +0000

    Add package rapidfuzz-capi/1.0.4/rapidfuzz_capi-1.0.4.tar.gz

---
+++ packages/rapidfuzz-capi/rapidfuzz_capi-1.0.4.tar.gz/rapidfuzz_capi-1.0.4/rapidfuzz_capi/__init__.py
@@ -0,0 +1,22 @@
+__author__ = "Max Bachmann"
+__license__ = "MIT"
+__version__ = "1.0.4"
+
+def get_include():
+    """
+    Return the directory that contains the RapidFuzz \\*.h header files.
+    Extension modules that need to compile against RapidFuzz should use this
+    function to locate the appropriate include directory.
+
+    Notes
+    -----
+    When using ``distutils``, for example in ``setup.py``.
+    ::
+        import rapidfuzz_capi
+        ...
+        Extension('extension_name', ...
+                include_dirs=[rapidfuzz_capi.get_include()])
+        ...
+    """
+    import os
+    return os.path.dirname(__file__)
