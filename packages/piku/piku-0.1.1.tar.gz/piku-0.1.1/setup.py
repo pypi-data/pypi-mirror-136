aa90c1ba401452ecb05c4924e5828dad20f9e746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:29 +0000

    Add package piku/0.1.1/piku-0.1.1.tar.gz

---
+++ packages/piku/piku-0.1.1.tar.gz/piku-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['piku', 'piku.commands']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['piku = piku.main:main']}
+
+setup_kwargs = {
+    'name': 'piku',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '# Piku\nA small command line utility for managing CircuitPython projects\n',
+    'author': 'Mark Raleson',
+    'author_email': 'markraleson@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/mraleson/rag.git',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
