5e5e96ef3f332bac2ed254e0d6121bb528c3f966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:36 +0000

    Add package awswrangler/2.14.0/awswrangler-2.14.0.tar.gz

---
+++ packages/awswrangler/awswrangler-2.14.0.tar.gz/awswrangler-2.14.0/awswrangler/athena/__init__.py
@@ -0,0 +1,31 @@
+"""Amazon Athena Module."""
+
+from awswrangler.athena._read import read_sql_query, read_sql_table, unload  # noqa
+from awswrangler.athena._utils import (  # noqa
+    create_athena_bucket,
+    describe_table,
+    get_query_columns_types,
+    get_query_execution,
+    get_work_group,
+    repair_table,
+    show_create_table,
+    start_query_execution,
+    stop_query_execution,
+    wait_query,
+)
+
+__all__ = [
+    "read_sql_query",
+    "read_sql_table",
+    "create_athena_bucket",
+    "describe_table",
+    "get_query_columns_types",
+    "get_query_execution",
+    "get_work_group",
+    "repair_table",
+    "show_create_table",
+    "start_query_execution",
+    "stop_query_execution",
+    "unload",
+    "wait_query",
+]
