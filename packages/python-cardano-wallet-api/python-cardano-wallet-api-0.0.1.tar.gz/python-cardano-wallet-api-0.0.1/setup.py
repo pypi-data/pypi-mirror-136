fc2c2053094484a3263aa5eb39f3980202991d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:05 +0000

    Add package python-cardano-wallet-api/0.0.1/python-cardano-wallet-api-0.0.1.tar.gz

---
+++ packages/python-cardano-wallet-api/python-cardano-wallet-api-0.0.1.tar.gz/python-cardano-wallet-api-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'python-cardano-wallet-api',         
+  packages = ['python-cardano-wallet-api'],   
+  version = '0.0.1',      
+  license='Apache',      
+  description = 'A python wrapper for the Cardano-node API',  
+  author = 'Leandros Holleman',                  
+  author_email = 'leantrosh@gmail.com',     
+  url = 'https://github.com/leo42/python-cardano-wallet-api',  
+  download_url = 'https://github.com/leo42/python-cardano-wallet-api/archive/refs/tags/0.0.1.tar.gz',    
+  keywords = ['Cardano-wallet', 'API', 'Wrapper'],  
+  install_requires=[            # I get to this in a second
+          'json',
+          'requests',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either 3 - Alpha, 4 - Beta or 5 - ProductionStable as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'License :: OSI Approved :: Apache Software License',   # Again, pick a license
+    'Programming Language :: Python :: 3'
+  ],
+)
