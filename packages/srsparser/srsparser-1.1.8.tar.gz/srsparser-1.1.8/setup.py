00b0826c64d65a5da85e6a050e80c01db4398e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:50 +0000

    Add package srsparser/1.1.8/srsparser-1.1.8.tar.gz

---
+++ packages/srsparser/srsparser-1.1.8.tar.gz/srsparser-1.1.8/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    'anytree>=2.8.0',
+    'python-docx>=0.8.11',
+    'pymorphy2>=0.9.1',
+    'nltk>=3.6.5',
+    'pymongo>=4.0.1',
+    'dnspython>=2.1.0',
+    'gensim>=4.1.2'
+]
+
+setup(
+    name='srsparser',
+    version='1.1.8',
+    author='Курмыза Павел',
+    author_email='tmrrwnxtsn@gmail.com',
+    url='https://github.com/Text-Analysis/srsparser',
+    python_requires='>=3.7',
+    description='Пакет для анализа и загрузки текстовых документов в NoSQL СУБД MongoDB.',
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'srsparser = srsparser.__main__:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        'Topic :: Text Processing :: Linguistic',
+    ],
+)
