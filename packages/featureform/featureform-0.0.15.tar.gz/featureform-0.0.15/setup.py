5c03979691ea67fd10eceedcd20dc542c154c2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:02 +0000

    Add package featureform/0.0.15/featureform-0.0.15.tar.gz

---
+++ packages/featureform/featureform-0.0.15.tar.gz/featureform-0.0.15/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="featureform",
+    version="0.0.15",
+    author="Featureform",
+    author_email="simba@featureform.com",
+    description="Data infrastructure for machine learning embeddings and other features.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/featureform/embeddinghub",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "protobuf",
+        "hnswlib",
+        "numpy",
+        "grpcio-tools",
+    ],
+    python_requires=">=3.6",
+)
