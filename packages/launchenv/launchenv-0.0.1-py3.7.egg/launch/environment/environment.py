d3bb45e41fa0eae794b31a728f2e513beb43b052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:01 +0000

    Add package launchenv/0.0.1/launchenv-0.0.1-py3.7.egg

---
+++ packages/launchenv/launchenv-0.0.1-py3.7.egg/launch/environment/environment.py
@@ -0,0 +1,46 @@
+import os
+import json
+
+class data:
+    
+
+    # Read json and return dictionary
+    def read_json(filename):
+        with open (filename + '.json', 'r') as f:
+            dic = json.load(f)
+            return dic
+
+    # create json file
+    def create_json(filename):
+        if os.path.exists(f'{filename}.json'):
+            print('File already exist.')
+        else:
+            dic = {}
+            with open(filename + '.json', 'a+') as f:
+                json.dump(dic, f)
+
+    # Add program to json file
+    def add_json(filename, key, value):
+        with open (filename + '.json', 'r+') as f:
+            dic = json.load(f)
+            dic = dic.copy()
+            dic.update({key:value})
+            f.seek(0)
+            json.dump(dic, f, indent=2)
+
+    # Remove program from json file
+    def remove_element_json(filename, item_key):
+        with open (filename + '.json') as f:
+            dic = json.load(f)
+            dic = dic.copy()
+            if item_key not in dic:
+                print(f'Program {item_key} not found')
+            del dic[item_key]
+            
+        with open (filename + '.json', 'w') as f:
+            json.dump(dic, f)
+
+
+    #lst = read_json('data')
+    #for _ in lst:
+    #    print(_)
---
+++ packages/launchenv/launchenv-0.0.1-py3.7.egg/launch/launch.py
@@ -0,0 +1,69 @@
+import os
+import subprocess
+
+from .environment.environment import data
+from .utils.utils import PATH, logo
+
+CURRENT_PATH = PATH
+
+class handle:
+    
+            
+    def add(filename):
+        name = input('Enter name of program to add this environment: ')
+        path = input('Enter path of program executable: ')
+        try:
+            data.add_json(filename, name, path)
+        except Exception as Error:
+            print(Error)
+            
+    def build(filename, build):
+        print(f'Launch file with name {build} will be created...')
+        data.create_json(filename)
+            
+    def launch(filename, env):
+        try:
+            dic = data.read_json(filename)
+            print(f'Launching workspace - {env}')
+            logo()
+            handle.run(dic)
+        except FileNotFoundError:
+            print(f'No Workspace named {env} found')
+            handle.ls()
+    
+    def purge(filename, purge):
+        if os.path.exists(filename):
+            verify = input(f'Confirm to delete {purge} environment [y/n]: ')
+            if verify == 'y':
+                os.remove(filename)
+        else:
+            print(f'No environment named {purge}.')
+            
+    def remove_element(filename):
+        item = input('Enter program name to delete from environment: ')
+        data.remove_element_json(filename, item)
+        
+    def show_lst(filename, show_list):
+        try:
+            dic = data.read_json(filename)
+            print(f'Workspace {show_list}')
+            for _ in dic:
+                print(f' {_}: {dic[_]}')
+        except FileNotFoundError:
+            print(f'No Workspace named {show_list} found')
+            
+    @staticmethod
+    def ls():
+        print('Here is list of available workspace:')
+        for _ in os.listdir(CURRENT_PATH):
+            if _.endswith('.json'):
+                print(' ',_.replace('.json',''))
+            
+    @staticmethod                
+    def run(dic):
+        for _ in dic:
+            try:
+                subprocess.Popen(dic[_])
+                print(f'Executed {_}')
+            except Exception:
+                print(f'Could not open {_}:{dic[_]}. Check if path/file_name is correct')
