7b9045c5c21f01c27c3b5df800a9057119acfb83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:14 +0000

    Add package markcrowe/0.0.9/markcrowe-0.0.9-py3-none-any.whl

---
+++ packages/markcrowe/markcrowe-0.0.9-py3-none-any.whl/population_planning/dataframe_labels.py
@@ -0,0 +1,54 @@
+# Names of the Series in the DataFrame for the births, deaths, and marriages data
+
+BIRTHS = 'Births'
+BIRTH_RATE = 'Birth Rate'
+DEATHS = 'Deaths'
+DEATH_RATE = 'Death Rate'
+ESTIMATED_POPULATION = 'Estimated Population'
+ESTIMATED_POPULATION_STD = 'Estimated Population STD'
+INFANT_MORTALITY_RATE = 'Infant Mortality Rate'
+MARRIAGES = 'Marriages'
+MARRIAGE_RATE = 'Marriage Rate'
+QUARTER = 'Quarter'
+QUARTERS = 'Quarters'
+RECORD_COMPLETENESS = 'Record Completeness%'
+STATE = 'State'
+YEAR = 'Year'
+
+# Names of the Series in the DataFrame for the county births data
+
+ALL_BIRTHS = 'All Births'
+AVERAGE_AGE_OF_FIRST_TIME_MOTHER = 'Average Age of First Time Mother'
+AVERAGE_AGE_OF_MOTHER = 'Average Age of Mother'
+BIRTHS_OUTSIDE_MARRIAGE = 'Births outside Marriage'
+BIRTHS_WITHIN_MARRIAGE = 'Births within Marriage'
+COUNTY = 'County'
+FIRST_BIRTHS = 'First Births'
+# YEAR # This is the same as the YEAR column in the births data
+
+# Names of the Series in the DataFrame for the deaths region data
+
+AGE_AT_DEATH = 'Age at Death'
+AREA_OF_RESIDENCE = 'Area of Residence'
+SEX = 'Sex'
+STATISTIC = 'Statistic'
+UNIT = 'UNIT'
+VALUE = 'VALUE'
+# YEAR # This is the same as the YEAR column in the births data
+
+# Names of the Series in the DataFrame for the county divorces
+
+AGE_RANGE = "Age Range"
+COUNT = 'Count'
+HOUSEHOLD_CHILD_COUNT = 'Household Child Count'
+# SEX # This is the same as the SEX column in the deaths region data
+
+# YEAR # This is the same as the YEAR column in the births data.
+
+# Names of the Series in the DataFrame for the population data
+
+AGE = "Age"
+POPULATION = "Population"
+# SEX # This is the same as the SEX column in the deaths region data
+
+# YEAR # This is the same as the YEAR column in the births data.
