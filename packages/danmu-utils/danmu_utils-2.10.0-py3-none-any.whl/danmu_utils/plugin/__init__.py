910dd27dec2d401579e422af200d581b2ebd8317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:33 +0000

    Add package danmu-utils/2.10.0/danmu_utils-2.10.0-py3-none-any.whl

---
+++ packages/danmu-utils/danmu_utils-2.10.0-py3-none-any.whl/danmu_utils/plugin/__init__.py
@@ -0,0 +1,11 @@
+import danmu_utils.common
+
+__all__ = []
+
+__all__.append('bilibili')
+__all__.append('diyidan')
+__all__.append('youku')
+__all__.append('iqiyi')
+__all__.append('mgtv')
+__all__.append('wetv')
+__all__.append('renrenvideo')
