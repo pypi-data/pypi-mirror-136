5a42a804f9d1983421c20357524808889f3ce3c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:50 +0000

    Add package skmCalc/0.1/skmCalc-0.1.tar.gz

---
+++ packages/skmCalc/skmCalc-0.1.tar.gz/skmCalc-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="skmCalc",
+version="0.1",
+description="simple calculator",
+long_description="this is a package to perform simple arithmetic operations of addition, subtraction, multiplication and division on two numbers",
+author="Sumit Kumar Mishra",
+packages=['skmCalc'],
+install_requires=[])
