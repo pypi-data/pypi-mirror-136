e9e2c87ea07e27a360b445080c53de9692e151bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:14 +0000

    Add package pathable/0.4.0/pathable-0.4.0.tar.gz

---
+++ packages/pathable/pathable-0.4.0.tar.gz/pathable-0.4.0/pathable/accessors.py
@@ -0,0 +1,30 @@
+"""Pathable accessors module"""
+from contextlib import contextmanager
+
+
+class LookupAccessor:
+    """Accessor for object that supports __getitem__ lookups"""
+
+    def __init__(self, lookup):
+        self.lookup = lookup
+
+    def stat(self, parts):
+        return NotImplementedError
+
+    def keys(self, parts):
+        with self.open(parts) as d:
+            return d.keys()
+
+    def len(self, parts):
+        with self.open(parts) as d:
+            return len(d)
+
+    @contextmanager
+    def open(self, parts):
+        content = self.lookup
+        for part in parts:
+            content = content[part]
+        try:
+            yield content
+        finally:
+            pass
