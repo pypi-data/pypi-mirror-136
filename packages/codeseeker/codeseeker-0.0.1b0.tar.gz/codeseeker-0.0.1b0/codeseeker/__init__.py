8aa3fe4b321380ee2503ad29c1ee2db198091e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:03 +0000

    Add package codeseeker/0.0.1b0/codeseeker-0.0.1b0.tar.gz

---
+++ packages/codeseeker/codeseeker-0.0.1b0.tar.gz/codeseeker-0.0.1b0/codeseeker/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# CodeSeeker
+from .seeker import Seeker, open_url
+from .utils import show
+
+__all__ = [
+    "Seeker",
+    "show",
+    "open_url",
+]
+
+__version__ = "0.0.1"
