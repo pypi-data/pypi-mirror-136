76fadd9cef4e54a68dfa89dd35e0c5876ad6e26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:19 +0000

    Add package SafeDelete/0.0.1/SafeDelete-0.0.1.tar.gz

---
+++ packages/SafeDelete/SafeDelete-0.0.1.tar.gz/SafeDelete-0.0.1/setup.py
@@ -0,0 +1,25 @@
+'''
+Author: Cheryl Li
+Date: 2022-01-28 14:17:54
+LastEditTime: 2022-01-28 14:57:22
+FilePath: /undefined/Users/licheryl/Desktop/test/setup.py
+'''
+import setuptools
+setuptools.setup(
+    name="SafeDelete",
+    version="0.0.1",
+    author="cheryl",
+    author_email="cheryllee626@gmail.com",
+    description="A small package for delecting files safely",
+    url="https://github.com/cherylLbt/SafeDelete.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+     install_requires=[
+        'datetime',
+        'shutil'
+    ],
+)
