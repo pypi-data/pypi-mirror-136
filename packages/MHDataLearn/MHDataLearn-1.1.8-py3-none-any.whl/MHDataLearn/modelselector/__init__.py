7dc5a851890957ed753115cf4207e944f0da3caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:03 +0000

    Add package MHDataLearn/1.1.8/MHDataLearn-1.1.8-py3-none-any.whl

---
+++ packages/MHDataLearn/MHDataLearn-1.1.8-py3-none-any.whl/MHDataLearn/modelselector/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+This subpackage contains functions to split the preprocessed
+data in to train and test sets, build classification machine
+learning models, evaluate model performance and select the
+best performing model
+"""
+from MHDataLearn.modelselector import model_selector, default_models
