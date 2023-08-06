1515e74a57dd283a7e41a1af241a8a72e4b7b70e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:56 +0000

    Add package pandemaniac/0.0.1/pandemaniac-0.0.1.tar.gz

---
+++ packages/pandemaniac/pandemaniac-0.0.1.tar.gz/pandemaniac-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+long_description = "Pandemaniac is a package for visualizing the history of competing seeds on a multiplayer graph for the 'pandemaniac' project in CS144 at Caltech."
+
+setuptools.setup(
+    name='pandemaniac',
+    version='0.0.1',
+    author='Rosita Fu',
+    author_email='rosita.fu99@gmail.com',
+    url="https://github.com/atisor73/pandemaniac",
+    description='CS144 Pandemaniac iterative visualizer',
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    install_requires=["numpy", "pandas", "bokeh>=1.4.0",
+                      "panel", "networkx", "iqplot"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
