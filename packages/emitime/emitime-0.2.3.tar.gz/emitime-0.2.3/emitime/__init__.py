ebb352eec3fb0c9c0ce5f0e4bdd3bd7ba1b70ac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:34 +0000

    Add package emitime/0.2.3/emitime-0.2.3.tar.gz

---
+++ packages/emitime/emitime-0.2.3.tar.gz/emitime-0.2.3/emitime/__init__.py
@@ -0,0 +1,2 @@
+from emitime.base import Interval, Moment
+from emitime import parser
---
+++ packages/emitime/emitime-0.2.3.tar.gz/emitime-0.2.3/emitime/__init__.pyi
@@ -0,0 +1,2 @@
+from emitime import parser as parser
+from emitime.base import Interval as Interval, Moment as Moment
