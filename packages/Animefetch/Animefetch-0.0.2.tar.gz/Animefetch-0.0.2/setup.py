1f9bc6e062c1cda23a73ad6a6f1c01d1242410c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:34 +0000

    Add package Animefetch/0.0.2/Animefetch-0.0.2.tar.gz

---
+++ packages/Animefetch/Animefetch-0.0.2.tar.gz/Animefetch-0.0.2/setup.py
@@ -0,0 +1,38 @@
+import pathlib
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION ='0.0.2'
+PACKAGE_NAME =  'Animefetch'
+AUTHOR = "Tadeo Asael 'Thadeuks'"
+AUTHOR_EMAIL = 'ass.hurtado2008@gmail.com'
+URL = 'https://github.com/Thadeuks'
+
+LICENSE= 'MIT'
+DESCRIPTION= 'Anime Command-Line System Information Tool'
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding='utf-8')
+LONG_DESC_TYPE = 'text/markdown'
+
+INSTALL_REQUIRES = [
+'colorama',
+'psutil'
+]
+
+setup(
+	name=PACKAGE_NAME,
+	version=VERSION,
+	description = DESCRIPTION,
+	long_description = LONG_DESCRIPTION,
+	long_description_content_type = LONG_DESC_TYPE,
+	author = AUTHOR,
+	author_email = AUTHOR_EMAIL,
+	url=URL,
+	install_requires = INSTALL_REQUIRES,
+	license = LICENSE,
+	packages = find_packages(),
+	include_package_data = True
+
+	)
+
+
