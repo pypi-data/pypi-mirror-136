8e3b00bbb6b91c546cf25414ad6eaff1ba61f4a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:34 +0000

    Add package askquestions/0.2.0/askquestions-0.2.0.tar.gz

---
+++ packages/askquestions/askquestions-0.2.0.tar.gz/askquestions-0.2.0/how/crawler.py
@@ -0,0 +1,49 @@
+
+"""
+Ask More Questions.
+Author: Ron Nathaniel
+"""
+
+from typing import Generator
+from googlesearch import search
+
+from how.util import gen_next_n
+
+
+def ask_google(query: str, limit=20,) -> Generator:
+    """
+    Ask Google Anything.
+    :param query: Query to google search
+    :param limit: Total results to return
+    :return: Lazy Generator of result URIs
+    """
+    results = search(
+        query,
+        stop=limit,
+    )
+
+    return results
+
+
+def ask_sof(query: str, limit=20, ) -> Generator:
+    """
+    Ask StackOverflow Anything.
+    :param query: Query to StackOverflow search
+    :param limit: Total results to return
+    :return: Lazy Generator of result URIs
+    """
+    results = ask_google(
+        query=query + ' site:stackoverflow.com',
+        limit=limit
+    )
+
+    return results
+
+
+if __name__ == '__main__':
+    # Example Usage
+    res = ask_sof(
+        'exit vi'
+    )
+
+    print(gen_next_n(res, 5))
