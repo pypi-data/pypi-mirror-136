315c227c1e00e0f687bb514ea53724fa2446fdfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:17 +0000

    Add package selenium-python-tools/0.0.1/selenium_python_tools-0.0.1.tar.gz

---
+++ packages/selenium-python-tools/selenium_python_tools-0.0.1.tar.gz/selenium_python_tools-0.0.1/README.md
@@ -0,0 +1,3 @@
+# Selenium Python Tools
+
+Sets chrome driver options appropriately for python selenium on Macbook w M1 chip
---
+++ packages/selenium-python-tools/selenium_python_tools-0.0.1.tar.gz/selenium_python_tools-0.0.1/src/selenium_python_tools/get_driver.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def set_chrome_options() -> None:
+    """Sets chrome options for Selenium.
+    Chrome options for headless browser is enabled.
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    return chrome_options
+
+def get_driver():
+    return webdriver.Chrome(options=set_chrome_options())
