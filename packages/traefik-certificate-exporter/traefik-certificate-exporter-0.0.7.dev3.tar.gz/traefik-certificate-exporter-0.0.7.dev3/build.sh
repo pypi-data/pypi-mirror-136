d82a871cf3f4edd4a7e27875db29683f9dad4360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:01 +0000

    Add package traefik-certificate-exporter/0.0.7.dev3/traefik-certificate-exporter-0.0.7.dev3.tar.gz

---
+++ packages/traefik-certificate-exporter/traefik-certificate-exporter-0.0.7.dev3.tar.gz/traefik-certificate-exporter-0.0.7.dev3/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+ACTION=${1}
+
+PYPI_INDEX=pypi
+[[ "$ACTION" != *"test"* ]] || PYPI_INDEX=testpypi
+
+if [ -d dist ]; then
+    rm -r dist
+fi
+
+if [[ "$ACTION" == *"build"* ]]; then
+    python3 -m build
+fi
+
+if [ "$ACTION" == *"publish"* ]; then
+    python3 -m twine upload --repository $PYPI_INDEX dist/*
+
+    echo "Sleaping 30 secods to allow $PYPI_INDEX enough time to process upload of new version"
+    sleep 30s
+
+    cd docker
+    ./build.sh $ACTION
+    cd ..
+fi
---
+++ packages/traefik-certificate-exporter/traefik-certificate-exporter-0.0.7.dev3.tar.gz/traefik-certificate-exporter-0.0.7.dev3/docker/build.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+ACTION=${1:-test}
+
+if [ -d data ]; then
+    sudo rm -r data
+fi
+
+[[ "$ACTION" != *"test"* ]] || TEST_VERSION=1
+
+[ -n "$DOCKER_REPO" ]    || DOCKER_REPO="ravensorb/"
+[ -n "$SOURCE_BRANCH" ]  || SOURCE_BRANCH=$(git symbolic-ref -q --short HEAD)  
+[ -n "$GIT_SHA1" ]       || GIT_SHA1=$(git rev-parse -q HEAD)
+[ -n "$VERSION" ]        || VERSION=$(git describe --long)
+
+[ -n "$SOURCE_TYPE" ]        || SOURCE_TYPE=git 
+[ -n "$DOCKERFILE" ]         || DOCKERFILE=Dockerfile
+[ -n "$DOCKERFILE_PATH" ]    || DOCKERFILE_PATH=.
+[ -n "$IMAGE_NAME" ]         || IMAGE_NAME=${DOCKER_REPO}traefik-certificate-exporter
+
+if [[ "$ACTION" == *"build"* ]]; then
+    docker build \
+        -f $DOCKERFILE \
+        --rm \
+        --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+        --build-arg "SOURCE_COMMIT=$GIT_SHA1" \
+        --build-arg "SOURCE_TYPE=$SOURCE_TYPE" \
+        ${TEST_VERSION:+--build-arg "TEST_VERSION=1"} \
+        ${VERSION:+--build-arg "VERSION=$VERSION"} \
+        -t $IMAGE_NAME:${VERSION} \
+        -t $IMAGE_NAME:latest . 
+fi
+
+if [[ "$ACTION" == *"run"* ]]; then
+    docker run \
+            --rm \
+            -v ${PWD}/data/data:/data:ro \
+            -v ${PWD}/data/certs:/certs:rw \
+            -v ${PWD}/data/config:/config:rw \
+            -v /var/run/docker.sock:/var/run/docker.sock:ro \
+            $IMAGE_NAME:latest 
+fi
+
+if [[ "$ACTION" == *"publish"* ]]; then
+    docker push $IMAGE_NAME:latest
+    docker push $IMAGE_NAME:${VERSION}
+fi
