df1e7f5b53929aeb6fc9a4b28d3707e5ae82ced7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:35 +0000

    Add package benderml/0.1.12/benderml-0.1.12-py3-none-any.whl

---
+++ packages/benderml/benderml-0.1.12-py3-none-any.whl/bender/pipeline/tests/test_cross_validation.py
@@ -0,0 +1,25 @@
+import pytest
+from pandas import DataFrame
+
+from bender.importers import DataImporters
+from bender.metrics import Metrics
+from bender.model_trainer.xgboosted_tree import XGBoostTrainer
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_cross_validation(date_df: DataFrame) -> None:
+
+    score = await (
+        DataImporters.literal(date_df)
+        .cross_validate(
+            'classification',
+            3,
+            lambda pipeline: pipeline.train(
+                XGBoostTrainer(), input_features=['y_values', 'x_values'], target_feature='bool_classification'
+            ).metric(Metrics.log_loss()),
+        )
+        .run()
+    )
+
+    assert score > 0
