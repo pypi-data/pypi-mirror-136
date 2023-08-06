aada110ff3934289d5a6b988e158c642c18b9ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:19 +0000

    Add package abide-distributions/0.2.1/abide-distributions-0.2.1.tar.gz

---
+++ packages/abide-distributions/abide-distributions-0.2.1.tar.gz/abide-distributions-0.2.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='abide-distributions',
+      version='0.2.1',
+      description='Gaussian distributions',
+      packages=['abide-distributions'],
+      zip_safe=False)
