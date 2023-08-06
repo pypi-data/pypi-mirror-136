93609ecdbcb12c34a566b2aba964e7f5b597bb42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:32 +0000

    Add package lockstep-sdk/2022.4.32.0/lockstep_sdk-2022.4.32.0-py3-none-any.whl

---
+++ packages/lockstep-sdk/lockstep_sdk-2022.4.32.0-py3-none-any.whl/lockstep/models/provisioningfinalizerequestmodel.py
@@ -0,0 +1,31 @@
+#
+# Lockstep Software Development Kit for Python
+#
+# (c) 2021-2022 Lockstep, Inc.
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+# @author     Ted Spence <tspence@lockstep.io>
+# @copyright  2021-2022 Lockstep, Inc.
+# @version    2022.4
+# @link       https://github.com/Lockstep-Network/lockstep-sdk-python
+#
+
+
+from dataclasses import dataclass
+from lockstep.models.companymodel import CompanyModel
+from lockstep.models.erpinfomodel import ErpInfoModel
+
+@dataclass
+class ProvisioningFinalizeRequestModel:
+    """
+    Represents the data to finalize onboarding for a user
+    """
+
+    fullName: str = None
+    timeZone: str = None
+    defaultCurrency: str = None
+    company: CompanyModel = None
+    emailConnector: ErpInfoModel = None
+
