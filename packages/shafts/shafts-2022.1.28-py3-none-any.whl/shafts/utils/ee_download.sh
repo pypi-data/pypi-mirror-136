28144ece92821a6cf60809199807372b18548889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:24 +0000

    Add package shafts/2022.1.28/shafts-2022.1.28-py3-none-any.whl

---
+++ packages/shafts/shafts-2022.1.28-py3-none-any.whl/shafts/utils/ee_download.sh
@@ -0,0 +1,7 @@
+python3 GEE_ops.py \
+                --type SRTM \
+                --input_csv GEE_Download_2021_case.csv \
+                --padding 0.04 \
+                --destination Drive \
+                --dst_dir SRTM_export \
+                --path_prefix /Volumes/ForLyy/Temp/ReferenceData
