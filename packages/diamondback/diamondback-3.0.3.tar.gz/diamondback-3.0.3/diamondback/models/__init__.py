1eec350c10ab0e300ee245940d865406c11e2da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:19 +0000

    Add package diamondback/3.0.3/diamondback-3.0.3.tar.gz

---
+++ packages/diamondback/diamondback-3.0.3.tar.gz/diamondback-3.0.3/diamondback/models/__init__.py
@@ -0,0 +1,13 @@
+""" **Description**
+        Initialize.
+
+    **License**
+        `BSD-3C. <https://github.com/larryturner/diamondback/blob/master/license>`_
+        © 2018 - 2022 Larry Turner, Schneider Electric Industries SAS. All rights reserved.
+
+    **Author**
+        Larry Turner, Schneider Electric, Analytics & AI, 2018-03-22.
+"""
+
+from .DiversityModel import DiversityModel  # noqa: F401
+from .PrincipalComponentModel import PrincipalComponentModel  # noqa: F401
