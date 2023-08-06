b983317592f8ca848bf88dff766d1e98d53071bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:32 +0000

    Add package Racial-Polarized-Voting-Tools/0.1.0/Racial_Polarized_Voting_Tools-0.1.0-py3-none-any.whl

---
+++ packages/Racial-Polarized-Voting-Tools/Racial_Polarized_Voting_Tools-0.1.0-py3-none-any.whl/Racial_Block_Voting_tools/__init__.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot 
+import numpy as np
+import statsmodels.api 
+from Racial_Block_Voting_tools import(
+    Ecological_Regression
+    Homogeneous_Precinct
+)
