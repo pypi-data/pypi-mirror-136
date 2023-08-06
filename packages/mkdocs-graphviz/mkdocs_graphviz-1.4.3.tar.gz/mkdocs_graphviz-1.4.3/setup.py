c728064fa4a8d55b2bca789318c9ab8a9cc5e2ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:19 +0000

    Add package mkdocs-graphviz/1.4.3/mkdocs_graphviz-1.4.3.tar.gz

---
+++ packages/mkdocs-graphviz/mkdocs_graphviz-1.4.3.tar.gz/mkdocs_graphviz-1.4.3/setup.py
@@ -0,0 +1,84 @@
+import os
+import sys
+from setuptools import setup
+
+VERSION = '1.4.3'
+
+if sys.argv[-1] == 'publish':
+    if os.system("pip freeze | grep wheel"):
+        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
+        sys.exit()
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a {0} -m 'version {0}'".format(VERSION))
+    print("  git push --tags")
+    sys.exit()
+
+setup(
+    name="mkdocs_graphviz",
+    version=VERSION,
+    py_modules=["mkdocs_graphviz"],
+    install_requires=['Markdown>=2.3.1'],
+    author="Rodrigo Schwencke",
+    author_email="dev.hopper@lyceeperier.fr",
+    description="Render Graphviz graphs in Mkdocs, as inline SVGs and PNGs, natively compatible with Mkdocs Light & Dark Themes, directly from your Markdown (python3 version)",
+    long_description_content_type="text/markdown",
+    long_description="""Project Page : [rodrigo.schwencke/mkdocs-graphviz](https://gitlab.com/rodrigo.schwencke/mkdocs-graphviz)
+
+Some examples in these pages:
+
+* Trees : https://eskool.gitlab.io/tnsi/donnees/arbres/quelconques/
+* Graphs : https://eskool.gitlab.io/tnsi/donnees/graphes/definitions/
+
+This project is one of others mkdocs-related projects. Please have a complete look at this page for a more complete view:
+
+* https://eskool.gitlab.io/mkhack3rs/
+
+This project was initially a continuation of the great job of (from newer to older) :
+
+* All newer Credits: [rodrigo.schwencke/mkdocs-graphviz](https://gitlab.com/rodrigo.schwencke/mkdocs-graphviz)
+* Cesare Morel [cesaremorel/markdown-inline-graphviz](https://github.com/cesaremorel/markdown-inline-graphviz), and before him,
+* Steffen Prince in [sprin/markdown-inline-graphviz](https://github.com/sprin/markdown-inline-graphviz), 
+* Initially inspired by Jawher Moussa [jawher/markdown-dot](https://github.com/jawher/markdown-dot)
+    
+In order to get it work with pip (python3) and mkdocs.
+
+If you use python 2, please use the original extension instead.
+
+Licences:
+
+* The older parts are MIT
+* All newer parts (from Rodrigo Schwencke) are GPLv3+""",
+    license="GPLv3+",
+    url="https://gitlab.com/rodrigo.schwencke/mkdocs-graphviz.git",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Documentation',
+        'Topic :: Text Processing',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: OS Independent',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+    ],
+)
