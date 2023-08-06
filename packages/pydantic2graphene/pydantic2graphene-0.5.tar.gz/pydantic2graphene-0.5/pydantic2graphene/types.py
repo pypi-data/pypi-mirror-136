32dd14082222147efb1bd11b599662c0a8b2eee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:52 +0000

    Add package pydantic2graphene/0.5/pydantic2graphene-0.5.tar.gz

---
+++ packages/pydantic2graphene/pydantic2graphene-0.5.tar.gz/pydantic2graphene-0.5/pydantic2graphene/types.py
@@ -0,0 +1,11 @@
+import typing
+
+import graphene
+
+GrapheneObjectType = typing.Union[
+    graphene.ObjectType,
+    graphene.InputObjectType,
+    graphene.Interface,
+]
+
+GrapheneField = typing.TypeVar("graphene.BaseType")
