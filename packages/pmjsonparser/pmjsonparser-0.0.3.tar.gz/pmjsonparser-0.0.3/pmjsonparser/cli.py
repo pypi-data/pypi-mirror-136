4a88caaf8a38c3eb42598ab02ce4f7692b160978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:54 +0000

    Add package pmjsonparser/0.0.3/pmjsonparser-0.0.3.tar.gz

---
+++ packages/pmjsonparser/pmjsonparser-0.0.3.tar.gz/pmjsonparser-0.0.3/pmjsonparser/cli.py
@@ -0,0 +1,26 @@
+import argparse
+import os
+import pmjsonparser.parser
+
+
+def main():
+  parser = argparse.ArgumentParser(description='Process JSON to generate postgres snippets')
+  parser.add_argument('json_source', type=str, help='path of the json file to be parsed')
+  parser.add_argument('json_target', type=str, help='target path resulting postman file')
+  parser.add_argument('pm_js_target', type=str, help='target path resulting file')
+  parser.add_argument('prefix', type=str, help='service prefix')
+
+  args = parser.parse_args()
+  json_source_path = args.json_source
+  json_target_path = args.json_target
+  js_target_path = args.pm_js_target
+
+  prefix = args.prefix
+  if not os.path.exists(json_source_path):
+    print('could not find json path at' + json_source_path)
+    exit(1)
+  pmjsonparser.parser.run(json_source_path, json_target_path, js_target_path, prefix)
+
+
+if __name__ == "__main__":
+  main()
