4c7e5b8346d8c4a9efc308b1018997f2ddb2b72b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:07 +0000

    Add package pyatmo/6.2.4.dev3/pyatmo-6.2.4.dev3.tar.gz

---
+++ packages/pyatmo/pyatmo-6.2.4.dev3.tar.gz/pyatmo-6.2.4.dev3/src/pyatmo/home_coach.py
@@ -0,0 +1,33 @@
+from .auth import AbstractAsyncAuth, NetatmoOAuth2
+from .helpers import _BASE_URL
+from .weather_station import AsyncWeatherStationData, WeatherStationData
+
+_GETHOMECOACHDATA_REQ = _BASE_URL + "api/gethomecoachsdata"
+
+
+class HomeCoachData(WeatherStationData):
+    """
+    Class of Netatmo Home Coach devices (stations and modules)
+    """
+
+    def __init__(self, auth: NetatmoOAuth2) -> None:
+        """Initialize self.
+
+        Arguments:
+            auth {NetatmoOAuth2} -- Authentication information with a valid access token
+        """
+        super().__init__(auth, url_req=_GETHOMECOACHDATA_REQ)
+
+
+class AsyncHomeCoachData(AsyncWeatherStationData):
+    """
+    Class of Netatmo Home Coach devices (stations and modules)
+    """
+
+    def __init__(self, auth: AbstractAsyncAuth) -> None:
+        """Initialize self.
+
+        Arguments:
+            auth {AbstractAsyncAuth} -- Authentication information with a valid access token
+        """
+        super().__init__(auth, url_req=_GETHOMECOACHDATA_REQ)
