819d38464b9636a3a3c7cc4e4b5fa3facc9ec7a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:36 +0000

    Add package mypy-boto3-transfer/1.20.46.post1/mypy-boto3-transfer-1.20.46.post1.tar.gz

---
+++ packages/mypy-boto3-transfer/mypy-boto3-transfer-1.20.46.post1.tar.gz/mypy-boto3-transfer-1.20.46.post1/mypy_boto3_transfer/paginator.py
@@ -0,0 +1,53 @@
+"""
+Type annotations for transfer service client paginators.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_transfer/paginators.html)
+
+Usage::
+
+    ```python
+    import boto3
+
+    from mypy_boto3_transfer import TransferClient
+    from mypy_boto3_transfer.paginator import (
+        ListServersPaginator,
+    )
+
+    client: TransferClient = boto3.client("transfer")
+
+    list_servers_paginator: ListServersPaginator = client.get_paginator("list_servers")
+    ```
+"""
+from typing import Generic, Iterator, TypeVar
+
+from botocore.paginate import PageIterator
+from botocore.paginate import Paginator as Boto3Paginator
+
+from .type_defs import ListServersResponseTypeDef, PaginatorConfigTypeDef
+
+__all__ = ("ListServersPaginator",)
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListServersPaginator(Boto3Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.20.46/reference/services/transfer.html#Transfer.Paginator.ListServers)
+    [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_transfer/paginators.html#listserverspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListServersResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.20.46/reference/services/transfer.html#Transfer.Paginator.ListServers.paginate)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_transfer/paginators.html#listserverspaginator)
+        """
