66950c0305ae3c47901c49969ac21e5390f11b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:15 +0000

    Add package mcda/0.3.2/mcda-0.3.2.tar.gz

---
+++ packages/mcda/mcda-0.3.2.tar.gz/mcda-0.3.2/mcda/mavt/__init__.py
@@ -0,0 +1,14 @@
+"""
+
+References
+----------
+
+.. [1] Munda, G. (2005). UTA Methods.
+   In Multiple criteria decision analysis: State of the art surveys
+   (pp 297-343). Springer, New York, NY.
+
+.. [2] Grabisch, M., Kojadinovic, I., & Meyer, P. (2008).
+   A review of methods for capacity identification in Choquet integral based multi-attribute utility theory: Applications of the Kappalab R package.
+   European journal of operational research, 186(2), 766-785.
+
+"""  # noqa E501
