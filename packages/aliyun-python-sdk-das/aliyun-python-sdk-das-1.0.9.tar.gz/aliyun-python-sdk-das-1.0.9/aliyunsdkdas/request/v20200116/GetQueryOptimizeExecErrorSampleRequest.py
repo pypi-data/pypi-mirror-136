fdee00f6eb1bd73c500d9f15694e750db778a1f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:17 +0000

    Add package aliyun-python-sdk-das/1.0.9/aliyun-python-sdk-das-1.0.9.tar.gz

---
+++ packages/aliyun-python-sdk-das/aliyun-python-sdk-das-1.0.9.tar.gz/aliyun-python-sdk-das-1.0.9/aliyunsdkdas/request/v20200116/GetQueryOptimizeExecErrorSampleRequest.py
@@ -0,0 +1,62 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkdas.endpoint import endpoint_data
+
+class GetQueryOptimizeExecErrorSampleRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'DAS', '2020-01-16', 'GetQueryOptimizeExecErrorSample','das')
+		self.set_method('GET')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_SqlId(self):
+		return self.get_query_params().get('SqlId')
+
+	def set_SqlId(self,SqlId):
+		self.add_query_param('SqlId',SqlId)
+
+	def get_ConsoleContext(self):
+		return self.get_query_params().get('ConsoleContext')
+
+	def set_ConsoleContext(self,ConsoleContext):
+		self.add_query_param('ConsoleContext',ConsoleContext)
+
+	def get_InstanceId(self):
+		return self.get_query_params().get('InstanceId')
+
+	def set_InstanceId(self,InstanceId):
+		self.add_query_param('InstanceId',InstanceId)
+
+	def get_Engine(self):
+		return self.get_query_params().get('Engine')
+
+	def set_Engine(self,Engine):
+		self.add_query_param('Engine',Engine)
+
+	def get_Time(self):
+		return self.get_query_params().get('Time')
+
+	def set_Time(self,Time):
+		self.add_query_param('Time',Time)
