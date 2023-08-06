1a463d04f8810984785412a4728647f511eb934a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:39 +0000

    Add package poetry-demo-alphaae/0.1.0/poetry-demo-alphaae-0.1.0.tar.gz

---
+++ packages/poetry-demo-alphaae/poetry-demo-alphaae-0.1.0.tar.gz/poetry-demo-alphaae-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poetry_demo_alphaae']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.22.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'poetry-demo-alphaae',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'alphaAE',
+    'author_email': 'a1226123914@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
