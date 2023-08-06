2b92dd4ddf6f7ab7d939bbc98b3a8628bfc6f898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:01 +0000

    Add package jupyterlab-requirements/0.15.0/jupyterlab_requirements-0.15.0.tar.gz

---
+++ packages/jupyterlab-requirements/jupyterlab_requirements-0.15.0.tar.gz/jupyterlab_requirements-0.15.0/src/dependencyManagementPanel.ts
@@ -0,0 +1,27 @@
+// /**
+//  * Jupyterlab requirements.
+//  *
+//  * Jupyterlab extension for managing dependencies.
+//  *
+//  * @link   https://github.com/thoth-station/jupyterlab-requirements#readme
+//  * @file   Jupyterlab extension for managing dependencies.
+//  * @author Francesco Murdaca <fmurdaca@redhat.com>
+//  * @since  0.0.1
+//  */
+
+// /**
+//  * A Counter Lumino Widget that wraps a CounterComponent.
+//  */
+//  export class CounterWidget extends ReactWidget {
+//     /**
+//      * Constructs a new CounterWidget.
+//      */
+//     constructor() {
+//       super();
+//       this.addClass('jp-ReactWidget');
+//     }
+  
+//     render(): JSX.Element {
+//       return <CounterComponent />;
+//     }
+//   }
