c9f339d75fb8d185675877db00598cda97ca8ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:21 +0000

    Add package reqbar/1.0.5/reqbar-1.0.5.tar.gz

---
+++ packages/reqbar/reqbar-1.0.5.tar.gz/reqbar-1.0.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name= 'reqbar',
+    version='1.0.5',
+    description="To install python package requirements with a bar rather logs!",
+    packages=['reqbar'],
+    author='Muhammad Ali',
+    author_email='malirashid1994@gmail.com',
+    zip_safe=False)
