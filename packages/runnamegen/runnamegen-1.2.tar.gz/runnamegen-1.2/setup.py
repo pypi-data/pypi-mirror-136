54a4589373bc7972ede6df286344226230224400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:01 +0000

    Add package runnamegen/1.2/runnamegen-1.2.tar.gz

---
+++ packages/runnamegen/runnamegen-1.2.tar.gz/runnamegen-1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from runnamegen import __version__
+
+setup(
+    name='runnamegen',
+    version=__version__,
+
+    url='https://github.com/LuggiStruggi/RunNameGenerator.git',
+    author='Lukas König',
+    author_email='lukasmkoenig@gmx.net',
+
+    packages=find_packages(),
+	package_data={'excludes' : ['excludes.txt'],
+				  'nouns' : ['nouns/*'],
+				  'adjectives' : ['adjectives/*']},
+
+	entry_points={
+    	'console_scripts': [
+        	'runnamegen=runnamegen.commands:cmd_main',
+    	],
+	},
+
+	decription="A simple run-name generator. Creates names in the style \"<adjective> <noun>\" like \"mad zebra\""
+)
