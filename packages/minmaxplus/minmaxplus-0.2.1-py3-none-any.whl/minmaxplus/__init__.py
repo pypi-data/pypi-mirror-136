354253b60aefadb47dab87fef356902d9d62e2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:41 +0000

    Add package minmaxplus/0.2.1/minmaxplus-0.2.1-py3-none-any.whl

---
+++ packages/minmaxplus/minmaxplus-0.2.1-py3-none-any.whl/minmaxplus/__init__.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#############################################################################
+# MinMax+  extends builtin min and max functions.
+# (C) 2021 alexpdev
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+############################################################################
+"""Functions for extending builtin min max functions."""
+
+from minmaxplus.minmax import minp, maxp, minmaxp
+
+__all__ = ["minp", "maxp", "minmax"]
+
+__version__ = "1.0"
+__author__ = "alexpdev"
