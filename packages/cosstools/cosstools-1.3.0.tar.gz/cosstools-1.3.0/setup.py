25357146b23825b07ec34cdebfa30e5eba2822b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:01 +0000

    Add package cosstools/1.3.0/cosstools-1.3.0.tar.gz

---
+++ packages/cosstools/cosstools-1.3.0.tar.gz/cosstools-1.3.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '1.3.0'
+DESCRIPTION = 'Python tools by Alessandro Cossard for the CRYSTAL software'
+
+# Setting up
+setup(
+    name="cosstools",
+    version=VERSION,
+    author="Alessandro Cossard",
+    author_email="<alessandro.cossard@unito.it>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'chemistry', 'physics', 'crystal', 'topond', 'quantum'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
