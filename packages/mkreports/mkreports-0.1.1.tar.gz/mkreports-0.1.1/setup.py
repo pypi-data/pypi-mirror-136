efcacd1768f87bd8b7935378efa5b89c10a5df82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:08 +0000

    Add package mkreports/0.1.1/mkreports-0.1.1.tar.gz

---
+++ packages/mkreports/mkreports-0.1.1.tar.gz/mkreports-0.1.1/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mkreports', 'mkreports.md']
+
+package_data = \
+{'': ['*'], 'mkreports': ['.pytest_cache/*', '.pytest_cache/v/cache/*']}
+
+install_requires = \
+['PyYAML>=6.0',
+ 'anytree>=2.8.0,<3.0.0',
+ 'deepmerge>=0.3.0',
+ 'immutabledict>=2.2.1',
+ 'intervaltree>=3.1.0,<4.0.0',
+ 'mdutils>=1.3.1',
+ 'mkdocs-material>=7.3.6',
+ 'mkdocs>=1.2.3',
+ 'more-itertools>=8.12.0,<9.0.0',
+ 'plotly>=5.5.0,<6.0.0',
+ 'pytest>=6.2.5',
+ 'python-frontmatter>=1.0.0',
+ 'tabulate>=0.8.9',
+ 'typer>=0.4.0,<0.5.0',
+ 'typing-extensions<=3.11']
+
+extras_require = \
+{':python_version >= "3.7" and python_version < "4.0"': ['mkdocstrings>=0.17.0,<0.18.0']}
+
+setup_kwargs = {
+    'name': 'mkreports',
+    'version': '0.1.1',
+    'description': 'Creating static reports from python using mkdocs',
+    'long_description': None,
+    'author': 'Holger Hoefling',
+    'author_email': 'hhoeflin@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/hhoeflin/mkreports',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.7',
+}
+
+
+setup(**setup_kwargs)
