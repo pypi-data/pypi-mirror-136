afe91b023004f6821aa8b3c64edb099ed1c24f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:31 +0000

    Add package invenio-app-rdm/8.0.0.dev3/invenio_app_rdm-8.0.0.dev3-py2.py3-none-any.whl

---
+++ packages/invenio-app-rdm/invenio_app_rdm-8.0.0.dev3-py2.py3-none-any.whl/invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/index.js
@@ -0,0 +1,75 @@
+// This file is part of InvenioRDM
+// Copyright (C) 2020-2021 CERN.
+// Copyright (C) 2020-2021 Northwestern University.
+// Copyright (C) 2021 Graz University of Technology.
+//
+// Invenio RDM Records is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+import $ from "jquery";
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { RecordManagement } from "./RecordManagement";
+import { RecordVersionsList } from "./RecordVersionsList";
+import { RecordCitationField } from "./RecordCitationField";
+
+const recordManagementAppDiv = document.getElementById("recordManagement");
+const recordVersionsAppDiv = document.getElementById("recordVersions");
+const recordCitationAppDiv = document.getElementById("recordCitation");
+
+if (recordManagementAppDiv) {
+  ReactDOM.render(
+    <RecordManagement
+      record={JSON.parse(recordManagementAppDiv.dataset.record)}
+      permissions={JSON.parse(recordManagementAppDiv.dataset.permissions)}
+    />,
+    recordManagementAppDiv
+  );
+}
+
+if (recordVersionsAppDiv) {
+  ReactDOM.render(
+    <RecordVersionsList
+      record={JSON.parse(recordVersionsAppDiv.dataset.record)}
+      isPreview={JSON.parse(recordVersionsAppDiv.dataset.preview)}
+    />,
+    recordVersionsAppDiv
+  );
+}
+
+if (recordCitationAppDiv) {
+  ReactDOM.render(
+    <RecordCitationField
+      record={JSON.parse(recordCitationAppDiv.dataset.record)}
+      styles={JSON.parse(recordCitationAppDiv.dataset.styles)}
+      defaultStyle={JSON.parse(recordCitationAppDiv.dataset.defaultstyle)}
+    />,
+    recordCitationAppDiv
+  );
+}
+
+$(".ui.accordion").accordion({
+  selector: {
+    trigger: '.title .dropdown'
+  }
+});
+
+$("#record-doi-badge").click(function () {
+  $("#doi-modal").modal("show");
+});
+
+$(".ui.tooltip-popup").popup();
+
+$(".preview-link").on("click", function (event) {
+  $("#preview").find(".title").html(event.target.dataset.fileKey);
+});
+
+$("#jump-btn").on("click", function (event) {
+  document.documentElement.scrollTop = 0;
+});
+
+// func to toggle the icon class
+$(".panel-heading").click(function () {
+  $("i", this).toggleClass("down right");
+});
