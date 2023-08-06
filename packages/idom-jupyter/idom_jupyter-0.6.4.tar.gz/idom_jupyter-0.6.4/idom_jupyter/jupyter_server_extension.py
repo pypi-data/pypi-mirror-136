5d2ed6db59c20b6f86ef0f2e10c2377e67283187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:07 +0000

    Add package idom-jupyter/0.6.4/idom_jupyter-0.6.4.tar.gz

---
+++ packages/idom-jupyter/idom_jupyter-0.6.4.tar.gz/idom_jupyter-0.6.4/idom_jupyter/jupyter_server_extension.py
@@ -0,0 +1,30 @@
+from urllib.parse import urljoin
+
+from appdirs import user_data_dir
+from notebook.notebookapp import NotebookApp
+from idom.config import IDOM_WED_MODULES_DIR
+from tornado.web import StaticFileHandler
+from tornado.web import Application
+
+
+IDOM_WED_MODULES_DIR.current = user_data_dir("idom-jupyter", "idom-team")
+
+
+def _load_jupyter_server_extension(notebook_app: NotebookApp):
+    web_app: Application = notebook_app.web_app
+    base_url = web_app.settings["base_url"]
+    route_pattern = urljoin(base_url, rf"_idom_web_modules/(.*)")
+    web_app.add_handlers(
+        host_pattern=r".*$",
+        host_handlers=[
+            (
+                route_pattern,
+                StaticFileHandler,
+                {"path": str(IDOM_WED_MODULES_DIR.current.absolute())},
+            ),
+        ],
+    )
+
+
+# compat for older versions of Jupyter
+load_jupyter_server_extension = _load_jupyter_server_extension
