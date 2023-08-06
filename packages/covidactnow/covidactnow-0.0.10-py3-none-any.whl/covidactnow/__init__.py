ad360acad5656355d6d52946e0c3fe609b9f3329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:35 +0000

    Add package covidactnow/0.0.10/covidactnow-0.0.10-py3-none-any.whl

---
+++ packages/covidactnow/covidactnow-0.0.10-py3-none-any.whl/covidactnow/__init__.py
@@ -0,0 +1,55 @@
+import requests
+
+# As CovidActNow's API is a REST API, queries are stored as URLs.
+
+class User:
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def queryUrl(self, state):
+        return f"http://api.covidactnow.org/v2/state/{state}.json?apiKey=" + self.api_key
+
+    def infRate(self, state):
+        # Infection Rate
+        if True:
+            return float(requests.get(self.queryUrl(state)).json()['metrics']['infectionRate'])
+        else:
+            self.infRate = "NaN"
+        
+        # Positive Rate
+        try:
+            self.posRate = round(100*float(requests.get(self.queryUrl(state)).json()['metrics']['testPositivityRatio']), 2)
+        except:
+            self.posRate = "NaN"
+        
+        # Vax Rate
+        try:
+            self.vaxRate = round(100*float(requests.get(self.queryUrl(state)).json()['metrics']['vaccinationsCompletedRatio']), 2)
+        except:
+            self.vaxRate = "NaN"
+        
+        # Free Bed Percentage
+        try:
+            self.freeBedPercentage = round(100*float(requests.get(self.queryUrl(state)).json()['actuals']['hospitalBeds']['currentUsageTotal'])/\
+                float(requests.get(self.queryUrl(state)).json()['actuals']['hospitalBeds']['capacity']), 2)
+        except:
+            self.freeBedPercentage = "NaN"
+        
+        # New Cases
+        try:
+            self.newCases = int(requests.get(self.queryUrl(state)).json()['actuals']['newCases'])
+        except:
+            self.newCases = "NaN"
+
+        # New Deaths
+        try:
+            self.newDeaths = int(requests.get(self.queryUrl(state)).json()['actuals']['newDeaths'])
+        except:
+            self.newDeaths = "NaN"
+        
+        # Covid Bed Percentage
+        try:
+            self.covidBedPercentage = round(100*float(requests.get(self.queryUrl(state)).json()['actuals']['hospitalBeds']['currentUsageCovid'])/\
+                float(requests.get(self.queryUrl(state)).json()['actuals']['hospitalBeds']['capacity']), 2)
+        except:
+            self.covidBedPercentage = "NaN"
