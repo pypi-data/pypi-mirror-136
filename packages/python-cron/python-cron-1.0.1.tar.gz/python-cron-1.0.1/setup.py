04e48d20fa0177d6ebb2b309259c87423d52a609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:24 +0000

    Add package python-cron/1.0.1/python-cron-1.0.1.tar.gz

---
+++ packages/python-cron/python-cron-1.0.1.tar.gz/python-cron-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import setuptools
+setup(
+  name = 'python-cron',
+  packages = ['pycron'],
+  version = '1.0.1',
+  license='MIT',
+  description = 'A simple Python library providing cron functionality via the use of a single decorator.',
+  author = 'whdev1',
+  author_email = 'whdev1@protonmail.com',
+  url = 'https://github.com/whdev1/pycron',
+  download_url = 'https://github.com/whdev1/pycron/archive/refs/tags/v1.0.1.tar.gz',
+  keywords = ['pycron', 'cron', 'timed functions', 'automation'],
+  install_requires=['croniter'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10'
+  ],
+)
