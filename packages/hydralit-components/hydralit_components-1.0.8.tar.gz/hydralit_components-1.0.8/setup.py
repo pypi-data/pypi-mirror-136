df9e6e9a96d63081ea292607d05d89e9da1e13ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:40 +0000

    Add package hydralit-components/1.0.8/hydralit_components-1.0.8.tar.gz

---
+++ packages/hydralit-components/hydralit_components-1.0.8.tar.gz/hydralit_components-1.0.8/setup.py
@@ -0,0 +1,48 @@
+from os import path,listdir,system
+import setuptools
+import hydralit_components as hc
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name=hc.__packagename__,
+    version='.'.join(str(hc.__version__)),
+    description='Components to use with or without the Hydralit package.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/tanglespace/hydralit_components',
+    author=hc.__author__,
+    author_email='c6lculus8ntr0py@gmail.com',
+    include_package_data=True,
+    license="Apache 2",
+    project_urls={
+        'Documentation': 'https://github.com/tanglespace/hydralit_components',
+        'Source': 'https://github.com/tanglespace/hydralit_components',
+        'Tracker': 'https://github.com/tanglespace/hydralit_components/issues',
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ],
+    install_requires=[
+        'streamlit>=0.86',
+        'lxml',
+        'bs4'
+    ],
+    python_requires='>=3.6',
+    keywords=[
+        'Streamlit',
+        'Web',
+        'Responsive',
+        'Deployment',
+        'Web Application',
+        'Presentation',
+    ],
+)
