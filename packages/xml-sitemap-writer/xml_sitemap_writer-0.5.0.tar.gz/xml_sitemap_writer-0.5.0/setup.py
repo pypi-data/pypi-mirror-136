a0027fe15755085b90688a40a4c6f56392fbd7ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:47 +0000

    Add package xml-sitemap-writer/0.5.0/xml_sitemap_writer-0.5.0.tar.gz

---
+++ packages/xml-sitemap-writer/xml_sitemap_writer-0.5.0.tar.gz/xml_sitemap_writer-0.5.0/setup.py
@@ -0,0 +1,45 @@
+"""
+Package definition
+"""
+from setuptools import setup
+
+VERSION = "0.5.0"
+
+# @see https://packaging.python.org/tutorials/packaging-projects/#creating-setup-py
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# @see https://github.com/pypa/sampleproject/blob/master/setup.py
+setup(
+    name="xml_sitemap_writer",
+    version=VERSION,
+    author="Maciej Brencz",
+    author_email="maciej.brencz@gmail.com",
+    license="MIT",
+    description="Python3 package for writing large XML sitemaps with no external dependencies",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pigs-will-fly/py-xml-sitemap-writer",
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 5 - Production/Stable",
+        # Pick your license as you wish
+        "License :: OSI Approved :: MIT License",
+        # Specify the Python versions you support here.
+        "Programming Language :: Python :: 3",
+    ],
+    py_modules=["xml_sitemap_writer"],
+    extras_require={
+        "dev": [
+            "black==21.12b0",
+            "coveralls==3.3.1",
+            "pylint==2.12.2",
+            "pytest==6.2.5",
+            "pytest-cov==3.0.0",
+        ]
+    },
+)
