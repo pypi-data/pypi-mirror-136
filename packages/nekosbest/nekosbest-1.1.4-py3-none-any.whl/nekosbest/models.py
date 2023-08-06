67dd7f601c666f189280018abe6161afea5018ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:11 +0000

    Add package nekosbest/1.1.4/nekosbest-1.1.4-py3-none-any.whl

---
+++ packages/nekosbest/nekosbest-1.1.4-py3-none-any.whl/nekosbest/models.py
@@ -0,0 +1,79 @@
+"""
+A simple async Python wrapper for nekos.best API.
+Copyright (C) 2021-present  PredaaA
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from typing import Optional
+
+CATEGORIES = (
+    "baka",
+    "bite",
+    "blush",
+    "bored",
+    "cry",
+    "cuddle",
+    "dance",
+    "facepalm",
+    "feed",
+    "happy",
+    "highfive",
+    "hug",
+    "kiss",
+    "laugh",
+    "nekos",
+    "pat",
+    "poke",
+    "pout",
+    "shrug",
+    "slap",
+    "sleep",
+    "smile",
+    "smug",
+    "stare",
+    "think",
+    "thumbsup",
+    "tickle",
+    "wave",
+    "wink",
+)
+
+
+class Result:
+    """Represents an image result from the API.
+
+    Attributes
+    ----------
+    url: Optional[str]
+        The image / gif url.
+    artist_href: Optional[str]
+        The artist's page URL.
+    artist_name: Optional[str]
+        The artist's name.
+    source_url: Optional[str]
+        The original link of this image.
+    anime_name: Optional[str]
+        The englified name of the anime the gif was taken from.
+    """
+
+    def __init__(self, data: dict):
+        self.url: Optional[str] = data.get("url")
+        self.artist_href: Optional[str] = data.get("artist_href")
+        self.artist_name: Optional[str] = data.get("artist_name")
+        self.source_url: Optional[str] = data.get("source_url")
+        self.anime_name: Optional[str] = data.get("anime_name")
+
+    def __repr__(self) -> str:
+        return f"<Result url={self.url} artist_href={self.artist_href} artist_name={self.artist_name} source_url={self.source_url} anime_name={self.anime_name}>"
