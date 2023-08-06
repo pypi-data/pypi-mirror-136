bbcc5e909eb9f758377e176e7e588e40117f5a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:07 +0000

    Add package demo-tools/0.0.1/demo-tools-0.0.1.tar.gz

---
+++ packages/demo-tools/demo-tools-0.0.1.tar.gz/demo-tools-0.0.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="demo-tools",
+    version="0.0.1",
+    author="mirrordust",
+    # author_email="gsmr19re@outlook.com",
+    description="A set of tools which demos for computer learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/mirrordust/demo-tools",
+    project_urls={
+        # "Bug Tracker": "https://github.com/mirrordust/demo-tools/issues",
+    },
+    entry_points={
+        "console_scripts": [
+            "demo-tools=demotools.command_line:main",
+            "dt=demotools.command_line:dt",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
