af2c601b60511571b44c1706de1b47cb47085c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:00 +0000

    Add package epcpm/2022.1.1/epcpm-2022.1.1.tar.gz

---
+++ packages/epcpm/epcpm-2022.1.1.tar.gz/epcpm-2022.1.1/src/epcpm/__build.py
@@ -0,0 +1,9 @@
+# This file has been generated
+
+
+build_system = 'GitHub Actions'
+build_id = '1763385442'
+build_number = '245'
+build_version = None
+job_id = '1763385442_245'
+job_url = 'https://github.com/epcpower/pm/actions/runs/1763385442'
