3ff322a12f0793731241eb2b617df6fbb081d286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:23 +0000

    Add package promote/1.11.1/promote-1.11.1.tar.gz

---
+++ packages/promote/promote-1.11.1.tar.gz/promote-1.11.1/setup.py
@@ -0,0 +1,27 @@
+import os
+from distutils.core import setup
+from setuptools import find_packages
+
+# Get version from defined python file
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'promote', 'version.py')) as fh:
+    version = fh.read().strip()
+    exec(version)
+
+    setup(
+        name="promote",
+        version=__version__,
+        author="Alteryx",
+        author_email="promotedev@alteryx.com",
+        url="https://github.com/alteryx/promote-python",
+        project_urls={'Source': "https://github.com/alteryx/promote-python"},
+        packages=find_packages(),
+        description="Client for deploying Python models to Promote.",
+        license="BSD",
+        classifiers=[],
+        install_requires=[
+            "requests>=2.22.0",
+            "schema"
+        ],
+        long_description=open("README.rst").read(),
+        keywords=['alteryx', 'scikit-learn', 'numpy', 'pandas'],
+    )
