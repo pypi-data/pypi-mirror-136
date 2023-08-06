3387346b8bd5d9dedf04caf03a5adb41485660cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:40 +0000

    Add package lithopscloud/1.0.0b81/lithopscloud-1.0.0b81-py3-none-any.whl

---
+++ packages/lithopscloud/lithopscloud-1.0.0b81-py3-none-any.whl/lithopscloud/modules/gen2/image.py
@@ -0,0 +1,24 @@
+from lithopscloud.modules.config_builder import ConfigBuilder, update_decorator, spinner
+from typing import Any, Dict
+from lithopscloud.modules.utils import find_obj, find_default
+
+
+class ImageConfig(ConfigBuilder):
+    
+    def __init__(self, base_config: Dict[str, Any]) -> None:
+        super().__init__(base_config)
+
+    @update_decorator
+    def run(self) -> Dict[str, Any]:
+
+        @spinner
+        def get_image_objects():
+            return self.ibm_vpc_client.list_images().get_result()['images']
+
+        image_objects = get_image_objects()
+
+        default = find_default(self.defaults, image_objects, id='image_id') or 'ibm-ubuntu-20-04-2-minimal-amd64-1'
+        image_obj = find_obj(image_objects, 'Please choose \033[92mUbuntu\033[0m 20.04 VM image, currently only Ubuntu supported', default=default)
+
+        return image_obj['id'], image_obj['minimum_provisioned_size']
+
