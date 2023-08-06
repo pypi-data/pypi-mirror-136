b6049966eabc50a393c67f56a8aaac928d78cabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:00 +0000

    Add package package-gen/0.0.4/package-gen-0.0.4.tar.gz

---
+++ packages/package-gen/package-gen-0.0.4.tar.gz/package-gen-0.0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+with open('README.md', 'r', encoding='utf-8') as fl:
+    long_description = fl.read()
+
+
+setuptools.setup(
+    name="package-gen",
+    version="0.0.4",
+    author="Zhang",
+    author_email="",
+    url='https://github.com/suifengpiaoyang/package-gen',
+    description="Generate python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "pk-gen=packagegenerator:main",
+        ]
+    },
+    python_requires='>=3.6.0'
+)
