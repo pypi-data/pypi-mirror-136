2ef9c8df408a6a4416597a5a0ee36f937cb83c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:30 +0000

    Add package discordLevelingSystem/1.1.0/discordLevelingSystem-1.1.0.tar.gz

---
+++ packages/discordLevelingSystem/discordLevelingSystem-1.1.0.tar.gz/discordLevelingSystem-1.1.0/discordLevelingSystem/__init__.py
@@ -0,0 +1,36 @@
+"""
+Discord Leveling System
+~~~~~~~~~~~~~~~~~~~~~~~
+
+A library to implement a leveling system into a discord bot. Contains features such as XP, level, ranks, and role awards.
+
+:copyright: (c) 2021-present Defxult#8269
+:license: MIT
+
+"""
+
+from .announcement import LevelUpAnnouncement
+from .leveling_system import DiscordLevelingSystem
+from .member_data import MemberData
+from .role_awards import RoleAward
+
+
+def version_info():
+    """Shows the current version, release type, and patch of the library
+    - `version` Current version of the library
+    - `releasetype` Either "final" (the PyPI version) or "pre-release" (the GitHub version)
+    - `patch` The last significant bug fix
+
+    >>> print(discordLevelingSystem.version_info())
+    """
+    from collections import namedtuple
+    VersionInfo = namedtuple('VersionInfo', ['version', 'releasetype', 'patch'])
+    return VersionInfo(version='1.1.0', releasetype='final', patch='1a')
+
+__source__ = 'https://github.com/Defxult/discordLevelingSystem'
+__all__ = (
+    'LevelUpAnnouncement',
+    'DiscordLevelingSystem',
+    'MemberData',
+    'RoleAward'
+)
---
+++ packages/discordLevelingSystem/discordLevelingSystem-1.1.0.tar.gz/discordLevelingSystem-1.1.0/setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+
+def _get_readme():
+    with open('README.md', encoding='utf-8') as fp:
+        return fp.read()
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+tags = [
+    'database',
+    'discord',
+    'discord bot',
+    'discord.py',
+    'discord py',
+    'discord level',
+    'discord leveling',
+    'discord leveling system',
+    'level',
+    'levels',
+    'leveling',
+    'level up',
+    'level system',
+    'mee6',
+    'pycord',
+    'py-cord',
+    'rank',
+    'ranking',
+    'role award',
+    'xp'
+]
+
+details = {
+    'Changelog' : 'https://github.com/Defxult/discordLevelingSystem/blob/main/CHANGELOG.md'
+}
+
+setup(
+    author='Defxult#8269',
+    name='discordLevelingSystem',
+    description='A library to implement a leveling system into a discord bot. Contains features such as XP, level, ranks, and role awards.',
+    version='1.1.0', 
+    url='https://github.com/Defxult/discordLevelingSystem',
+    project_urls=details,
+    classifiers=classifiers,
+    long_description=_get_readme(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    keywords=tags,
+    packages=find_packages(),
+    install_requires=['aiosqlite>=0.17.0']
+    #! update to pycord 2.0.0 when released
+)
