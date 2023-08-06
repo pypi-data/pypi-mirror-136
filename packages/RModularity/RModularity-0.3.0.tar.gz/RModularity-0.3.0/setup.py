986b25a88b0b6583df3b7bb1ed52ec1a0d1dfc9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:17 +0000

    Add package RModularity/0.3.0/RModularity-0.3.0.tar.gz

---
+++ packages/RModularity/RModularity-0.3.0.tar.gz/RModularity-0.3.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RModularity",
+    version="0.3.0",
+    author="Filipi N. Silva",
+    author_email="filipi@iu.edu",
+    description="Python library to calculate Robustness modularity of networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/filipinascimento/RModularity",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
