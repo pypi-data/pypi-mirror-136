fd7f8dc3e1c437fd3efe344361ff7efa3f22a1de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:21 +0000

    Add package signalyzer/0.2.1/signalyzer-0.2.1.tar.gz

---
+++ packages/signalyzer/signalyzer-0.2.1.tar.gz/signalyzer-0.2.1/src/signalyzer/__init__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+signalyzer.py
+~~~~~~~~~~~~~
+Public package API.
+
+:copyright: (c) 2021-2022 by Jochen Gerhaeusser.
+:license: BSD, see LICENSE for details.
+"""
+
+from .trace import *
+from .statemachine import *
+
+# package exports
+__all__ = [
+    # Representations
+    'Point2D', 'Vector',
+    'Point3D',
+    'State2D', 'State3D',
+
+    # Signal Processing
+    'Statistics',
+    'SlewRateLimiter',
+    'ExponentialSmoothing',
+    'LinearRegression',
+    'IIRFilter',
+
+    # State Machines
+    'Statemachine',
+
+    # Traces
+    'Trace', 'vectorize',
+    'Traces', 'as_traces',
+    'VectorTraces', 'polar',
+    'StatisticsTraces',
+    'MovingAverageTraces',
+    'SetTraces', 'combine',
+    'SlewRateLimiterTraces',
+    'ExponentialSmoothingTraces',
+    'LinearRegressionTraces',
+
+]
+
+__version__ = '0.2.1'
+
