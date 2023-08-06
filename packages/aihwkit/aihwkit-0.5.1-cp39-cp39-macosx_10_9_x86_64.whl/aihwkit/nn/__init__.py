f5403c21cf7f95a40d5e4ef422ba108a9d1a5f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:08 +0000

    Add package aihwkit/0.5.1/aihwkit-0.5.1-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/aihwkit/aihwkit-0.5.1-cp39-cp39-macosx_10_9_x86_64.whl/aihwkit/nn/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# (C) Copyright 2020, 2021 IBM. All Rights Reserved.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Neural network modules."""
+
+# Convenience imports for easier access to the classes.
+
+from aihwkit.nn.modules.container import AnalogSequential
+from aihwkit.nn.modules.conv import AnalogConv1d, AnalogConv2d, AnalogConv3d
+from aihwkit.nn.modules.linear import AnalogLinear
+from aihwkit.nn.modules.lstm import AnalogLSTM
+from aihwkit.nn.modules.linear_mapped import AnalogLinearMapped
+from aihwkit.nn.modules.conv_mapped import (
+    AnalogConv1dMapped, AnalogConv2dMapped, AnalogConv3dMapped
+)
