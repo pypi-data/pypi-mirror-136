eb9522cad16a2ff58c96f2e21ad89d194911aa79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:54 +0000

    Add package py-senertec/0.0.2/py-senertec-0.0.2.tar.gz

---
+++ packages/py-senertec/py-senertec-0.0.2.tar.gz/py-senertec-0.0.2/src/senertec/canipvalue.py
@@ -0,0 +1,7 @@
+class canipvalue(object):
+    def __init__(self):
+        self.boardName = str()
+        self.friendlyBoardName = str()
+        self.dataValue = float()
+        self.dataUnit = str()
+        self.friendlyDataName = str()
