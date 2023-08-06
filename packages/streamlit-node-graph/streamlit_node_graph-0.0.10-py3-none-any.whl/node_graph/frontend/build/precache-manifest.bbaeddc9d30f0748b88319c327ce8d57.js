08c5056d69e9030250138fb6abb60382e4305ca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:31 +0000

    Add package streamlit-node-graph/0.0.10/streamlit_node_graph-0.0.10-py3-none-any.whl

---
+++ packages/streamlit-node-graph/streamlit_node_graph-0.0.10-py3-none-any.whl/node_graph/frontend/build/precache-manifest.bbaeddc9d30f0748b88319c327ce8d57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bcc9991fab6dfb1a13b60bbd9d717fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "384768fc8255e06a332c",
+    "url": "./static/css/2.f0e4fa12.chunk.css"
+  },
+  {
+    "revision": "ffab5ad0b8b870a557ba",
+    "url": "./static/css/main.8269038f.chunk.css"
+  },
+  {
+    "revision": "384768fc8255e06a332c",
+    "url": "./static/js/2.c7696da5.chunk.js"
+  },
+  {
+    "revision": "7f2dba955ac4eb46d1d3be8b94f2d1ea",
+    "url": "./static/js/2.c7696da5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffab5ad0b8b870a557ba",
+    "url": "./static/js/main.8b86ca71.chunk.js"
+  },
+  {
+    "revision": "f58b7c5c12615fd0dddb",
+    "url": "./static/js/runtime-main.2cb3803c.js"
+  }
+]);
