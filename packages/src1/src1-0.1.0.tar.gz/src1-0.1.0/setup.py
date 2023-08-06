175ff642f4053965f310a9bdc02680b7b50f7a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:14 +0000

    Add package src1/0.1.0/src1-0.1.0.tar.gz

---
+++ packages/src1/src1-0.1.0.tar.gz/src1-0.1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src1',
+    version='0.1.0',
+    packages=['src1']
+)
---
+++ packages/src1/src1-0.1.0.tar.gz/src1-0.1.0/src1/config1.py
@@ -0,0 +1,16 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class mytransformer(BaseEstimator, TransformerMixin):
+	def __init__(self):
+		print('hello')
+	def fit(self, X, y = None):
+		print("fit!")
+		return self
+
+	def transform(self, X, y = None):
+		print("transform")
+		return X
+    
+	def temp(self):
+		print("temp")
+
