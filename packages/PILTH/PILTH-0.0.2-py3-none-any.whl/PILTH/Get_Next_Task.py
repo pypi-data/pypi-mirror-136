b7b51b78f4bef322ad8eef562137b96be7bc00fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:14 +0000

    Add package PILTH/0.0.2/PILTH-0.0.2-py3-none-any.whl

---
+++ packages/PILTH/PILTH-0.0.2-py3-none-any.whl/PILTH/Get_Next_Task.py
@@ -0,0 +1,7 @@
+import pyfiglet
+
+def gimmeLinkAndCode():
+    Link = pyfiglet.figlet_format("https://the-hunt.vercel.app/36032071", font = "digital")
+    Code = pyfiglet.figlet_format("earthshakers", font = "bubble" )
+    print(Link)
+    print(Code)
