3dc6325952d6cd22d6473d1cdbfd188582d02b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:26 +0000

    Add package libvoiced/0.8.0/libvoiced-0.8.0.tar.gz

---
+++ packages/libvoiced/libvoiced-0.8.0.tar.gz/libvoiced-0.8.0/tests/test_skeleton.py
@@ -0,0 +1,14 @@
+from libvoiced.skeleton import main
+
+__author__ = "Taylor Monacelli"
+__copyright__ = "Taylor Monacelli"
+__license__ = "MPL-2.0"
+
+
+def test_main(capsys):
+    """CLI Tests"""
+    # capsys is a pytest fixture that allows asserts agains stdout/stderr
+    # https://docs.pytest.org/en/stable/capture.html
+    main(["7"])
+    # captured = capsys.readouterr()
+    # assert "The 7-th Fibonacci number is 13" in captured.out
