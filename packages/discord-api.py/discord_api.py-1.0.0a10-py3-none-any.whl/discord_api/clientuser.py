c17939d874052a6af6e42566a6b2bdc91d1dadff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:32 +0000

    Add package discord-api.py/1.0.0a10/discord_api.py-1.0.0a10-py3-none-any.whl

---
+++ packages/discord-api.py/discord_api.py-1.0.0a10-py3-none-any.whl/discord_api/clientuser.py
@@ -0,0 +1,20 @@
+class ClientUser:
+    """
+    Attributes
+    ----------
+    id : str
+        return a bot's id.
+    name : str
+        return a bot's name.
+    discriminator : str
+        return a bot's discriminator.
+    """
+    def __init__(self, data):
+        self.id = data["id"]
+        self.name = data["username"]
+        self.avatar = data["avatar"]
+        self.discriminator = data["discriminator"]
+        
+    @classmethod
+    def from_dict(cls, data):
+        return cls(data)
