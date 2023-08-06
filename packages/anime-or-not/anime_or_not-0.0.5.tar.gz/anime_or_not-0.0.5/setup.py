4cd86d766326aa04a8cc2ee9a24f1b8d9d0bbc56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:26 +0000

    Add package anime-or-not/0.0.5/anime_or_not-0.0.5.tar.gz

---
+++ packages/anime-or-not/anime_or_not-0.0.5.tar.gz/anime_or_not-0.0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Classifies if an image is "anime" or not'
+LONG_DESCRIPTION = 'Classifies if an image is "anime" or not'
+
+# Setting up
+setup(
+    name="anime_or_not",
+    version=VERSION,
+    author="LaihoE",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['onnxruntime', 'Pillow', 'torchvision','torch'],
+    include_package_data=True
+)
+
