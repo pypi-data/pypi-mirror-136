780065afdc5502dced65fe2a5bcc38510fac6d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:58 +0000

    Add package satip/2.3.11/satip-2.3.11.tar.gz

---
+++ packages/satip/satip-2.3.11.tar.gz/satip-2.3.11/README.md
@@ -0,0 +1,21 @@
+# Satip
+
+[![PyPI version](https://badge.fury.io/py/satip.svg)](https://badge.fury.io/py/satip)
+
+> Satip is a library for <b>sat</b>ellite <b>i</b>mage <b>p</b>rocessing, and provides all of the functionality necessary for retrieving, and storing EUMETSAT data
+
+<br>
+
+### Installation
+
+To install the `satip` library please run:
+
+```bash
+pip install satip
+```
+
+Or if you're working in the development environment you can run the following from the directory root:
+
+```bash
+pip install -e .
+```
---
+++ packages/satip/satip-2.3.11.tar.gz/satip-2.3.11/setup.py
@@ -0,0 +1,22 @@
+# read the contents of your README file
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+install_requires = (this_directory / "requirements.txt").read_text().splitlines()
+
+setup(
+    name="satip",
+    version="2.3.11",
+    license="MIT",
+    description="Satip provides the functionality necessary for retrieving, and storing EUMETSAT data",
+    author="Jacob Bieker, Ayrton Bourn",
+    author_email="info@openclimatefix.org",
+    company="Open Climate Fix Ltd",
+    install_requires=install_requires,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+)
