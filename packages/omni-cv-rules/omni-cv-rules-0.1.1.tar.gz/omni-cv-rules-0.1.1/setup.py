8e01c7ff8e2e3af2b527e80ddd357fbf55f1d99f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:10 +0000

    Add package omni-cv-rules/0.1.1/omni-cv-rules-0.1.1.tar.gz

---
+++ packages/omni-cv-rules/omni-cv-rules-0.1.1.tar.gz/omni-cv-rules-0.1.1/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['omni_cv_rules', 'omni_cv_rules.coconut']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Pillow',
+ 'bqplot',
+ 'frozendict',
+ 'ipywidgets',
+ 'numpy',
+ 'pandas',
+ 'pprintpp',
+ 'py-omni-converter',
+ 'torch',
+ 'torchvision']
+
+setup_kwargs = {
+    'name': 'omni-cv-rules',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Kento Masui',
+    'author_email': 'nameissoap@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
