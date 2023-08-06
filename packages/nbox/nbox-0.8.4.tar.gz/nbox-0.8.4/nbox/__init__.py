f8b1b9e9ba220441d9f37d461bce2a9d259741a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:08 +0000

    Add package nbox/0.8.4/nbox-0.8.4.tar.gz

---
+++ packages/nbox/nbox-0.8.4.tar.gz/nbox-0.8.4/nbox/__init__.py
@@ -0,0 +1,11 @@
+from .init import reset_log
+reset_log()
+
+from .model import Model
+from .load import load, plug, PRETRAINED_MODELS
+from .parsers import ImageParser, TextParser
+from .jobs import Instance
+from .auth import AWSClient, GCPClient, OCIClient, DOClient, AzureClient
+from .operators import Operator
+
+__version__ = "0.8.4"
