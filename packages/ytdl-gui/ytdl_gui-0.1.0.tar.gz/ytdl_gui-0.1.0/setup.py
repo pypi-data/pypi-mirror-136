2d83a272241e9ad0df7d15263a18458e4fc1f05f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:10 +0000

    Add package ytdl-gui/0.1.0/ytdl_gui-0.1.0.tar.gz

---
+++ packages/ytdl-gui/ytdl_gui-0.1.0.tar.gz/ytdl_gui-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ytdl_gui']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyGObject>=3.42.0,<4.0.0', 'yt-dlp>=2022.1.21,<2023.0.0']
+
+entry_points = \
+{'console_scripts': ['ytdl-gui = ytdl_gui:main']}
+
+setup_kwargs = {
+    'name': 'ytdl-gui',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'sleepntsheep',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
