83c8e49f30d3a92222473736f030f1bc3b08de5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:40 +0000

    Add package eyja-nats-hub/0.1.8/eyja_nats_hub-0.1.8-py3-none-any.whl

---
+++ packages/eyja-nats-hub/eyja_nats_hub-0.1.8-py3-none-any.whl/eyja_nats/nats_route_group.py
@@ -0,0 +1,27 @@
+from typing import Callable, Any, List, Optional
+
+from pydantic import BaseModel
+
+from .nats_route import NATSRoute
+
+
+class NATSRouteGroup(BaseModel):
+    routes: Optional[List[NATSRoute]] = []
+
+    def add_route(self, queue: str, handler: Callable, data_cls: Any = None):
+        self.routes.append(
+            NATSRoute(
+                queue=queue,
+                handler=handler,
+                data_cls=data_cls,
+            )
+        )
+
+    def add_route_group(self, group: 'NATSRouteGroup'):
+        self.routes += group.routes
+
+    def queue(self, queue: str, data_cls: Any = None):
+        def decorator(func):
+            self.add_route(queue, func, data_cls)
+            return func
+        return decorator
