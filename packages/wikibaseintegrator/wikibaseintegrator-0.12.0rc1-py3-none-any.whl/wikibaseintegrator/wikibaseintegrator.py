aa1676692eb2db229050db2ea34e4a1c42524fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:00 +0000

    Add package wikibaseintegrator/0.12.0rc1/wikibaseintegrator-0.12.0rc1-py3-none-any.whl

---
+++ packages/wikibaseintegrator/wikibaseintegrator-0.12.0rc1-py3-none-any.whl/wikibaseintegrator/wikibaseintegrator.py
@@ -0,0 +1,34 @@
+"""
+Main class of the Library.
+"""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from wikibaseintegrator.entities.item import Item
+from wikibaseintegrator.entities.lexeme import Lexeme
+from wikibaseintegrator.entities.mediainfo import MediaInfo
+from wikibaseintegrator.entities.property import Property
+
+if TYPE_CHECKING:
+    from wikibaseintegrator.wbi_login import _Login
+
+
+class WikibaseIntegrator:
+
+    def __init__(self, is_bot: bool = False, login: _Login = None):
+        """
+        This function initializes a WikibaseIntegrator instance to quickly access different entity type instances.
+
+        :param is_bot: declare if the bot flag must be set when you interact with the Mediawiki API.
+        :param login: a wbi_login instance needed when you try to access a restricted Mediawiki instance.
+        """
+        # Runtime variables
+        self.is_bot = is_bot or False
+        self.login = login
+
+        # Quick access to entities
+        self.item = Item(api=self)
+        self.property = Property(api=self)
+        self.lexeme = Lexeme(api=self)
+        self.mediainfo = MediaInfo(api=self)
