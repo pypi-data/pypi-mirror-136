b75a02fb3288941d5a5af8ca4d6557c7c3966f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:55 +0000

    Add package bone/0.1.0/bone-0.1.0.tar.gz

---
+++ packages/bone/bone-0.1.0.tar.gz/bone-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='bone',
+    version='0.1.0',
+    description='',
+    url='http://github.com/qe/bone',
+    author='Alex Ismodes',
+    author_email='helloemailmerighthere@gmail.com',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    keywords='bone',
+    packages=['bone'],
+    install_requires=[''],
+    )
