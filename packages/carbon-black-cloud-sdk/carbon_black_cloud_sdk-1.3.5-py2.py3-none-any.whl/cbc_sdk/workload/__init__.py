8efca1692f3b6ffd1c4066c9d59467923cb8f17d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:09 +0000

    Add package carbon-black-cloud-sdk/1.3.5/carbon_black_cloud_sdk-1.3.5-py2.py3-none-any.whl

---
+++ packages/carbon-black-cloud-sdk/carbon_black_cloud_sdk-1.3.5-py2.py3-none-any.whl/cbc_sdk/workload/__init__.py
@@ -0,0 +1,5 @@
+from cbc_sdk.workload.vm_workloads_search import ComputeResource
+from cbc_sdk.workload.sensor_lifecycle import SensorKit
+
+# Maintain link for easier migration
+from cbc_sdk.platform.vulnerability_assessment import Vulnerability
