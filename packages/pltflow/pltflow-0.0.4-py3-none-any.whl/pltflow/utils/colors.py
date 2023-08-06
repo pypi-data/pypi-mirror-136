2b1982e5402fd3a04f116bcd50ddbd098e6d349e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:03 +0000

    Add package pltflow/0.0.4/pltflow-0.0.4-py3-none-any.whl

---
+++ packages/pltflow/pltflow-0.0.4-py3-none-any.whl/pltflow/utils/colors.py
@@ -0,0 +1,12 @@
+plt_tab_colors = [
+    "tab:red",
+    "tab:blue",
+    "tab:green",
+    "tab:orange",
+    "tab:olive",
+    "tab:pink",
+    "tab:purple",
+    "tab:brown",
+    "tab:gray",
+    "tab:cyan",
+]
