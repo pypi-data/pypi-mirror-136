0fc3e2fe452f016b9efc84e22608360e24c1a109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:36 +0000

    Add package ert/2.31.2/ert-2.31.2-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/ert/ert-2.31.2-cp39-cp39-macosx_10_15_x86_64.whl/ert/serialization/_registry.py
@@ -0,0 +1,33 @@
+from typing import Tuple
+
+from pyrsistent import pmap
+from pyrsistent.typing import PMap
+
+from ._serializer import (
+    Serializer,
+    _ecl_sum_serializer,
+    _json_serializer,
+    _yaml_serializer,
+)
+
+_registry: PMap[str, Serializer] = pmap(
+    {
+        "application/json": _json_serializer(),
+        "application/x-yaml": _yaml_serializer(),
+        "multipart/x-eclipse.sum": _ecl_sum_serializer(),
+    }
+)
+
+
+def has_serializer(mime: str) -> bool:
+    return mime in _registry
+
+
+def get_serializer(mime: str) -> Serializer:
+    if mime not in _registry:
+        raise ValueError(f"no serializer for {mime}")
+    return _registry[mime]
+
+
+def registered_types() -> Tuple[str, ...]:
+    return tuple(sorted(_registry.keys()))
