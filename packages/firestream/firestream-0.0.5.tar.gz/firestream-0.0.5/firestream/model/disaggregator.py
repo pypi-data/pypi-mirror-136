755ea8a61025bc62f70a7b995aa316a2c84c0e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:20 +0000

    Add package firestream/0.0.5/firestream-0.0.5.tar.gz

---
+++ packages/firestream/firestream-0.0.5.tar.gz/firestream-0.0.5/firestream/model/disaggregator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 24 23:59:01 2022
+
+@author: Mohammad Asim
+"""
+
+class Disaggregator():
+    
+    def __init__(self, name, stream, model):
+        
+        """
+            Initialize disaggregator
+        """
+        self.model = model
+        self.stream = stream
+        self.history = []
+        
+    def train(self):
+
+        """
+            Function to starting training 
+        """
+        self.history = self.model.fit(self.stream.train_dataset, validation_data=self.stream.validation_dataset, epochs=self.stream.epochs)
