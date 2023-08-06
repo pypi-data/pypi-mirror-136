c7bb0711af1222327986e839c716210c12b08385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:47 +0000

    Add package openssh-key-parser/0.0.3/openssh_key_parser-0.0.3.tar.gz

---
+++ packages/openssh-key-parser/openssh_key_parser-0.0.3.tar.gz/openssh_key_parser-0.0.3/openssh_key/utils.py
@@ -0,0 +1,52 @@
+"""
+Utility classes and methods.
+"""
+
+import typing
+
+
+def is_abstract(cls: typing.Type[typing.Any]) -> bool:
+    """
+    Returns whether ``cls`` is an abstract class.
+    """
+    return hasattr(cls, '__abstractmethods__') \
+        and len(cls.__abstractmethods__) != 0
+
+
+ReadonlyStaticPropertyTypeVar = typing.TypeVar(
+    'ReadonlyStaticPropertyTypeVar'
+)
+
+
+class readonly_static_property(
+    typing.Generic[ReadonlyStaticPropertyTypeVar]
+):
+    """
+    A static property that calls the first method in an object's MRO of the
+    same name as that of the provided method.
+
+    Args:
+        getter
+            The method the name of which is that of the method to call.
+    """
+
+    def __init__(
+        self,
+        getter: typing.Union[
+            typing.Callable[[], ReadonlyStaticPropertyTypeVar],
+            typing.Callable[[typing.Type[typing.Any]], ReadonlyStaticPropertyTypeVar]
+        ]
+    ) -> None:
+        self._getter = getter
+
+    def __get__(
+        self,
+        obj: typing.Any,
+        cls: typing.Optional[typing.Type[typing.Any]] = None
+    ) -> ReadonlyStaticPropertyTypeVar:
+        if cls is None:
+            cls = type(obj)
+        return typing.cast(
+            ReadonlyStaticPropertyTypeVar,
+            getattr(cls, self._getter.__name__)()
+        )
