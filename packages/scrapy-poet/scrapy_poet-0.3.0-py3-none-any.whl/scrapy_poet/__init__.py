01bfb38f204bfe077591c72adbecea2b54823e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:23 +0000

    Add package scrapy-poet/0.3.0/scrapy_poet-0.3.0-py3-none-any.whl

---
+++ packages/scrapy-poet/scrapy_poet-0.3.0-py3-none-any.whl/scrapy_poet/__init__.py
@@ -0,0 +1,7 @@
+from .middleware import InjectionMiddleware
+from .api import callback_for, DummyResponse
+from .page_input_providers import (
+    PageObjectInputProvider,
+    CacheDataProviderMixin,
+    ResponseDataProvider,
+)
