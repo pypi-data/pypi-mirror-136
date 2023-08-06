51e079d12e28c4d21dd5abe8c5cca74db4b03f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:47 +0000

    Add package sportsdata/0.0.0/sportsdata-0.0.0-py3-none-any.whl

---
+++ packages/sportsdata/sportsdata-0.0.0-py3-none-any.whl/sports/nba/nba_team.py
@@ -0,0 +1,8 @@
+from enum import IntEnum
+
+
+class NBA_Team(object):
+    def __init__(self):
+        self.name           =  None   # Name of the Team (Without City)
+        self.full_name      =  None   # Full Name Of The Team (Including City)
+        self.team_id        =  None
