ad15f0d990fd4612e2ff2d1f8fbee1b32426b4ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:05 +0000

    Add package EduCDM/0.0.13/EduCDM-0.0.13.tar.gz

---
+++ packages/EduCDM/EduCDM-0.0.13.tar.gz/EduCDM-0.0.13/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+test_deps = [
+    'pytest>=4',
+    'pytest-cov>=2.6.0',
+    'pytest-flake8',
+]
+
+setup(
+    name='EduCDM',
+    version='0.0.13',
+    extras_require={
+        'test': test_deps,
+    },
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "tqdm",
+        "numpy>=1.16.5",
+        "scikit-learn",
+        "pandas",
+        "longling>=1.3.33"
+    ],  # And any other dependencies for needs
+    entry_points={
+    },
+)
