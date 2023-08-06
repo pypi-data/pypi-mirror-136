245cd95a2148b84b4b0b014e266dea70c32e6cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:01 +0000

    Add package tsts/1.0.0/tsts-1.0.0-py3-none-any.whl

---
+++ packages/tsts/tsts-1.0.0-py3-none-any.whl/tsts/__init__.py
@@ -0,0 +1,27 @@
+from . import (
+    cfg,
+    dataloaders,
+    datasets,
+    losses,
+    metrics,
+    models,
+    optimizers,
+    solvers,
+    thirdparty,
+    transforms,
+    utils,
+)
+
+__all__ = [
+    "cfg",
+    "dataloaders",
+    "datasets",
+    "losses",
+    "metrics",
+    "models",
+    "optimizers",
+    "solvers",
+    "thirdparty",
+    "transforms",
+    "utils",
+]
