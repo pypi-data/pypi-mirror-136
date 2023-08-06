9af9904a3d219e9417843837964dafffba24c46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:39 +0000

    Add package edx-enterprise/3.38.7/edx_enterprise-3.38.7-py3-none-any.whl

---
+++ packages/edx-enterprise/edx_enterprise-3.38.7-py3-none-any.whl/integrated_channels/cornerstone/urls.py
@@ -0,0 +1,20 @@
+"""
+URL definitions for Cornerstone API.
+"""
+
+from django.conf.urls import url
+
+from integrated_channels.cornerstone.views import CornerstoneCoursesListView, CornerstoneCoursesUpdates
+
+urlpatterns = [
+    url(
+        r'^course-list$',
+        CornerstoneCoursesListView.as_view(),
+        name='cornerstone-course-list'
+    ),
+    url(
+        r'course-updates',
+        CornerstoneCoursesUpdates.as_view(),
+        name='cornerstone-course-updates'
+    )
+]
