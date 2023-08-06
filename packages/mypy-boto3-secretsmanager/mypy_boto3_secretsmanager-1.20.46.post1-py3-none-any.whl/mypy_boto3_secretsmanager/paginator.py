6ceebbb3bfcea596fad54196358289f41ee2e49c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:45 +0000

    Add package mypy-boto3-secretsmanager/1.20.46.post1/mypy_boto3_secretsmanager-1.20.46.post1-py3-none-any.whl

---
+++ packages/mypy-boto3-secretsmanager/mypy_boto3_secretsmanager-1.20.46.post1-py3-none-any.whl/mypy_boto3_secretsmanager/paginator.py
@@ -0,0 +1,58 @@
+"""
+Type annotations for secretsmanager service client paginators.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_secretsmanager/paginators.html)
+
+Usage::
+
+    ```python
+    import boto3
+
+    from mypy_boto3_secretsmanager import SecretsManagerClient
+    from mypy_boto3_secretsmanager.paginator import (
+        ListSecretsPaginator,
+    )
+
+    client: SecretsManagerClient = boto3.client("secretsmanager")
+
+    list_secrets_paginator: ListSecretsPaginator = client.get_paginator("list_secrets")
+    ```
+"""
+from typing import Generic, Iterator, Sequence, TypeVar
+
+from botocore.paginate import PageIterator
+from botocore.paginate import Paginator as Boto3Paginator
+
+from .literals import SortOrderTypeType
+from .type_defs import FilterTypeDef, ListSecretsResponseTypeDef, PaginatorConfigTypeDef
+
+__all__ = ("ListSecretsPaginator",)
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListSecretsPaginator(Boto3Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.20.46/reference/services/secretsmanager.html#SecretsManager.Paginator.ListSecrets)
+    [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_secretsmanager/paginators.html#listsecretspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        Filters: Sequence["FilterTypeDef"] = ...,
+        SortOrder: SortOrderTypeType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListSecretsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.20.46/reference/services/secretsmanager.html#SecretsManager.Paginator.ListSecrets.paginate)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_secretsmanager/paginators.html#listsecretspaginator)
+        """
