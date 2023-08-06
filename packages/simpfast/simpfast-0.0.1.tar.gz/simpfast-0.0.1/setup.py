9b2f51ba5580294a94f72d5de9efdc724d3dcfe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:44 +0000

    Add package simpfast/0.0.1/simpfast-0.0.1.tar.gz

---
+++ packages/simpfast/simpfast-0.0.1.tar.gz/simpfast-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='simpfast',
+  version='0.0.1',
+  description='A simple liberary to use fastai in easy way',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Israr Ahmad',
+  author_email='israrbuaa@gmail.com',
+  license='MIT',
+  classifiers=classifiers,
+  keywords='', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
