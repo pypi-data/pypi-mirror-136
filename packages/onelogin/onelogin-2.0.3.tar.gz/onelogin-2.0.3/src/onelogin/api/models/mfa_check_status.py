5c65f79c1893a2c9908e627a78e9442579a5f1ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:25 +0000

    Add package onelogin/2.0.3/onelogin-2.0.3.tar.gz

---
+++ packages/onelogin/onelogin-2.0.3.tar.gz/onelogin-2.0.3/src/onelogin/api/models/mfa_check_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+from onelogin.api.util.utils import str2int
+
+from .base import Base
+
+import sys
+if sys.version_info[0] >= 3:
+    unicode = str
+
+
+class MFACheckStatus(Base):
+    def __init__(self, data):
+        self.id = data.get('id', None)
+        self.status = data.get('status', None)
+        device_id = data.get('device_id', None)
+        if device_id:
+            device_id = unicode(device_id)
+        self.device_id = device_id
