cf5bb47f13b14f09cb252c113a1cc992795b3168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:14 +0000

    Add package modelify/0.0.1/modelify-0.0.1-py3-none-any.whl

---
+++ packages/modelify/modelify-0.0.1-py3-none-any.whl/modelify/frameworks/lightgbm.py
@@ -0,0 +1,36 @@
+from onnxmltools.convert import convert_lightgbm
+from modelify.schema import InputList
+from modelify.frameworks import BaseController
+from modelify.utils import message
+
+
+class LightGBMController(BaseController):
+    def __init__(self):
+        self.name = "LIGHTGBM"
+        super().__init__()
+
+    def deploy(self, model_name, model, inputs:InputList, version=9):
+        message("Model is converting...")
+        export_path, file_name = self.export_model(model, inputs=inputs, version=version)
+        message("Model converted successfully")
+        super().upload_pipeline(framework_name= self.name, export_path=export_path, file_name=file_name,
+         model_name=model_name, inputs=inputs,input_type=inputs.type)
+        message("Done")
+
+    def update(self, model_name, model, inputs:InputList, version=9):
+        message("Model is converting...")
+        export_path, file_name = self.export_model(model, inputs=inputs, version=version)
+        message("Model converted successfully")
+        super().upload_pipeline(framework_name= self.name, export_path=export_path, file_name=file_name,
+         model_name=model_name, inputs=inputs,input_type=inputs.type, update=True)
+        message("Done")
+
+        
+
+    def export_model(self, model, inputs:InputList, version):
+        initial_type = inputs.convert_onnx()
+        onnx = convert_lightgbm(model, initial_types=initial_type, target_opset=version)
+        export_path, file_name = super().save_onnx_file(onnx)
+
+        return export_path, file_name
+
