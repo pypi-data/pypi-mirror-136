3d18a179aa09b5fad8c300984ba2a7417d624295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:11 +0000

    Add package monocdk/1.141.0/monocdk-1.141.0-py3-none-any.whl

---
+++ packages/monocdk/monocdk-1.141.0-py3-none-any.whl/monocdk/lambda_layer_node_proxy_agent/__init__.py
@@ -0,0 +1,67 @@
+'''
+# AWS Lambda Layer with the NPM dependency proxy-agent
+
+This module exports a single class called `NodeProxyAgentLayer` which is a `lambda.Layer` that bundles the NPM dependency [`proxy-agent`](https://www.npmjs.com/package/proxy-agent).
+
+> * proxy-agent Version: 5.0.0
+
+Usage:
+
+```python
+# Example automatically generated from non-compiling source. May contain errors.
+fn = lambda_.Function(...)
+fn.add_layers(NodeProxyAgentLayer(stack, "NodeProxyAgentLayer"))
+```
+
+[`proxy-agent`](https://www.npmjs.com/package/proxy-agent) will be installed under `/opt/nodejs/node_modules`.
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from .._jsii import *
+
+import constructs
+from ..aws_lambda import LayerVersion as _LayerVersion_34d6006f
+
+
+class NodeProxyAgentLayer(
+    _LayerVersion_34d6006f,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="monocdk.lambda_layer_node_proxy_agent.NodeProxyAgentLayer",
+):
+    '''(experimental) An AWS Lambda layer that includes the NPM dependency ``proxy-agent``.
+
+    :stability: experimental
+    :exampleMetadata: fixture=_generated
+
+    Example::
+
+        # The code below shows an example of how to instantiate this type.
+        # The values are placeholders you should change.
+        from monocdk import lambda_layer_node_proxy_agent
+        
+        node_proxy_agent_layer = lambda_layer_node_proxy_agent.NodeProxyAgentLayer(self, "MyNodeProxyAgentLayer")
+    '''
+
+    def __init__(self, scope: constructs.Construct, id: builtins.str) -> None:
+        '''
+        :param scope: -
+        :param id: -
+
+        :stability: experimental
+        '''
+        jsii.create(self.__class__, self, [scope, id])
+
+
+__all__ = [
+    "NodeProxyAgentLayer",
+]
+
+publication.publish()
