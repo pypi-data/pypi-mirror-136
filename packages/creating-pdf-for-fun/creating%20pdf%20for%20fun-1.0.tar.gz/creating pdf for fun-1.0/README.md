09011a897e30bcc94d1b0862b1de9c62c66c8869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:50 +0000

    Add package creating-pdf-for-fun/1.0/creating%20pdf%20for%20fun-1.0.tar.gz

---
+++ packages/creating-pdf-for-fun/creating%20pdf%20for%20fun-1.0.tar.gz/creating pdf for fun-1.0/README.md
@@ -0,0 +1,1 @@
+Hey it is created by me and this is the best package I created. YAAA HOOO!!!!!!!!!
---
+++ packages/creating-pdf-for-fun/creating%20pdf%20for%20fun-1.0.tar.gz/creating pdf for fun-1.0/creatingpdfforfun/createpdf.py
@@ -0,0 +1,3 @@
+def create():
+    """this function creates a pdf file"""
+    print("pdf file created")
