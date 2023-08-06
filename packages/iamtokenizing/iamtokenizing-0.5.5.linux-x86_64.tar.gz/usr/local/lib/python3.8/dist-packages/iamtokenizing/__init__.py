a5d0ce9b95b75174c9e630e72f0b136b16dade36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:43 +0000

    Add package iamtokenizing/0.5.5/iamtokenizing-0.5.5.linux-x86_64.tar.gz

---
+++ packages/iamtokenizing/iamtokenizing-0.5.5.linux-x86_64.tar.gz/usr/local/lib/python3.8/dist-packages/iamtokenizing/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pkg_resources import get_distribution
+__version__ = get_distribution("iamtokenizing").version
+
+from .ngrams import NGrams
+from .chargrams import CharGrams
+from .regex_detector import RegexDetector
+from .context_detector import ContextDetector
+
+
