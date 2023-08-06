dd7a13f06361733652918748c0e8542727914499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:09 +0000

    Add package Landonius13-distributions/0.1/Landonius13_distributions-0.1.tar.gz

---
+++ packages/Landonius13-distributions/Landonius13_distributions-0.1.tar.gz/Landonius13_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Landonius13_distributions',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['Landonius13_distributions'],
+      author='Landon Settle',
+      author_email='settlelandon@yahoo.com',
+      zip_safe=False)
