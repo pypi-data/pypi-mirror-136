567522c262406c827baf863d091f150b9c3ea3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:01 +0000

    Add package senkalib/0.0.6/senkalib-0.0.6.tar.gz

---
+++ packages/senkalib/senkalib-0.0.6.tar.gz/senkalib-0.0.6/src/senkalib/caaj_plugin.py
@@ -0,0 +1,14 @@
+from abc import ABCMeta, abstractmethod
+from senkalib.caaj_journal import CaajJournal
+from senkalib.chain.transaction import Transaction
+
+class CaajPlugin(metaclass=ABCMeta):
+  chain = None
+
+  @abstractmethod
+  def can_handle(cls, transaction:Transaction) -> bool:
+    pass
+
+  @abstractmethod
+  def get_caajs(cls, address:str, transaction:Transaction) -> CaajJournal:
+    pass
