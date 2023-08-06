717f0bb2b00f17a59c21618606e32fca6f18ba62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:29 +0000

    Add package dqsegdb/2.0.0/dqsegdb-2.0.0-py2.py3-none-any.whl

---
+++ packages/dqsegdb/dqsegdb-2.0.0-py2.py3-none-any.whl/dqsegdb-2.0.0.data/data/etc/dqsegdb-user-env.csh
@@ -0,0 +1,2 @@
+setenv PATH /dqsegdb-2.0.0.data/scripts:${PATH}
+setenv PYTHONPATH /dqsegdb-2.0.0.data/platlib::${PYTHONPATH}
---
+++ packages/dqsegdb/dqsegdb-2.0.0-py2.py3-none-any.whl/dqsegdb-2.0.0.data/data/etc/dqsegdb-user-env.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+PATH=/dqsegdb-2.0.0.data/scripts:${PATH}
+export PATH
+PYTHONPATH=/dqsegdb-2.0.0.data/platlib::${PYTHONPATH}
+export PYTHONPATH
