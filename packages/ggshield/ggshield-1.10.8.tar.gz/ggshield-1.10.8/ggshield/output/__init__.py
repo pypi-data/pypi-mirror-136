000cb347a8e83ff3b89c8970df339b2ce827d383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:48 +0000

    Add package ggshield/1.10.8/ggshield-1.10.8.tar.gz

---
+++ packages/ggshield/ggshield-1.10.8.tar.gz/ggshield-1.10.8/ggshield/output/__init__.py
@@ -0,0 +1,6 @@
+from .json import JSONOutputHandler
+from .output_handler import OutputHandler
+from .text import TextOutputHandler
+
+
+__all__ = ["TextOutputHandler", "OutputHandler", "JSONOutputHandler"]
