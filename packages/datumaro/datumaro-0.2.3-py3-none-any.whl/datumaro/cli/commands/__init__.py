6b68e3e78e830ec4dc9aa39697bd8c84c8c4bfc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:25 +0000

    Add package datumaro/0.2.3/datumaro-0.2.3-py3-none-any.whl

---
+++ packages/datumaro/datumaro-0.2.3-py3-none-any.whl/datumaro/cli/commands/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2019-2021 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+# pylint: disable=redefined-builtin
+
+from . import (
+    add, checkout, commit, convert, create, diff, download, explain, export,
+    filter, import_, info, log, merge, patch, remove, stats, status, transform,
+    validate,
+)
