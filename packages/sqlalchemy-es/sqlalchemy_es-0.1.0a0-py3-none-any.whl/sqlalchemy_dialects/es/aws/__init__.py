5cdbb1e856baf4f2b289c23dee545dcf03a508bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:29 +0000

    Add package sqlalchemy-es/0.1.0a0/sqlalchemy_es-0.1.0a0-py3-none-any.whl

---
+++ packages/sqlalchemy-es/sqlalchemy_es-0.1.0a0-py3-none-any.whl/sqlalchemy_dialects/es/aws/__init__.py
@@ -0,0 +1,37 @@
+from sqlalchemy_dialects.es.exceptions import (
+    DatabaseError,
+    DataError,
+    Error,
+    IntegrityError,
+    InterfaceError,
+    InternalError,
+    NotSupportedError,
+    OperationalError,
+    ProgrammingError,
+    Warning,
+)
+from .api import connect
+
+
+__all__ = [
+    "connect",
+    "apilevel",
+    "threadsafety",
+    "paramstyle",
+    "DataError",
+    "DatabaseError",
+    "Error",
+    "IntegrityError",
+    "InterfaceError",
+    "InternalError",
+    "NotSupportedError",
+    "OperationalError",
+    "ProgrammingError",
+    "Warning",
+]
+
+
+apilevel = "2.0"
+# Threads may share the module and connections
+threadsafety = 2
+paramstyle = "pyformat"
