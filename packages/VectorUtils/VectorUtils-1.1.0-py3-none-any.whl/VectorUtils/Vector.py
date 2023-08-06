f6921e0785124ce48e11f60ddc7e2261176a520e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:02 +0000

    Add package VectorUtils/1.1.0/VectorUtils-1.1.0-py3-none-any.whl

---
+++ packages/VectorUtils/VectorUtils-1.1.0-py3-none-any.whl/VectorUtils/Vector.py
@@ -0,0 +1,43 @@
+from math import sqrt
+
+
+class Vector2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def mag(self):
+        return sqrt(self.x**2 + self.y**2)
+
+    def toTuple(self):
+        return self.x, self.y
+
+    def toInt(self):
+        return Vector2(int(self.x), int(self.y))
+
+    def toFloat(self):
+        return Vector2(float(self.x), float(self.y))
+
+    def __add__(self, other):
+        if type(other) == Vector2:
+            return Vector2(self.x + other.x, self.y + other.y)
+        elif type(other) == int or float:
+            return Vector2(self.x + other, self.y + other)
+
+    def __sub__(self, other):
+        if type(other) == Vector2:
+            return Vector2(self.x - other.x, self.y - other.y)
+        elif type(other) == int or float:
+            return Vector2(self.x - other, self.y - other)
+
+    def __mul__(self, other):
+        if type(other) == Vector2:
+            return Vector2(self.x * other.x, self.y * other.y)
+        elif type(other) == int or float:
+            return Vector2(self.x * other, self.y * other)
+
+    def __truediv__(self, other):
+        if type(other) == Vector2:
+            return Vector2(self.x / other.x, self.y / other.y)
+        elif type(other) == int or float:
+            return Vector2(self.x / other, self.y / other)
