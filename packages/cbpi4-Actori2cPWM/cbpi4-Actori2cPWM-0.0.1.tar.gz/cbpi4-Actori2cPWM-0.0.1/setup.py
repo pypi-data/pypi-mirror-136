42a107d9ada887cdc343f94a0854d33fbf24c15e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:54 +0000

    Add package cbpi4-Actori2cPWM/0.0.1/cbpi4-Actori2cPWM-0.0.1.tar.gz

---
+++ packages/cbpi4-Actori2cPWM/cbpi4-Actori2cPWM-0.0.1.tar.gz/cbpi4-Actori2cPWM-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='cbpi4-Actori2cPWM',
+      version='0.0.1',
+      description='CraftBeerPi Plugin',
+      author='',
+      author_email='',
+      url='',
+      include_package_data=True,
+      package_data={
+        # If any package contains *.txt or *.rst files, include them:
+      '': ['*.txt', '*.rst', '*.yaml'],
+      'cbpi4-Actori2cPWM': ['*','*.txt', '*.rst', '*.yaml']},
+      packages=['cbpi4-Actori2cPWM'],
+      install_requires=[
+          'cbpi>=4.0.0.33',
+          'adafruit-circuitpython-motorkit'
+      ],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
+     )
