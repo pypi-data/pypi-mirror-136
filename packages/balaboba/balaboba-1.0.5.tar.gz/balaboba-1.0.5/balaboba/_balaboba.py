5e04aeafdbcb1949059868f31e5d486ec44d5b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:40 +0000

    Add package balaboba/1.0.5/balaboba-1.0.5.tar.gz

---
+++ packages/balaboba/balaboba-1.0.5.tar.gz/balaboba-1.0.5/balaboba/_balaboba.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from typing import Optional
+
+from cloudscraper import CloudScraper
+
+
+def fetch(query: str, intro: int, session: CloudScraper) -> str:
+    with session.post(
+        "https://yandex.ru/lab/api/yalm/text3",
+        json={"query": query, "intro": intro, "filter": 1},
+    ) as resp:
+        r = resp.json()
+    return f"{r['query']}{r['text']}"
+
+
+def balaboba(
+    query: str, *, intro: int = 0, session: Optional[CloudScraper] = None
+) -> str:
+    """Отправка запроса Яндекс Балабобе.
+
+    Args:
+        query (str): Текст для Балабобы.
+        intro (int, optional): Вариант стилизации.
+            0 - Без стиля. По умолчанию.
+            1 - Теории заговора.
+            2 - ТВ-репортажи.
+            3 - Тосты.
+            4 - Пацанские цитаты.
+            5 - Рекламные слоганы.
+            6 - Короткие истории.
+            7 - Подписи в Instagram.
+            8 - Короче, Википедия.
+            9 - Синопсисы фильмов.
+            10 - Гороскоп.
+            11 - Народные мудрости.
+            18 - Новый Европейский Театр.
+            19 - Яндекс.Директ.
+            20 - Новогодние открытки.
+        session (Optional[CloudScraper], optional): По умолчанию None.
+
+    Returns:
+        str: Ответ Балабобы.
+    """
+    if isinstance(session, CloudScraper):
+        return fetch(query, intro, session)
+    with CloudScraper.create_scraper() as session:
+        return fetch(query, intro, session)
