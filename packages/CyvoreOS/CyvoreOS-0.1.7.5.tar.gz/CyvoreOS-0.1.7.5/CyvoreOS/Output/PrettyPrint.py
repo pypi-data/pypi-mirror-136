3821e1e1d23c924925579dcc9931d1c7eaaf1b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:38 +0000

    Add package CyvoreOS/0.1.7.5/CyvoreOS-0.1.7.5.tar.gz

---
+++ packages/CyvoreOS/CyvoreOS-0.1.7.5.tar.gz/CyvoreOS-0.1.7.5/CyvoreOS/Output/PrettyPrint.py
@@ -0,0 +1,43 @@
+
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+ENDC = '\033[0m'
+
+def printStatus(url, status):
+    color = GREEN
+
+    if status != "200":
+        color = RED
+
+    print(color+status+ENDC+' '+ url)
+
+class mycolors:
+
+    reset='\033[0m'
+    reverse='\033[07m'
+    bold='\033[01m'
+    class foreground:
+        orange='\033[33m'
+        blue='\033[34m'
+        purple='\033[35m'
+        lightgreen='\033[92m'
+        lightblue='\033[94m'
+        pink='\033[95m'
+        lightcyan='\033[96m'
+        red='\033[31m'
+        green='\033[32m'
+        cyan='\033[36m'
+        lightgrey='\033[37m'
+        darkgrey='\033[90m'
+        lightred='\033[91m'
+        yellow='\033[93m'
+    class background:
+        black='\033[40m'
+        blue='\033[44m'
+        cyan='\033[46m'
+        lightgrey='\033[47m'
+        purple='\033[45m'
+        green='\033[42m'
+        orange='\033[43m'
+        red='\033[41m'
