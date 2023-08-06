bc4819e99b020eb8e28fdfe61c00999ef904575f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:40 +0000

    Add package deezer-python/5.0.1/deezer_python-5.0.1-py3-none-any.whl

---
+++ packages/deezer-python/deezer_python-5.0.1-py3-none-any.whl/deezer/__init__.py
@@ -0,0 +1,28 @@
+from deezer.client import Client
+from deezer.pagination import PaginatedList
+from deezer.resources import (
+    Album,
+    Artist,
+    Genre,
+    Playlist,
+    Radio,
+    Resource,
+    Track,
+    User,
+)
+
+__version__ = "5.0.1"
+__all__ = [
+    "Client",
+    "PaginatedList",
+    "Resource",
+    "Album",
+    "Artist",
+    "Genre",
+    "Playlist",
+    "Track",
+    "User",
+    "Radio",
+]
+
+USER_AGENT = f"Deezer Python API Wrapper v{__version__}"
