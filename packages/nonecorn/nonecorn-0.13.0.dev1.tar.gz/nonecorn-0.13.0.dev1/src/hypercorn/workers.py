3583b2b4d38f2fe14c06dae33bfd7abf638e87a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:17 +0000

    Add package nonecorn/0.13.0.dev1/nonecorn-0.13.0.dev1.tar.gz

---
+++ packages/nonecorn/nonecorn-0.13.0.dev1.tar.gz/nonecorn-0.13.0.dev1/src/hypercorn/workers.py
@@ -0,0 +1,121 @@
+from typing import List, Callable, Awaitable, Any
+import asyncio
+import signal
+from functools import partial
+
+from gunicorn.workers.base import Worker
+from gunicorn.sock import TCPSocket
+
+from hypercorn.config import Config as _Config, Sockets
+from hypercorn.asyncio import serve as asyncio_serve
+
+
+class Config(_Config):
+    sockets: Sockets = None
+
+    def create_sockets(self) -> Sockets:
+        return self.sockets
+
+
+def transfer_sock(gunicorn_sock: List[TCPSocket]) -> Sockets:
+    secure_sockets = []
+    insecure_sockets = []
+    for sock in gunicorn_sock:
+        if sock.conf.is_ssl:
+            secure_sockets.append(sock.sock)
+        else:
+            insecure_sockets.append(sock.sock)
+    return Sockets(secure_sockets=secure_sockets, insecure_sockets=insecure_sockets, quic_sockets=[])
+
+
+class HypercornAsyncioWorker(Worker):
+    """
+    Borrowed from uvicorn
+    """
+    CONFIG_KWARGS = {"worker_class": "asyncio"}
+
+    def __init__(self, *args, **kwargs):
+        super(HypercornAsyncioWorker, self).__init__(*args, **kwargs)
+
+        config_kwargs = {
+            "access_log_format": self.cfg.access_log_format,
+            "accesslog": self.cfg.accesslog,
+            "loglevel": self.cfg.loglevel.upper(),
+            "errorlog": self.cfg.errorlog,
+            "logconfig": self.cfg.logconfig,
+            "keep_alive_timeout": self.cfg.keepalive,
+            "graceful_timeout": self.cfg.graceful_timeout,
+            "group": self.cfg.group,
+            "dogstatsd_tags": self.cfg.dogstatsd_tags,
+            "statsd_host": self.cfg.statsd_host,
+            "statsd_prefix": self.cfg.statsd_prefix,
+            "umask": self.cfg.umask,
+            "user": self.cfg.user
+        }
+        config_kwargs.update(logconfig_dict=self.cfg.logconfig_dict if self.cfg.logconfig_dict else None)
+
+        if self.cfg.is_ssl:
+            ssl_kwargs = {
+                "keyfile": self.cfg.ssl_options.get("keyfile"),
+                "certfile": self.cfg.ssl_options.get("certfile"),
+                "ca_certs": self.cfg.ssl_options.get("ca_certs"),
+            }
+            if self.cfg.ssl_options.get("ciphers") is not None:
+                ssl_kwargs.update(ciphers=self.cfg.ssl_options.get("ciphers"))
+            config_kwargs.update(ssl_kwargs)
+
+        if self.cfg.settings["backlog"].value:
+            config_kwargs["backlog"] = self.cfg.settings["backlog"].value
+
+        config_kwargs.update(self.CONFIG_KWARGS)
+        self.config = Config()  # todo
+        for k, v in config_kwargs.items():
+            setattr(self.config, k, v)
+
+    def init_signals(self):
+        for s in self.SIGNALS:
+            signal.signal(s, signal.SIG_DFL)
+
+    def run(self):
+        asgi_app = self.wsgi
+        self.config.sockets = transfer_sock(self.sockets)
+        if self.config.worker_class == "trio":
+            from hypercorn.trio import serve as trio_serve
+            import trio
+
+            async def start():
+                async with trio.open_nursery() as nursery:
+                    async def wrap(func: Callable[[], Awaitable[Any]]) -> None:
+                        await func()
+                        nursery.cancel_scope.cancel()
+
+                    nursery.start_soon(wrap, partial(trio_serve, asgi_app, self.config))
+                    await wrap(self.trio_callback_notify)
+
+            trio.run(start)
+            return
+        if self.config.worker_class == "uvloop":
+            import uvloop
+            uvloop.install()
+        asyncio.run(asyncio.wait([asyncio_serve(asgi_app, self.config),
+                                  self.asyncio_callback_notify()],
+                                 return_when=asyncio.FIRST_COMPLETED))
+
+    async def asyncio_callback_notify(self):
+        while True:
+            self.notify()
+            await asyncio.sleep(self.timeout)
+
+    async def trio_callback_notify(self):
+        import trio
+        while True:
+            self.notify()
+            await trio.sleep(self.timeout)
+
+
+class HypercornUvloopWorker(HypercornAsyncioWorker):
+    CONFIG_KWARGS = {"worker_class": "uvloop"}
+
+
+class HypercornTrioWorker(HypercornAsyncioWorker):
+    CONFIG_KWARGS = {"worker_class": "trio"}
---
+++ packages/nonecorn/nonecorn-0.13.0.dev1.tar.gz/nonecorn-0.13.0.dev1/tests/protocol/test_h2.py
@@ -0,0 +1,81 @@
+from __future__ import annotations
+
+import asyncio
+from unittest.mock import call, Mock
+
+import pytest
+
+from hypercorn.asyncio.tcp_server import EventWrapper
+from hypercorn.asyncio.worker_context import WorkerContext
+from hypercorn.config import Config
+from hypercorn.events import Closed, RawData
+from hypercorn.protocol.h2 import BUFFER_HIGH_WATER, BufferCompleteError, H2Protocol, StreamBuffer
+
+try:
+    from unittest.mock import AsyncMock
+except ImportError:
+    # Python < 3.8
+    from mock import AsyncMock  # type: ignore
+
+
+@pytest.mark.asyncio
+async def test_stream_buffer_push_and_pop(event_loop: asyncio.AbstractEventLoop) -> None:
+    stream_buffer = StreamBuffer(EventWrapper)
+
+    async def _push_over_limit() -> bool:
+        await stream_buffer.push(b"a" * (BUFFER_HIGH_WATER + 1))
+        return True
+
+    task = event_loop.create_task(_push_over_limit())
+    assert not task.done()  # Blocked as over high water
+    await stream_buffer.pop(BUFFER_HIGH_WATER // 4)
+    assert not task.done()  # Blocked as over low water
+    await stream_buffer.pop(BUFFER_HIGH_WATER // 4)
+    assert (await task) is True
+
+
+@pytest.mark.asyncio
+async def test_stream_buffer_drain(event_loop: asyncio.AbstractEventLoop) -> None:
+    stream_buffer = StreamBuffer(EventWrapper)
+    await stream_buffer.push(b"a" * 10)
+
+    async def _drain() -> bool:
+        await stream_buffer.drain()
+        return True
+
+    task = event_loop.create_task(_drain())
+    assert not task.done()  # Blocked
+    await stream_buffer.pop(20)
+    assert (await task) is True
+
+
+@pytest.mark.asyncio
+async def test_stream_buffer_closed(event_loop: asyncio.AbstractEventLoop) -> None:
+    stream_buffer = StreamBuffer(EventWrapper)
+    await stream_buffer.close()
+    await stream_buffer._is_empty.wait()
+    await stream_buffer._paused.wait()
+    assert True
+    with pytest.raises(BufferCompleteError):
+        await stream_buffer.push(b"a")
+
+
+@pytest.mark.asyncio
+async def test_stream_buffer_complete(event_loop: asyncio.AbstractEventLoop) -> None:
+    stream_buffer = StreamBuffer(EventWrapper)
+    await stream_buffer.push(b"a" * 10)
+    assert not stream_buffer.complete
+    stream_buffer.set_complete()
+    assert not stream_buffer.complete
+    await stream_buffer.pop(20)
+    assert stream_buffer.complete
+
+
+@pytest.mark.asyncio
+async def test_protocol_handle_protocol_error() -> None:
+    protocol = H2Protocol(
+        Mock(), Config(), WorkerContext(), AsyncMock(), False, None, None, AsyncMock()
+    )
+    await protocol.handle(RawData(data=b"broken nonsense\r\n\r\n"))
+    protocol.send.assert_awaited()  # type: ignore
+    assert protocol.send.call_args_list == [call(Closed())]  # type: ignore
