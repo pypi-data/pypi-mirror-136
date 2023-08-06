e39bb0e4eb1b2acfece45489aea13eb93f872b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:22 +0000

    Add package atomate2/0.0.5/atomate2-0.0.5.tar.gz

---
+++ packages/atomate2/atomate2-0.0.5.tar.gz/atomate2-0.0.5/src/atomate2/utils/path.py
@@ -0,0 +1,56 @@
+"""Utilities for dealing with paths."""
+
+from __future__ import annotations
+
+import socket
+from pathlib import Path
+
+__all__ = ["get_uri", "strip_hostname"]
+
+
+def get_uri(dir_name: str | Path) -> str:
+    """
+    Return the URI path for a directory.
+
+    This allows files hosted on different file servers to have distinct locations.
+
+    Parameters
+    ----------
+    dir_name : str or Path
+        A directory name.
+
+    Returns
+    -------
+    str
+        Full URI path, e.g., "fileserver.host.com:/full/path/of/dir_name".
+    """
+    fullpath = Path(dir_name).absolute()
+    hostname = socket.gethostname()
+    try:
+        hostname = socket.gethostbyaddr(hostname)[0]
+    except (socket.gaierror, socket.herror):
+        pass
+    return f"{hostname}:{fullpath}"
+
+
+def strip_hostname(uri_path: str | Path) -> str:
+    """
+    Strop the hostname from a URI path.
+
+    For example, "fileserver.host.com:/full/path/of/dir_name" will be transformed to
+    "/full/path/of/dir_name".
+
+    Parameters
+    ----------
+    uri_path : str or Path
+        A URI path.
+
+    Returns
+    -------
+    str
+        The path without the hostname information.
+    """
+    dir_name = str(uri_path)
+    if ":" in dir_name:
+        dir_name = dir_name.split(":", 1)[1]
+    return dir_name
