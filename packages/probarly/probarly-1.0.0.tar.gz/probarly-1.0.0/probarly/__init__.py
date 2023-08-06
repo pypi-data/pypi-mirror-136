ad3b74ef03a853a555b5d91d81eb2aa92c21ae72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:24 +0000

    Add package probarly/1.0.0/probarly-1.0.0.tar.gz

---
+++ packages/probarly/probarly-1.0.0.tar.gz/probarly-1.0.0/probarly/__init__.py
@@ -0,0 +1,15 @@
+from probarly.probarly import (
+    Task,
+    Animation,
+    BarAnimations,
+    OtherAnimations,
+    Progbar,
+    AnimationType,
+    TaskType,
+)
+import probarly
+
+__version__ = "1.0.0"
+__author__ = "Phil-Gates"
+__credits__ = "me lol"
+__help__ = help(probarly.probarly)
