b7ba5536ea40a750f6658b95c0c7857df0a9d7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:14 +0000

    Add package django-quill-editor/0.1.38/django_quill_editor-0.1.38-py3-none-any.whl

---
+++ packages/django-quill-editor/django_quill_editor-0.1.38-py3-none-any.whl/playground/django_quill/config.py
@@ -0,0 +1,45 @@
+DEFAULT_CONFIG = {
+    "theme": "snow",
+    "modules": {
+        "syntax": True,
+        "toolbar": [
+            [
+                {"font": []},
+                {"header": []},
+                {"align": []},
+                "bold",
+                "italic",
+                "underline",
+                "strike",
+                "blockquote",
+                {"color": []},
+                {"background": []},
+            ],
+            ["code-block", "link", "image", "video"],
+            ["clean"],
+        ],
+        # quill-image-compress
+        "imageCompressor": {
+            "quality": 0.8,
+            "maxWidth": 2000,
+            "maxHeight": 2000,
+            "imageType": "image/jpeg",
+            "debug": False,
+            "suppressErrorLogging": True,
+        },
+        # quill-image-resize
+        "imageResize": {},
+    },
+}
+MEDIA_JS = [
+    "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/highlight.min.js",
+    "https://cdn.quilljs.com/1.3.7/quill.min.js",
+    "https://unpkg.com/quill-image-compress@1.2.21/dist/quill.imageCompressor.min.js",
+    "https://cdn.jsdelivr.net/npm/quill-image-resize-module@3.0.0/image-resize.min.js",
+    "django_quill/django_quill.js",
+]
+MEDIA_CSS = [
+    "https://cdn.quilljs.com/1.3.7/quill.snow.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/styles/darcula.min.css",
+    "django_quill/django_quill.css",
+]
