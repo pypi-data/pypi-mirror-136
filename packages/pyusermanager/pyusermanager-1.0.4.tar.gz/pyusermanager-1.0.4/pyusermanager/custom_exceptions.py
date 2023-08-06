a698c11f9aaa45622dc52de86184448a6f142a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:19 +0000

    Add package pyusermanager/1.0.4/pyusermanager-1.0.4.tar.gz

---
+++ packages/pyusermanager/pyusermanager-1.0.4.tar.gz/pyusermanager-1.0.4/pyusermanager/custom_exceptions.py
@@ -0,0 +1,15 @@
+class NotInitedException(Exception):
+    def __init__(self,*args,**kwargs):
+        Exception.__init__(self,*args,**kwargs)
+
+class MissingUserException(Exception):
+    def __init__(self,*args,**kwargs):
+        Exception.__init__(self,*args,**kwargs)
+
+class AlreadyExistsException(Exception):
+    def __init__(self,*args,**kwargs):
+        Exception.__init__(self,*args,**kwargs)
+
+class TokenMissingException(Exception):
+    def __init__(self,*args,**kwargs):
+        Exception.__init__(self,*args,**kwargs)
