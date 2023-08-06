d592062d374f2b4b24708dd4342e9281465e11c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:59 +0000

    Add package mlflow-oss-store/0.1.1/mlflow-oss-store-0.1.1.tar.gz

---
+++ packages/mlflow-oss-store/mlflow-oss-store-0.1.1.tar.gz/mlflow-oss-store-0.1.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mlflow_oss']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['oss2>=2.15.0,<3.0.0']
+
+entry_points = \
+{'mlflow.artifact_repository': ['oss = '
+                                'mlflow_oss.artifact_repo:OSSArtifactRepository']}
+
+setup_kwargs = {
+    'name': 'mlflow-oss-store',
+    'version': '0.1.1',
+    'description': 'MLflow artifact store plugin for AliCloud OSS',
+    'long_description': '# MLflow artifact store plugin for AliCloud OSS\n\nYet another MLflow artifact store plugin for AliCloud OSS.\n\nSomehow https://github.com/mrly16/mlflow-aliyunstore and https://github.com/SeaOfOcean/mlflow-aliyunstore\ndo not work for me. So I spent a rainy day to make another one.',
+    'author': 'Guan Hao',
+    'author_email': 'raptium@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/raptium/mlflow-oss-store.git',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
