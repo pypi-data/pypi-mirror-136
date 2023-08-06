0fd610d66badc381cb76aedef2538339c35d02be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:48 +0000

    Add package jb-misc-lib/0.1.2.2/jb-misc-lib-0.1.2.2.tar.gz

---
+++ packages/jb-misc-lib/jb-misc-lib-0.1.2.2.tar.gz/jb-misc-lib-0.1.2.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jb-misc-lib",
+    version="0.1.2.2",
+    author="Jason Brown",
+    author_email="jasonbrown_dev@protonmail.com",
+    description="Shared code for various personal and public projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/jrbrown/jb-misc-lib",
+    project_urls={
+        "Bug Tracker": "https://gitlab.com/jrbrown/jb-misc-lib/-/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+        "Operating System :: POSIX"
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
+
