802966008be7df3df9c04248a85bd277d8e73db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:44 +0000

    Add package streamlit-plotly-mapboxEvents/0.0.4/streamlit_plotly_mapboxEvents-0.0.4-py3-none-any.whl

---
+++ packages/streamlit-plotly-mapboxEvents/streamlit_plotly_mapboxEvents-0.0.4-py3-none-any.whl/streamlit_plotly_mapbox_events/frontend/build/precache-manifest.ca7c5b06687613b29ce7fe7b6ac195db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8642b4d7d4d4bfa297e47f261f781b27",
+    "url": "./index.html"
+  },
+  {
+    "revision": "45535daa0fea80c2cb85",
+    "url": "./static/js/2.c69c915a.chunk.js"
+  },
+  {
+    "revision": "177510fbe674e77c7ff004960ab47a68",
+    "url": "./static/js/2.c69c915a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c71dfecafc48e6cb603c",
+    "url": "./static/js/main.31e45009.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
