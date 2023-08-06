372e1ce3541f4f4f054fcec5c53181526767c488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:41 +0000

    Add package ChemW/0.1.4/ChemW-0.1.4.tar.gz

---
+++ packages/ChemW/ChemW-0.1.4.tar.gz/ChemW-0.1.4/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('README.rst') as file:
+    readme = file.read()
+
+setup(
+  name = 'ChemW',      
+  package_dir = {'mw':'chemw'},
+  packages = find_packages(),
+  package_data = {
+	'test':['databases/*'],
+    'chemw':['amino_acids_masses.json'],
+  },
+  version = '0.1.4',
+  license = 'MIT',
+  description = "Calculates the Molecular Weight, to the appropriate significant digits, from a string of an arbitrary chemical formula, protein sequence, or common chemical name.", 
+  long_description = readme,
+  author = 'Andrew Freiburger',               
+  author_email = 'andrewfreiburger@gmail.com',
+  url = 'https://github.com/freiburgermsu/chemw',   
+  keywords = ['chemistry', 'math', 'mass', 'weight', 'PHREEQC', 'molecular', 'mineral', 'formula', 'calculate'],
+  install_requires = ['chemicals', 'pandas', 'pubchempy']
+)
