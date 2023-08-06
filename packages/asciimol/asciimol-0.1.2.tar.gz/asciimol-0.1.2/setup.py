b7ced4e4ea6d7a99d816569275d4803e9b491cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:59 +0000

    Add package asciimol/0.1.2/asciimol-0.1.2.tar.gz

---
+++ packages/asciimol/asciimol-0.1.2.tar.gz/asciimol-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('./README.md') as f:
+    readme = f.read()
+
+with open('./LICENSE') as f:
+    lic = f.read()
+
+packages = find_packages()
+
+setup(
+    name='asciimol',
+    version='0.1.2',
+    description='An ASCII molecule viewer.',
+    long_description=readme,
+    author='Dominik Behrens',
+    author_email='dewberryants@gmail.com',
+    install_requires=['numpy'],
+    url='https://github.com/dewberryants/asciimol',
+    license=lic,
+    packages=find_packages(exclude="docs"),
+    package_data={"": ["data/*"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Chemistry"
+    ],
+)
