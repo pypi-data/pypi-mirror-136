792c8d2c233627385835cca38043cc5c8a9d0a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:19 +0000

    Add package citric/0.0.6/citric-0.0.6.tar.gz

---
+++ packages/citric/citric-0.0.6.tar.gz/citric-0.0.6/src/citric/exceptions.py
@@ -0,0 +1,21 @@
+"""Common exceptions."""
+
+
+class LimeSurveyError(Exception):
+    """Basic exception raised by LimeSurvey."""
+
+    def __init__(self, message: str) -> None:  # noqa:: ANN101
+        """Create a generic error for the LimeSurvey RPC API.
+
+        Args:
+            message: Exception message. By default none, and a generic message is used.
+        """
+        super().__init__(message)
+
+
+class LimeSurveyStatusError(LimeSurveyError):
+    """Exception raised when LimeSurvey responds with an error status."""
+
+
+class LimeSurveyApiError(LimeSurveyError):
+    """Exception raised when LimeSurvey responds with a non-null error."""
