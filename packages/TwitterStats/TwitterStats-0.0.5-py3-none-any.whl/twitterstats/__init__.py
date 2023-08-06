456b265c0629be7cbfb168aff30c17d786953a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:37 +0000

    Add package TwitterStats/0.0.5/TwitterStats-0.0.5-py3-none-any.whl

---
+++ packages/TwitterStats/TwitterStats-0.0.5-py3-none-any.whl/twitterstats/__init__.py
@@ -0,0 +1,16 @@
+import os
+from .api_client import TwitterAPI as _Fetcher
+
+VERSION = "0.0.5"
+
+
+def Fetcher(**kwargs):
+    if all([param is None for _, param in kwargs.items()]):
+        kwargs["consumer_key"] = os.getenv("TWITTER_CONSUMER_KEY")
+        kwargs["consumer_secret"] = os.getenv("TWITTER_CONSUMER_SECRET")
+        kwargs["access_token"] = os.getenv("TWITTER_ACCESS_KEY")
+        kwargs["access_token_secret"] = os.getenv("TWITTER_ACCESS_KEY_SECRET")
+    return _Fetcher(**kwargs)
+
+
+__all__ = ["VERSION", "Fetcher"]
