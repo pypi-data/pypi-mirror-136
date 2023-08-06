e955717fd32feaf5ecbba8a886c883ec0d17d799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:31 +0000

    Add package fred-matt-merman/0.6/fred_matt_merman-0.6.tar.gz

---
+++ packages/fred-matt-merman/fred_matt_merman-0.6.tar.gz/fred_matt_merman-0.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fred_matt_merman',
+    url='https://github.com/matt-merman',
+    author='Mattia Di Battista',
+    author_email='geom.dibattistamattia@gmail.com',
+    # Needed to actually package something
+    packages=find_packages(),
+    # Needed for dependencies
+    install_requires=['numpy', 'requests', 'pandas', 'matplotlib'],
+    version='0.6',
+)
