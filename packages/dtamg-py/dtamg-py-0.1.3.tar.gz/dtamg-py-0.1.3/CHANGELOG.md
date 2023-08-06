746651e020ffcf4c590b4826165a94e2810cde94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:50 +0000

    Add package dtamg-py/0.1.3/dtamg-py-0.1.3.tar.gz

---
+++ packages/dtamg-py/dtamg-py-0.1.3.tar.gz/dtamg-py-0.1.3/CHANGELOG.md
@@ -0,0 +1,25 @@
+## Controle de alterações
+
+Documentação das principais alterações sofridas por este repositório. Baseado na filosofia [Mantenha um Changelog](https://keepachangelog.com/pt-BR/1.0.0/).
+
+### [0.1.3] - 2022-01-31
+
+- Inclui função extract
+
+### [0.1.2] - 2021-12-21
+
+- Especifica versões de dependências com >=
+- Merge branch dev
+
+### [0.1.1] - 2021-12-21
+
+- Acrescenta função de validação e remoção colunas sqa e sqe
+
+### [0.1.0] - 2021-12-15
+
+- Otimiza função extract_resource
+
+### [0.0.900] - 2021-12-13
+
+- Criação do pacote
+- Migração projeto etl-make - https://github.com/dados-mg/etl-make
---
+++ packages/dtamg-py/dtamg-py-0.1.3.tar.gz/dtamg-py-0.1.3/dtamg_py/cli.py
@@ -0,0 +1,30 @@
+import click
+from dtamg_py.full_extract import full_extract_cli
+from dtamg_py.extract import extract_cli
+from dtamg_py.validate import validate_cli
+from dtamg_py.build_datapackage import build_datapackage_cli
+from dtamg_py.build_datapackages import build_datapackages_cli
+from dtamg_py.create import dpckan_create_cli
+from dtamg_py.update import dpckan_update_cli
+from dtamg_py.build_documentation_folder import build_documentation_folder_cli
+from dtamg_py.validate_tableschemas import validate_tableschemas_cli
+from dtamg_py.remove_sqs import remove_sqs_cli
+
+@click.group(context_settings=dict(help_option_names=["-h", "--help"]))
+def cli():
+  pass
+
+@cli.group()
+def etl_make():
+  pass
+
+etl_make.add_command(full_extract_cli)
+etl_make.add_command(extract_cli)
+etl_make.add_command(validate_cli)
+etl_make.add_command(build_datapackage_cli)
+etl_make.add_command(build_datapackages_cli)
+etl_make.add_command(dpckan_create_cli)
+etl_make.add_command(dpckan_update_cli)
+etl_make.add_command(build_documentation_folder_cli)
+etl_make.add_command(validate_tableschemas_cli)
+etl_make.add_command(remove_sqs_cli)
