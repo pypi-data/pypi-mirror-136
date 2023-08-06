f3a8a0a1abaf04eb6ee7a73299fe598fabfc14e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:54 +0000

    Add package wandb-mv/0.1.0/wandb_mv-0.1.0.tar.gz

---
+++ packages/wandb-mv/wandb_mv-0.1.0.tar.gz/wandb_mv-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='wandb_mv',
+    version='0.1.0',    
+    description='Model Versioning using Weight & Biases',
+    url='https://github.com/hectorLop/Wandb-MV',
+    author='Hector Lopez',
+    author_email='lopez.almazan.hector@gmail.com',
+    license='MIT',
+    packages=['wandb_mv'],
+    install_requires=['wandb']
+)
