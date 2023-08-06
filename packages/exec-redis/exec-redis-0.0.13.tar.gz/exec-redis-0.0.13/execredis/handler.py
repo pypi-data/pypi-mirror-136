ade8edc2e032b8f712546f4836c2bbcf0112cabe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:41 +0000

    Add package exec-redis/0.0.13/exec-redis-0.0.13.tar.gz

---
+++ packages/exec-redis/exec-redis-0.0.13.tar.gz/exec-redis-0.0.13/execredis/handler.py
@@ -0,0 +1,44 @@
+from json import dumps, loads
+
+
+class ExecMappingData:
+    def __init__(self, proj_name, redis_app):
+        self.__proj_name = proj_name
+        self.__redis_app = redis_app
+
+    def hget(self, name: str, default_value):
+        return loads(self.__redis_app.hget(
+            f"{self.__proj_name}." + 'MappingData', key=name.lower()) or b'null') or default_value
+
+
+class ExecMappingAction:
+    def __init__(self, mapping_data, proj_name, redis_app, logger_func=print):
+        self.__mapping_data = mapping_data
+        self.__proj_name = proj_name
+        self.__logger = logger_func
+        self.__redis_app = redis_app
+
+    def setup_mapping_data(self, mapping_data_default, value=None):
+        default_value = mapping_data_default
+        mapping_data = self.__mapping_data.__dict__
+        name = list(mapping_data.keys())[list(mapping_data.values()).index(default_value)]
+        if not value:
+            mapping_value = default_value
+        else:
+            if not isinstance(value, type(default_value)):
+                raise ValueError(f'Value: {value} type not match default.')
+            mapping_value = value
+        self.__setup_mapping_data(name, mapping_value)
+
+    def setup_all_mapping_data(self, support_types=(list, int, dict, str)):
+        flag = input('Will set all MappingData with default value on Redis. Y/N:')
+        if flag == 'Y':
+            for k, v in self.__mapping_data.__dict__.items():
+                if "__" not in k and isinstance(v, support_types):
+                    self.__setup_mapping_data(k, v)
+
+    def __setup_mapping_data(self, name: str, value):
+        self.__logger(f"Setup MappingData.{name}: {self.__hset_mapping_data(name.lower(), dumps(value))}")
+
+    def __hset_mapping_data(self, key=None, value=None, mapping=None):
+        return self.__redis_app.hset(f"{self.__proj_name}." + 'MappingData', key=key, value=value, mapping=mapping)
