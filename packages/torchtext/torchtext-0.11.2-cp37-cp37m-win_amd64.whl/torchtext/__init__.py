bd2525bfec5383cbc28276fed2138d494420fe7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:12 +0000

    Add package torchtext/0.11.2/torchtext-0.11.2-cp37-cp37m-win_amd64.whl

---
+++ packages/torchtext/torchtext-0.11.2-cp37-cp37m-win_amd64.whl/torchtext/__init__.py
@@ -0,0 +1,26 @@
+from . import data
+from . import nn
+from . import datasets
+from . import utils
+from . import vocab
+from . import legacy
+from ._extension import _init_extension
+
+
+try:
+    from .version import __version__, git_version  # noqa: F401
+except ImportError:
+    pass
+
+__all__ = ['data',
+           'nn',
+           'datasets',
+           'utils',
+           'vocab',
+           'legacy']
+
+
+_init_extension()
+
+
+del _init_extension
