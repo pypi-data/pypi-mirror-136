36d55183c8a2ec060d1e4147e2d38c6ebd0b7a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:58 +0000

    Add package mlphys/0.0.31/mlphys-0.0.31.tar.gz

---
+++ packages/mlphys/mlphys-0.0.31.tar.gz/mlphys-0.0.31/deepqis/network/inference.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import pkg_resources
+from deepqis.utils import Extract_Net as arlnet
+import os
+
+def load():
+    """ Return pre-trained model"""
+    # path_to_model, file_name = os.path.split(__file__)
+    path_to_model = os.path.join(__file__, 'models/ARL_ONRL_meas_n_1000_qubits_2_batch_4_alpha_0.4_BEST')
+    # stream = pkg_resources.resource_stream(path_to_model)
+    return path_to_model
+
+def fit(input_data):
+
+    loaded_file = load()
+    model = tf.keras.models.load_model(loaded_file, custom_objects={'ErrorNode':arlnet.ErrorNode, \
+                                        'PredictDensityMatrix':arlnet.PredictDensityMatrix})
+
+    def scaling_mean_0_std_1(row_matrix):
+        m = tf.math.reduce_mean(row_matrix)
+        std = tf.math.reduce_std(row_matrix)
+        scaled = (row_matrix - m) / std
+        return scaled
+
+    x_in = scaling_mean_0_std_1(input_data, [-1, 6, 6, 1])
+    logits, dm_pred = model(x_in)
+
+    return dm_pred
+
+print(load())
