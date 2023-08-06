305ab20245c80946d6ee6a18cd780a48310994d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:06 +0000

    Add package loadguard-runner/0.0.5/loadguard_runner-0.0.5-py3-none-any.whl

---
+++ packages/loadguard-runner/loadguard_runner-0.0.5-py3-none-any.whl/loadguard/defaults.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+# :module:`loadguard.defaults`
+
+This module provides a various set of values.
+
+This file is a part of LoadGuard Runner.
+
+(c) 2022, Deepnox SAS.
+"""
+
+LG_PROJECT_CONFIGURATION_DEFAULT_FILENAME: str = "loadguard-project.yml"
+""" The default filename of a LoadGuard project configuration. """
