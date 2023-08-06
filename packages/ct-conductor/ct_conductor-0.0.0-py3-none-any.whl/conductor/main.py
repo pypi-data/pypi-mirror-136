ec4a7b4fc49ecab91e307d0a4afc291d36456362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:15 +0000

    Add package ct-conductor/0.0.0/ct_conductor-0.0.0-py3-none-any.whl

---
+++ packages/ct-conductor/ct_conductor-0.0.0-py3-none-any.whl/conductor/main.py
@@ -0,0 +1,9 @@
+
+import sys
+
+def cli():
+    print('This is not the conductor package you are looking for! Download instructions are in the CLI Quick Start documentation. Uninstall this package using python3 -m pip uninstall ct-conductor.')
+    sys.exit()
+
+if __name__ == '__main__':
+    cli()
