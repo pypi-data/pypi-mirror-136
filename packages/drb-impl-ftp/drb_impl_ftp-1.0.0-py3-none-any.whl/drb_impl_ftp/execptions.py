370235a6d0b3a622d1e8514f990d95d05c91b377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:08 +0000

    Add package drb-impl-ftp/1.0.0/drb_impl_ftp-1.0.0-py3-none-any.whl

---
+++ packages/drb-impl-ftp/drb_impl_ftp-1.0.0-py3-none-any.whl/drb_impl_ftp/execptions.py
@@ -0,0 +1,9 @@
+from drb.exceptions import DrbException, DrbFactoryException
+
+
+class DrbFtpNodeException(DrbException):
+    pass
+
+
+class DrbFtpNodeFactoryException(DrbFactoryException):
+    pass
