0c10469ac8d0eb0d5999bc7255e291d5325774b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:15 +0000

    Add package pydapper/0.1.2/pydapper-0.1.2-py3-none-any.whl

---
+++ packages/pydapper/pydapper-0.1.2-py3-none-any.whl/pydapper/__init__.py
@@ -0,0 +1,7 @@
+from .main import connect
+from .main import register
+from .main import using
+from .mssql import PymssqlCommands as _PymssqlCommands
+from .mysql import MySqlConnectorPythonCommands as _MySqlConnectorPythonCommands
+from .postgresql import Psycopg2Commands as _Psycopg2Commands
+from .sqlite import Sqlite3Commands as _Sqlite3Commands
