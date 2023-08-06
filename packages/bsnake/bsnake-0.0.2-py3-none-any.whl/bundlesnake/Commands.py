b82dc61e389ba32ec6910514f437fa591e750781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:15 +0000

    Add package bsnake/0.0.2/bsnake-0.0.2-py3-none-any.whl

---
+++ packages/bsnake/bsnake-0.0.2-py3-none-any.whl/bundlesnake/Commands.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from tkinter.filedialog import askdirectory
+import os
+import shutil
+
+class Commands():
+
+	def __init__(self):
+		self.dir_name = "."
+
+	def get_path(self):
+		self.dir_name = askdirectory()
+
+	def create(self , packname, path):
+		if not path :
+			self.get_path()
+		packname , run_name = packname.split(':')
+		os.makedirs(f"{self.dir_name}/{packname}/{packname}")
+		files=os.listdir("./dep")
+		for fname in files:
+			shutil.copy2(os.path.join("./dep",fname),f"{self.dir_name}/{packname}/" )
+		with open(f"{self.dir_name}/{packname}/{packname}/__init__.py" ,"w+") as f :
+				f.write(" ")				
+		with open(f"{self.dir_name}/{packname}/{packname}/{run_name}.py" ,"w+") as f :
+				f.write("#GETTING STARTED")				
