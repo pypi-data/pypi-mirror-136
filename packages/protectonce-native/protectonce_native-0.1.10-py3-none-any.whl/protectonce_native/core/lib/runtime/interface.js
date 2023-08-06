79c04e0a347a1381cacfe118225a2e1c9a4f7daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:19 +0000

    Add package protectonce-native/0.1.10/protectonce_native-0.1.10-py3-none-any.whl

---
+++ packages/protectonce-native/protectonce_native-0.1.10-py3-none-any.whl/protectonce_native/core/lib/runtime/interface.js
@@ -0,0 +1,25 @@
+const login = require('../modules/login');
+const rasp = require('../modules/rasp');
+const waf = require('../modules/waf');
+const httpServer = require('../modules/httpServer');
+const userMonitoring = require('../modules/userMonitoring');
+
+function stop() {
+    // TODO: Implement this method and add cleanup if any
+    return null;
+}
+
+function sync() {
+    // TODO: Implement this method
+    return null;
+}
+
+module.exports = {
+    init: login,
+    sync: sync,
+    userMonitoring: userMonitoring,
+    rasp: rasp,
+    waf: waf,
+    httpServer: httpServer,
+    stop: stop
+};
