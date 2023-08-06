2d6af2869e6945ecab70697cd76fa3c5be0ceab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:32 +0000

    Add package qBitrr/2.3.3/qBitrr-2.3.3.tar.gz

---
+++ packages/qBitrr/qBitrr-2.3.3.tar.gz/qBitrr-2.3.3/qBitrr/bundled_data.py
@@ -0,0 +1,6 @@
+version = "2.3.3"
+git_hash = "b02334e"
+license_text = (
+    "Licence can be found on:\n\nhttps://github.com/Drapersniper/Qbitrr/blob/master/LICENSE"
+)
+patched_version = f"{version}-{git_hash}"
