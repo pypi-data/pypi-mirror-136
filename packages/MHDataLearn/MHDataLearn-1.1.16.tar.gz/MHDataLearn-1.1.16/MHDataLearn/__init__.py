0250f27c00cd9e5f202d9f07516d688bd4785e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:15 +0000

    Add package MHDataLearn/1.1.16/MHDataLearn-1.1.16.tar.gz

---
+++ packages/MHDataLearn/MHDataLearn-1.1.16.tar.gz/MHDataLearn-1.1.16/MHDataLearn/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+This package contains tools to: preprocess raw Mental Health
+Services Data Set (MHSDS) data in preparation for analysis,
+train classification machine learning models and select the
+best performing model, and apply the selected model to
+new, preprocessed MHSDS data to identify people at high
+risk of readmission.
+"""
+
+from MHDataLearn import preprocessing, modelselector
