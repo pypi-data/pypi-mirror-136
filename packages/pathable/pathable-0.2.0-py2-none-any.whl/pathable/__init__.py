0ba9ece0489dcef6da354c701a0dcb99c9db1e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:20 +0000

    Add package pathable/0.2.0/pathable-0.2.0-py2-none-any.whl

---
+++ packages/pathable/pathable-0.2.0-py2-none-any.whl/pathable/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Pathable module"""
+from pathable.paths import (
+    BasePath, AccessorPath, DictOrListPath,
+    DictOrListPath as DictPath,
+    DictOrListPath as ListPath,
+)
+
+__author__ = 'Artur Maciag'
+__email__ = 'maciag.artur@gmail.com'
+__version__ = '0.2.0'
+__url__ = 'https://github.com/p1c2u/pathable'
+__license__ = 'Apache License, Version 2.0'
+
+__all__ = [
+    'BasePath', 'AccessorPath', 'DictOrListPath',
+    'DictPath', 'ListPath',
+]
