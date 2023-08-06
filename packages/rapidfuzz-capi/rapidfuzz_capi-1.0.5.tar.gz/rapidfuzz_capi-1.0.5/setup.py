67a1a71f9f030b0aaa2456c0ea0dfc2c789d6ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:57 +0000

    Add package rapidfuzz-capi/1.0.5/rapidfuzz_capi-1.0.5.tar.gz

---
+++ packages/rapidfuzz-capi/rapidfuzz_capi-1.0.5.tar.gz/rapidfuzz_capi-1.0.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open('README.md', 'rt', encoding="utf8") as f:
+    readme = f.read()
+
+setup(
+    name="rapidfuzz_capi",
+    version="1.0.5",
+    url="https://github.com/maxbachmann/rapidfuzz_capi",
+    author="Max Bachmann",
+    author_email="pypi@maxbachmann.de",
+    description="C-API of RapidFuzz, which can be used to extend RapidFuzz from separate packages",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+
+    license="MIT",
+    classifiers=[
+        'Programming Language :: C',
+        'Programming Language :: C++',
+        'Programming Language :: Fortran',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools'
+    ],
+    packages=["rapidfuzz_capi"],
+    zip_safe=True,
+    include_package_data=True
+)
