332b29bea389f26c2f4b670c1e44eabdb02b4938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:46 +0000

    Add package twintel/1.5.2/twintel-1.5.2-py3-none-any.whl

---
+++ packages/twintel/twintel-1.5.2-py3-none-any.whl/twintel/__init__.py
@@ -0,0 +1,7 @@
+from twintel._methods.stream import stream
+from twintel._methods.flatfile import flatfile
+from twintel._methods.search import search
+from twintel._methods.timeline import timeline
+
+import nltk
+nltk.download('vader_lexicon')
