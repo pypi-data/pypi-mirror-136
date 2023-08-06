a955e1047239b88944bb561077d7b629bb703e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:34 +0000

    Add package drb-impl-java/1.0.0/drb_impl_java-1.0.0-py3-none-any.whl

---
+++ packages/drb-impl-java/drb_impl_java-1.0.0-py3-none-any.whl/drb_impl_java/__init__.py
@@ -0,0 +1,15 @@
+
+from . import _version
+
+__version__ = _version.get_versions()['version']
+
+from .drb_impl_java_node import DrbJavaBaseNode
+
+from .drb_impl_java_node import DrbJavaNode
+
+del _version
+
+__all__ = [
+    'DrbJavaNode',
+    'DrbJavaBaseNode'
+]
