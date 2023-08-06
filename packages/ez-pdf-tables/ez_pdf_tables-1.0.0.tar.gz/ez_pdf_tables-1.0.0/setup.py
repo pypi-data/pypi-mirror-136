5faeb60129380eb467a3993b9d8bc553c10a94c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:10 +0000

    Add package ez-pdf-tables/1.0.0/ez_pdf_tables-1.0.0.tar.gz

---
+++ packages/ez-pdf-tables/ez_pdf_tables-1.0.0.tar.gz/ez_pdf_tables-1.0.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ez_pdf_tables",
+    version="1.0.0",
+    author="lamerlink",
+    author_email="lamerlink@live.com",
+    description="An easy interface to quickly make PDF reports from CSV or DataFrames.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LamerLink/ez_pdf_tables",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pandas',
+        'reportlab'
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "Typing :: Typed",
+    ],
+    python_requires='>=3.7',
+)
