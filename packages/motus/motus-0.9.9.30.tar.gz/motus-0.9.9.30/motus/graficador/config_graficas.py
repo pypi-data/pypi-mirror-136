5b2f265eadd63ae1d924d3eff07a026342866fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:43 +0000

    Add package motus/0.9.9.30/motus-0.9.9.30.tar.gz

---
+++ packages/motus/motus-0.9.9.30.tar.gz/motus-0.9.9.30/motus/graficador/config_graficas.py
@@ -0,0 +1,18 @@
+class ConfigGrafica:
+    def __init__(self):
+        self.limite_eje_x_min = None
+        self.limite_eje_x_max = None
+        self.limite_eje_y_min = None
+        self.limite_eje_y_max = None
+        self.objeto_1 = True
+        self.objeto_2 = True
+        self.objeto_3 = True
+        self.objeto_4 = True
+        self.angulo = 220
+        self.dist_max_figuras = 24
+        self.total_figuras = 2
+        self.tipo_figura = "Cilindros"
+        self.tipo_arreglo_regresion_lineal = "Velocidad"
+        self.brinco_temporal_regresion_lineal= 2
+        self.longitud_serie_tiempo = 2
+        self.regiones_preferidas_3d = None
