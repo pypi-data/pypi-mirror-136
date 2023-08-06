52d57dd130c4ea92a6aaddcc2309eecb9ecd031c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:13 +0000

    Add package setriq/1.4.2/setriq-1.4.2.tar.gz

---
+++ packages/setriq/setriq-1.4.2.tar.gz/setriq-1.4.2/src/setriq/_C/python_wrapper.cpp
@@ -0,0 +1,71 @@
+//
+// Created by Benjamin Tenmann on 22/11/2021.
+//
+
+#ifndef EXTENSION_NAME
+#define EXTENSION_NAME _C
+#endif
+
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include "pairwise_distance_computation.h"
+#include "metrics/CdrDist.h"
+#include "metrics/Levenshtein.h"
+#include "metrics/TcrDist.h"
+#include "utils/type_defs.h"
+
+namespace py = pybind11;
+
+py::list cdr_dist(const string_vector_t& sequences,
+                  const double_matrix_t& substitution_matrix,
+                  const token_index_map_t& index,
+                  const double& gap_opening_penalty,
+                  const double& gap_extension_penalty) {
+    metric::CdrDist metric {substitution_matrix, index, gap_opening_penalty, gap_extension_penalty};
+
+    double_vector_t out = pairwise_distance_computation(metric, sequences);
+    return py::cast(out);
+}
+
+py::list levenshtein(const string_vector_t& sequences, const double& extra_cost) {
+    metric::Levenshtein metric {extra_cost};
+
+    double_vector_t out = pairwise_distance_computation(metric, sequences);
+    return py::cast(out);
+}
+
+py::list tcr_dist_component(const string_vector_t& sequences,
+                            const double_matrix_t& substitution_matrix,
+                            const token_index_map_t& index,
+                            const double& gap_penalty,
+                            const char& gap_symbol,
+                            const double& distance_weight) {
+    metric::TcrDist metric {substitution_matrix, index, gap_penalty, gap_symbol, distance_weight};
+
+    double_vector_t out = pairwise_distance_computation(metric, sequences);
+    return py::cast(out);
+}
+
+PYBIND11_MODULE(EXTENSION_NAME, m) {
+    m.doc() = "Python module written in C++ for pairwise distance computation for sequences.";
+
+    m.def("cdr_dist", &cdr_dist, "Compute the pairwise CDR-dist metric for a set of CDR3 sequences.",
+          py::arg("sequences"), py::arg("substitution_matrix"), py::arg("index"),
+          py::arg("gap_opening_penalty"), py::arg("gap_extension_penalty"));
+
+    m.def("levenshtein", &levenshtein, "Compute the pairwise Levenshtein distances for a set of sequences.",
+          py::arg("sequences"), py::arg("extra_cost"));
+
+    m.def("tcr_dist_component", &tcr_dist_component, "Compute pairwise TCR-dist for a set of TCR components.",
+          py::arg("sequences"), py::arg("substitution_matrix"), py::arg("index"),
+          py::arg("gap_penalty"), py::arg("gap_symbol"), py::arg("weight"));
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
