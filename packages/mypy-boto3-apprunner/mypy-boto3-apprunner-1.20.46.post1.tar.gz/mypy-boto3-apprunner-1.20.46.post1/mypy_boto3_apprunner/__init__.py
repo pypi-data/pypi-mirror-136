5812408af5bc068fdc25aba4015934f17de03a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:45 +0000

    Add package mypy-boto3-apprunner/1.20.46.post1/mypy-boto3-apprunner-1.20.46.post1.tar.gz

---
+++ packages/mypy-boto3-apprunner/mypy-boto3-apprunner-1.20.46.post1.tar.gz/mypy-boto3-apprunner-1.20.46.post1/mypy_boto3_apprunner/__init__.py
@@ -0,0 +1,24 @@
+"""
+Main interface for apprunner service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_apprunner import (
+        AppRunnerClient,
+        Client,
+    )
+
+    session = boto3.Session()
+
+    client: AppRunnerClient = boto3.client("apprunner")
+    session_client: AppRunnerClient = session.client("apprunner")
+    ```
+"""
+from .client import AppRunnerClient
+
+Client = AppRunnerClient
+
+
+__all__ = ("AppRunnerClient", "Client")
