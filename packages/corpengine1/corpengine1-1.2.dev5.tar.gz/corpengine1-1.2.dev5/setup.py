9134310e6aaabb5ab9a2d1c73852c4ca03ea0038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:57 +0000

    Add package corpengine1/1.2.dev5/corpengine1-1.2.dev5.tar.gz

---
+++ packages/corpengine1/corpengine1-1.2.dev5.tar.gz/corpengine1-1.2.dev5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+changelog = 'v1.2.dev5\n\nnew setIcon() at Window & set cursor fixes'
+
+setup(
+    name='corpengine1',
+    version='1.2.dev5',
+    description='Fast, Organized, Object-Oriented Python Game Development',
+    long_description='github repository: https://github.com/corpengine/corpengine' + '\n\nChangelog:\n'+changelog,
+    url='https://corpengine.github.io/',
+    author='CORPEngine Organization + Contributors',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='corpengine',
+    packages=find_packages(),
+    install_requires=['easygui', 'pygame']
+)
