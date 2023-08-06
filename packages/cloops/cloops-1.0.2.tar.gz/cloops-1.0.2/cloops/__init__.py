a2a05e2a463a2fd05f2dc6f676a327dfde5e43a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:23 +0000

    Add package cloops/1.0.2/cloops-1.0.2.tar.gz

---
+++ packages/cloops/cloops-1.0.2.tar.gz/cloops-1.0.2/cloops/__init__.py
@@ -0,0 +1,32 @@
+_version_ = '0.1.0'
+import random
+def whp(what_do_you_want_to_print):
+  while True:
+    print(what_do_you_want_to_print)
+def fop(how_many_times, what_do_you_want_to_print):
+  for i in range(how_many_times):
+    print(what_do_you_want_to_print)
+def foc(range_):
+  for i in range(range_):
+    print(i)
+def fori(what_do_you_want_to_print_random_in_range_0_100):
+  a = random.randint(0, 100)
+  for i in range(a):
+    print(what_do_you_want_to_print_random_in_range_0_100)
+def wht():
+    while True:
+        True
+def cal():
+    while True:
+        z = eval(input('Put your math problem here: '))
+        print(z)
+def ral(your_list):
+  a = random.choice(your_list)
+  print(a)
+def foc(your_word):
+    print(your_word)
+    for i in your_word:
+        your_word = your_word[0:len(your_word)-1]
+        print(your_word)
+
+    
