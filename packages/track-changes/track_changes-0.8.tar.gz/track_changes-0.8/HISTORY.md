b6ca6d8d5df13f794e14665be0f98100dce04720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:54 +0000

    Add package track-changes/0.8/track_changes-0.8.tar.gz

---
+++ packages/track-changes/track_changes-0.8.tar.gz/track_changes-0.8/HISTORY.md
@@ -0,0 +1,24 @@
+# 🎁 Release notes (0.7)
+
+## Changes
+```
+fix: fixed tempalted not being included
+ref: fixed pypi readme markdown
+ref: updated history md
+fix: format
+fix: github somehow ignored the newline
+ref: updated changes log
+fix: fixed errors on macos
+ref: renamed
+ref: updated doc; changelog
+feat: usable
+fix: fixed entrypoint; fixed version style
+ref: rm redundant code
+```
+
+## Metadata
+```
+This version -------- 0.7
+Previous version ---- 0.1
+Total commits ------- 12
+```
---
+++ packages/track-changes/track_changes-0.8.tar.gz/track_changes-0.8/track_changes/__init__.py
@@ -0,0 +1,53 @@
+import os
+from typing import Optional
+
+import fire
+from jinja2 import Environment, FileSystemLoader
+from track_changes.utils import run_system_command
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+j2_env = Environment(loader=FileSystemLoader(THIS_DIR), trim_blocks=True)
+
+
+def render_changelog(
+    version: Optional[str] = None,
+    previous_version: Optional[str] = None,
+    out: Optional[str] = None,
+) -> None:
+    """Render Changelog from template
+
+    Args:
+        version (Optional[str], optional): render changelog for which version. Defaults to None, meaning the latest git tag veresion.
+        previous_version (Optional[str], optional): render changelog since which verision. Defaults to None, meaning the one before the latest git tag version.
+    """
+    if version is None:
+        code, version, _ = run_system_command("git describe --tags --abbrev=0")
+        assert code == 0, "Failed to get latest git tag version"
+        version = version.strip()
+    if previous_version is None:
+        code, previous_version, _ = run_system_command(
+            f"git describe --abbrev=0 --tags {version}^"
+        )
+        assert code == 0, "Failed to get previous git tag version"
+        previous_version = previous_version.strip()
+    code, changes, _ = run_system_command(
+        f"git log --pretty=format:%s {previous_version}..{version}"
+    )
+    assert code == 0, "Failed to get git log"
+    changes = changes.splitlines()
+    res = j2_env.get_template("resources/changelog_template.md").render(
+        version=version,
+        prev_version=previous_version,
+        changes=changes,
+        commits=len(changes),
+    )
+
+    if out is None:
+        print(res)
+    else:
+        with open(out, "w", encoding="utf-8") as f:
+            f.write(res)
+
+
+def entrypoint() -> None:
+    fire.Fire(render_changelog)
