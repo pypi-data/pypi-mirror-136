8a4666b4b0420694ecd3688ed98145d8b41ac475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:14 +0000

    Add package dose/1.2.3/dose-1.2.3-py2.py3-none-any.whl

---
+++ packages/dose/dose-1.2.3-py2.py3-none-any.whl/dose/__init__.py
@@ -0,0 +1,10 @@
+"""Dose GUI for TDD: package metadata."""
+
+# Be careful: this file is imported by setup.py!
+
+__version__ = "1.2.3"
+__author__ = "Danilo de Jesus da Silva Bellini"
+__author_email__  = "danilo.bellini@gmail.com"
+__url__ = "https://github.com/danilobellini/dose"
+
+_from_setup = False
