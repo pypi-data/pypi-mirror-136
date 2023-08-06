9eaf806b39d7c2e0f07f243dfd8b69b1bb4d6c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:51 +0000

    Add package corpengine1/1.2.dev3/corpengine1-1.2.dev3.tar.gz

---
+++ packages/corpengine1/corpengine1-1.2.dev3.tar.gz/corpengine1-1.2.dev3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+changelog = 'v1.2.dev3\n\nFixed color range checking at Colors.mix and list element numbers.'
+
+setup(
+    name='corpengine1',
+    version='1.2.dev3',
+    description='Fast, Organized, Object-Oriented Python Game Development',
+    long_description='github repository: https://github.com/corpengine/corpengine' + '\n\nChangelog:\n'+changelog,
+    url='https://corpengine.github.io/',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='corpengine',
+    packages=find_packages(),
+    install_requires=['easygui', 'pygame']
+)
