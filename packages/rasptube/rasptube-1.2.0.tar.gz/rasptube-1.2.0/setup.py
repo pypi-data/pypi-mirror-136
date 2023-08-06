78ffb5aec12ca38766110b230bacfc4e02e3c805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:55 +0000

    Add package rasptube/1.2.0/rasptube-1.2.0.tar.gz

---
+++ packages/rasptube/rasptube-1.2.0.tar.gz/rasptube-1.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+__project__ = "rasptube"
+__version__ = "1.2.0"
+__description__ = "a python module that automates the process for making videos compatable with the Raspberry Pi"
+__packages__ = ["rasptube"]
+__author__ = "Walter J Hare"
+__keywords__ = ["raspberry pi", "compatability", "youtube"]
+__requires__ = ["os"]
+__license__ = "MIT"
+__classifiers__ = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Education",
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License"
+]
+
+setup(
+    name = __project__,
+    version = __version__,
+    description = __description__,
+    packages = __packages__,
+    author = __author__,
+    classifiers = __classifiers__,
+    keywords = __keywords__,
+    requires = __requires__,
+    license = __license__
+)
