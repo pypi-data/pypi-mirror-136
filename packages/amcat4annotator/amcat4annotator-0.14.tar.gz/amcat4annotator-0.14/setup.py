86a6231fcdefe0986a2399622314dc6708c0ac84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:16 +0000

    Add package amcat4annotator/0.14/amcat4annotator-0.14.tar.gz

---
+++ packages/amcat4annotator/amcat4annotator-0.14.tar.gz/amcat4annotator-0.14/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="amcat4annotator",
+    version="0.14",
+    description="Annotator Backend API for AmCAT4 Text Analysis",
+    author="Wouter van Atteveldt, Farzam Fanitabasi",
+    author_email="wouter@vanatteveldt.com",
+    packages=["amcat4annotator"],
+    include_package_data=True,
+    zip_safe=False,
+    keywords=["API", "text"],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Text Processing",
+    ],
+    install_requires=[
+        "Flask",
+        "Flask-HTTPAuth",
+        "flask-cors",
+        "peewee",
+        'authlib',
+        'bcrypt'
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-flask',
+            'codecov',
+        ]
+    },
+)
