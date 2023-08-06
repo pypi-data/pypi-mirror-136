bc15caadcf8e6b08ecfd2512708341f507ce82b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:27 +0000

    Add package azaka/0.2.0/Azaka-0.2.0-py3-none-any.whl

---
+++ packages/azaka/Azaka-0.2.0-py3-none-any.whl/azaka/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.2.0"
+
+from .client import *
+from .tools import *
+from .connection import *
+from .context import *
+from .interface import *
---
+++ packages/azaka/Azaka-0.2.0-py3-none-any.whl/azaka/tools/__init__.py
@@ -0,0 +1,5 @@
+from .enums import *
+from .mixins import *
+from .paginator import *
+from ..commands import *
+from .utils import *
