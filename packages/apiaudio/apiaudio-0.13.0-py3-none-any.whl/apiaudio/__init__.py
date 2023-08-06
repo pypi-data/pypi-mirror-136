48c09e165baeb07ec4f09568d2c63c83cde967bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:55 +0000

    Add package apiaudio/0.13.0/apiaudio-0.13.0-py3-none-any.whl

---
+++ packages/apiaudio/apiaudio-0.13.0-py3-none-any.whl/apiaudio/__init__.py
@@ -0,0 +1,25 @@
+# Aflorithmic Python bindings
+# API docs at https://docs.api.audio/
+
+# Configuration variables
+
+sdk_version = "0.13.0"
+api_key = None
+client_id = None
+api_base = "https://v1.api.audio"
+upload_api_base = "https://file.api.audio"  # not implemented yet.
+
+# future
+api_version = None
+verify_ssl_certs = True
+proxy = None
+default_http_client = None
+app_info = None
+enable_telemetry = True
+max_network_retries = 0
+
+# Set to either 'debug' or 'info', controls console logging
+log = None
+
+# API resources
+from apiaudio.api_resources import *
