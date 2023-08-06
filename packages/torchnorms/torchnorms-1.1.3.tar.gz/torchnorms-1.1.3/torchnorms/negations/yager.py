fe790d766b3e273f258be737e1aa1322e684c047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:51 +0000

    Add package torchnorms/1.1.3/torchnorms-1.1.3.tar.gz

---
+++ packages/torchnorms/torchnorms-1.1.3.tar.gz/torchnorms-1.1.3/torchnorms/negations/yager.py
@@ -0,0 +1,23 @@
+import torch
+from typing import Optional
+from torch import nn, Tensor
+from torchnorms.negations.base import BaseNegation
+
+
+class YagerNegation(BaseNegation):
+    def __init__(self,
+                 p: Optional[Tensor] = None,
+                 default_p: float = 0.1) -> None:
+        super().__init__()
+        self.p = p
+        if self.p is None:
+            assert default_p > -1
+            self.p = nn.Parameter(torch.tensor(default_p))
+        assert len(self.p.shape) == 0
+
+    def __call__(self,
+                 a: Tensor) -> Tensor:
+        res: Optional[Tensor] = None
+        res = torch.pow((1.0 - torch.pow(a,self.p)), 1.0 / self.p)
+
+        return res
---
+++ packages/torchnorms/torchnorms-1.1.3.tar.gz/torchnorms-1.1.3/torchnorms/tnorms/dombi.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import torch
+from torch import nn, Tensor
+
+from torchnorms.tnorms.base import BaseTNorm
+
+from typing import Optional
+
+
+class DombiTNorm(BaseTNorm):
+    def __init__(self,
+                 p: Optional[Tensor] = None,
+                 default_p: float = 0.1) -> None:
+        super().__init__()
+        self.p = p
+        if self.p is None:
+            self.p = nn.Parameter(torch.tensor(default_p))
+        assert len(self.p.shape) == 0
+
+    def __call__(self,
+                 a: Tensor,
+                 b: Tensor) -> Tensor:
+        res: Optional[Tensor] = None
+        p_1 = torch.pow((1-a)/a , self.p)
+        p_2 = torch.pow((1-b)/b, self.p)
+        p_3 = 1.0 + torch.pow(p_1 + p_2, 1.0/self.p)
+        res = 1.0 / p_3
+
+        return res
