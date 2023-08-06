ad0ff7733b1a577faf80b9f368a284bfe2116e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:53 +0000

    Add package quick-sql/1.0.0/quick-sql-1.0.0.tar.gz

---
+++ packages/quick-sql/quick-sql-1.0.0.tar.gz/quick-sql-1.0.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="quick-sql",
+    version="1.0.0",
+    author="Kishcods",
+    author_email="kisshancods@protonmail.com",
+    description="A python library for sqlite3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "quick_sqlite"},
+    packages=setuptools.find_packages(where="quick_sqlite"),
+    python_requires=">=3.7",
+)
