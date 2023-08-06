688bc7140a203f5c71cfdc85645f2ce74811c6e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:18 +0000

    Add package Flask-Redmail/0.2.0/Flask-Redmail-0.2.0.tar.gz

---
+++ packages/Flask-Redmail/Flask-Redmail-0.2.0.tar.gz/Flask-Redmail-0.2.0/flask_redmail/test/test_send.py
@@ -0,0 +1,60 @@
+
+from flask_redmail import RedMail
+from flask import Flask
+from redmail import EmailSender
+
+def dummy_send(msg):
+    pass
+
+def test_send(cls_dummy_smtp):
+    app = Flask("pytest")
+    app.config["EMAIL_HOST"] = "localhost"
+    app.config["EMAIL_PORT"] = 0
+    app.config["EMAIL_USER"] = "me@example.com"
+    app.config["EMAIL_PASSWORD"] = "1234"
+    app.config["EMAIL_SENDER"] = "no-reply@example.com"
+    app.config["EMAIL_CLS_SMTP"] = cls_dummy_smtp
+
+    email = RedMail()
+    email.init_app(app)
+    assert email.sender is None
+
+    with app.app_context():
+        email.sender.send_message = dummy_send
+        email.send(
+            subject="An example",
+            receivers=["me@example.com"],
+            html="<h1>An example</h1>"
+        )
+        
+    assert email.sender is None
+
+def test_send_defaults(cls_dummy_smtp):
+    app = Flask("pytest")
+    app.config["EMAIL_HOST"] = "localhost"
+    app.config["EMAIL_PORT"] = 0
+    app.config["EMAIL_USER"] = "me@example.com"
+    app.config["EMAIL_PASSWORD"] = "1234"
+    app.config["EMAIL_SENDER"] = "config@example.com"
+    app.config["EMAIL_CLS_SMTP"] = cls_dummy_smtp
+
+    email = RedMail(subject="Default subject", sender="default@example.com")
+    email.init_app(app)
+    assert email.sender is None
+
+    with app.app_context():
+        email.sender.send_message = dummy_send
+        msg = email.send(
+            subject="Subject",
+            receivers=["me@example.com"],
+            html="<h1>An example</h1>",
+            sender="sender@example.com",
+        )
+        assert dict(msg.items()) == {
+            'from':'sender@example.com',
+            'subject':'Subject',
+            'to':'me@example.com',
+            'Content-Type':'multipart/alternative',
+        }
+        
+    assert email.sender is None
