94321daeacce722e4081a012393e9bddcace3e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:22 +0000

    Add package elementary-lineage/0.1.4/elementary-lineage-0.1.4.tar.gz

---
+++ packages/elementary-lineage/elementary-lineage-0.1.4.tar.gz/elementary-lineage-0.1.4/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='elementary-lineage',
+    description='elementary-lineage is deprecated and moved to elementary-data',
+    version='0.1.4',
+    python_requires='>=3.6.2',
+    entry_points='''
+        [console_scripts]
+        edr=cli.cli:cli
+    ''',
+    author="Elementary",
+    keyword="data, lineage, data lineage, data warehouse, DWH, observability, data monitoring, data observability, "
+            "Snowflake, BigQuery, data reliability, analytics engineering",
+    long_description=README,
+    install_requires=[
+        'elementary-data'
+    ],
+    long_description_content_type="text/markdown",
+    license='',
+    url='https://github.com/elementary-data/elementary',
+    author_email='or@elementary-data.com',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+)
