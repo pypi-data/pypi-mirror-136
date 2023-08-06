6a61d45857ecc17d428f78f02cd1e67d8c339e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:58 +0000

    Add package pypadb/0.0.9/pypadb-0.0.9-py3-none-any.whl

---
+++ packages/pypadb/pypadb-0.0.9-py3-none-any.whl/pypadb/conf/table_configurer.py
@@ -0,0 +1,22 @@
+from pypadb.table.base_table import BaseTable
+
+
+class TableConfigurer:
+    __slots__ = ['tables']
+
+    tables: dict[str, BaseTable]
+
+    def __init__(self):
+        self.tables = {}
+
+    def __getattr__(self, item: str) -> BaseTable:
+        return self.tables[item]
+
+    def __getitem__(self, item: str) -> BaseTable:
+        return self.tables[item]
+
+    def init_tables(self, **kwargs):
+        self.tables = {**self.tables, **kwargs}
+
+
+tables: TableConfigurer = TableConfigurer()
