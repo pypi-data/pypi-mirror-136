229ac75b62e5373afec0c6b3ab6fa750362ad942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:38 +0000

    Add package ElectronicsCalculator/0.2/ElectronicsCalculator-0.2.tar.gz

---
+++ packages/ElectronicsCalculator/ElectronicsCalculator-0.2.tar.gz/ElectronicsCalculator-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ElectronicsCalculator',
+    version='0.2',
+    packages=['ElectronicsCalculator'],
+    url='https://github.com/mino827/ElectronicsCalculator',
+    license='GPL v3.0',
+    author='Mino Girimonti',
+    author_email='mino827@protonmail.com',
+    description='Python module that provides methods to perform common electronics calculations.',
+    keywords=['electronics', 'electrical', 'engineering', 'formula', 'formulae', 'calculation']
+)
