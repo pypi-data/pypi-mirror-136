0fcb38439a62c6ea463e9d48fb78d017d7345252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:24 +0000

    Add package blockchainmod/0.0.1/blockchainmod-0.0.1.tar.gz

---
+++ packages/blockchainmod/blockchainmod-0.0.1.tar.gz/blockchainmod-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A module to help you make a blockchain'
+LONG_DESCRIPTION = 'This package helps make a blockchain with less than 10 lines of code!'
+
+# Setting up
+setup(
+    name="blockchainmod",
+    version=VERSION,
+    author="Advaith S",
+    author_email="<popular9adu@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'blockchain', 'python3', 'cryptocurrency'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
