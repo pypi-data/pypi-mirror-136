541c3591738484b3f34cdbe20be591b0668734b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:34 +0000

    Add package metaphor-connectors/0.10.10/metaphor-connectors-0.10.10.tar.gz

---
+++ packages/metaphor-connectors/metaphor-connectors-0.10.10.tar.gz/metaphor-connectors-0.10.10/metaphor/tableau/config.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+from pydantic.dataclasses import dataclass
+from serde import deserialize
+
+from metaphor.common.base_config import BaseConfig
+
+
+@deserialize
+@dataclass
+class TableauTokenAuthConfig:
+    """Config for Personal Access Token authentication"""
+
+    token_name: str
+    token_value: str
+
+
+@deserialize
+@dataclass
+class TableauPasswordAuthConfig:
+    """Config for Username Password authentication"""
+
+    username: str
+    password: str
+
+
+@deserialize
+@dataclass
+class TableauRunConfig(BaseConfig):
+    server_url: str
+    site_name: str
+    access_token: Optional[TableauTokenAuthConfig]
+    user_password: Optional[TableauPasswordAuthConfig]
+
+    # Snowflake data source account
+    snowflake_account: Optional[str]
+
+    disable_preview_image: bool = False
