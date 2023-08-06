cdc3e8e4f4926d82639538ae4a2514f0d170cfee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:27 +0000

    Add package vsdx/0.4.17/vsdx-0.4.17.tar.gz

---
+++ packages/vsdx/vsdx-0.4.17.tar.gz/vsdx-0.4.17/vsdx/__init__.py
@@ -0,0 +1,16 @@
+namespace = "{http://schemas.microsoft.com/office/visio/2012/main}"  # visio file name space
+ext_prop_namespace = '{http://schemas.openxmlformats.org/officeDocument/2006/extended-properties}'
+vt_namespace = '{http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes}'
+r_namespace = '{http://schemas.openxmlformats.org/officeDocument/2006/relationships}'
+document_rels_namespace = "{http://schemas.openxmlformats.org/package/2006/relationships}"
+cont_types_namespace = '{http://schemas.openxmlformats.org/package/2006/content-types}'
+
+from .shapes import Cell
+from .connectors import Connect
+from .shapes import DataProperty
+from .pages import Page
+from .pages import PagePosition
+from .shapes import Shape
+from .vsdxfile import VisioFile
+from .media import Media
+from .geometry import Geometry, GeometryRow, GeometryCell
