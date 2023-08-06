cea4289092ce78c9f0c8da0e33be9f1076d2c255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:26 +0000

    Add package HandsAnalyzer/0.0.6/HandsAnalyzer-0.0.6.tar.gz

---
+++ packages/HandsAnalyzer/HandsAnalyzer-0.0.6.tar.gz/HandsAnalyzer-0.0.6/HandsAnalyzer/helpers/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""=== Face Analyzer Helpers =>
+    Module : helpers
+    Author : Saifeddine ALOUI (ParisNeo)
+    Licence : MIT
+    Description :
+        User interface helpers
+<================"""
