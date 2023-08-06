8c71067d4a6b778f72c62551fbefc543823b2fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:51 +0000

    Add package honey/0.1.0/honey-0.1.0.tar.gz

---
+++ packages/honey/honey-0.1.0.tar.gz/honey-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='honey',
+    version='0.1.0',
+    description='',
+    url='http://github.com/qe/honey',
+    author='Alex Ismodes',
+    author_email='helloemailmerighthere@gmail.com',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    keywords='honey',
+    packages=['honey'],
+    install_requires=[''],
+    )
