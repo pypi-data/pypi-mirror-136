413601ba4ab870d166208d18f76f6f32bc634485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:12 +0000

    Add package discordwebhook.py/1.0.1/discordwebhook.py-1.0.1.tar.gz

---
+++ packages/discordwebhook.py/discordwebhook.py-1.0.1.tar.gz/discordwebhook.py-1.0.1/discordwebhook/__init__.py
@@ -0,0 +1,9 @@
+
+from discordwebhook import versions, webhook, embed, allowedmentions
+
+
+Webhook = webhook.Webhook
+Embed = embed.Embed
+AllowedMentions = allowedmentions.AllowedMentions
+
+CurrentVersion = versions.Current
---
+++ packages/discordwebhook.py/discordwebhook.py-1.0.1.tar.gz/discordwebhook.py-1.0.1/discordwebhook/versions.py
@@ -0,0 +1,4 @@
+
+class Current:
+    name = "1.0.1"
+    release_date = "30 January 2022"
