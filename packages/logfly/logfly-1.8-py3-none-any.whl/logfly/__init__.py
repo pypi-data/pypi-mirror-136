108e2041bd3ba6ab9e69ababe616ed21812d87c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:29 +0000

    Add package logfly/1.8/logfly-1.8-py3-none-any.whl

---
+++ packages/logfly/logfly-1.8-py3-none-any.whl/logfly/__init__.py
@@ -0,0 +1,178 @@
+#####################
+# Author: Yuki Sui
+# Date: 2022-1-30
+#####################
+
+import time
+import shutil
+import os
+from pathlib import Path
+from colorama import init
+
+init(autoreset=True)
+__version__ = '1.8'
+
+
+def create_file(pathfile='', filenname=''):
+    if pathfile is None:
+        return False
+    if not os.path.exists(pathfile):
+        os.makedirs(pathfile)
+    else:
+        pass
+    filepath = pathfile + filenname
+    if not os.path.isfile(filepath):
+        with open(filepath, 'w', encoding='utf-8') as filefiletemp:
+            filefiletemp.write('')
+            filefiletemp.close()
+    else:
+        pass
+
+
+def create_log_folder(folder_name, hidden):
+    dirs = '.\\logs\\'
+    dirs2 = '.\\logs\\' + folder_name + '\\' + get_time('date') + '\\'
+    dirs3 = str(Path.home()) + '\\.1o9f1y\\' + folder_name + '\\' + get_time('date') + '\\'
+    if not os.path.exists(dirs):
+        os.makedirs(dirs)
+    else:
+        pass
+    if not os.path.exists(dirs2):
+        os.makedirs(dirs2)
+    if hidden == 'yes':
+        if not os.path.exists(dirs3):
+            os.makedirs(dirs3)
+        else:
+            pass
+    else:
+        pass
+
+
+def get_time(flag):
+    if flag == 'datetime':
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    elif flag == 'date':
+        return time.strftime("%Y-%m-%d", time.localtime())
+    elif flag == 'times':
+        return time.strftime("%H:%M:%S", time.localtime())
+    elif flag == 'datetimefile':
+        return time.strftime("%Y%m%d%H%M%S", time.localtime())
+    elif flag == 'timestamp':
+        return time.time()
+
+
+# noinspection PyTypeChecker
+def write_log(name, position, level, message, mode='add',
+              folder_name='logflys', hidden='no', color='yes', str_message='yes'):
+    global LOGFILE, LOGFILE2, logfolder, logfolder_hidden, LOGFILE_hidden
+    if str_message == 'yes':
+        message = str(message)
+    elif str_message == 'no':
+        pass
+    else:
+        error()
+    if hidden == "no":
+        logfolder = '.\\logs\\' + folder_name + '\\' + get_time('date') + '\\'
+    elif hidden == 'yes':
+        logfolder = '.\\logs\\' + folder_name + '\\' + get_time('date') + '\\'
+        logfolder_hidden = str(Path.home()) + '\\.1o9f1y\\' + folder_name + '\\' + get_time('date') + '\\'
+    if mode == 'add':
+        LOGFILE_hidden = name + '-' + get_time('date') + '.log'
+        LOGFILE = logfolder + name + '-' + get_time('date') + '.log'
+    elif mode == 'new':
+        LOGFILE_hidden = name + '-' + get_time('datetimefile') + '.log'
+        LOGFILE = logfolder + name + '-' + get_time('datetimefile') + '.log'
+    else:
+        error()
+    create_log_folder(folder_name, hidden)
+    if position == 'CLI':
+        if color == 'no':
+            print(name + ' ' + get_time('datetime') + ' ' + '[' + str.upper(level) + ']' + ' ' + message + '\r\n')
+        elif color == 'yes':
+            LogFlyMessage = name + ' ' + get_time('datetime') + ' ' + '[' + str.upper(level) + ']' + ' ' + message + \
+                            '\r\n '
+            if str.upper(level) == 'INFO':
+                print(f'\033[0;34m{LogFlyMessage}\033[0m')
+            elif str.upper(level) == 'WARNING':
+                print(f'\033[0;33m{LogFlyMessage}\033[0m')
+            elif str.upper(level) == 'ERROR':
+                print(f'\033[0;31m{LogFlyMessage}\033[0m')
+            else:
+                print(f'\033[0;37m{LogFlyMessage}\033[0m')
+        else:
+            error()
+    elif position == 'file':
+        if mode == 'add':
+            File = open(LOGFILE, 'a', newline='', encoding='utf-8')
+            File.write(get_time('datetime') + ' ' + '[' + str.upper(level) + ']' + ' ' + message + '\r\n')
+            File.close()
+            if hidden == 'yes':
+                LOGFILE2 = logfolder_hidden + LOGFILE_hidden
+                File = open(LOGFILE2, 'a', newline='', encoding='utf-8')
+                File.write(get_time('datetime') + ' ' + '[' + str.upper(level) + ']' + ' ' + message + '\r\n')
+                File.close()
+        elif mode == 'new':
+            File = open(LOGFILE, 'w', newline='', encoding='utf-8')
+            File.write(get_time('datetime') + ' ' + '[' + str.upper(level) + ']' + ' ' + message + '\r\n')
+            File.close()
+            if hidden == 'yes':
+                LOGFILE2 = logfolder_hidden + LOGFILE_hidden
+                File = open(LOGFILE2, 'a', newline='', encoding='utf-8')
+                File.write(get_time('datetime') + ' ' + '[' + str.upper(level) + ']' + ' ' + message + '\r\n')
+                File.close()
+        else:
+            error()
+    elif position == 'fileCLI':
+        if color == 'no':
+            print(name + ' ' + get_time('datetime') + ' ' + '[' + str.upper(level) + ']' + ' ' + message + '\r\n')
+        elif color == 'yes':
+            LogFlyMessage = name + ' ' + get_time('datetime') + ' ' + '[' + str.upper(level) + ']' + ' ' + message + \
+                            '\r\n '
+            if str.upper(level) == 'INFO':
+                print(f'\033[0;34m{LogFlyMessage}\033[0m')
+            elif str.upper(level) == 'WARNING':
+                print(f'\033[0;33m{LogFlyMessage}\033[0m')
+            elif str.upper(level) == 'ERROR':
+                print(f'\033[0;31m{LogFlyMessage}\033[0m')
+            else:
+                print(f'\033[0;37m{LogFlyMessage}\033[0m')
+        else:
+            error()
+        if mode == 'add':
+            File = open(LOGFILE, 'a', newline='', encoding='utf-8')
+            File.write(get_time('datetime') + ' ' + '[' + str.upper(level) + ']' + ' ' + message + '\r\n')
+            File.close()
+            if hidden == 'yes':
+                LOGFILE2 = logfolder_hidden + LOGFILE_hidden
+                File = open(LOGFILE2, 'a', newline='', encoding='utf-8')
+                File.write(get_time('datetime') + ' ' + '[' + str.upper(level) + ']' + ' ' + message + '\r\n')
+                File.close()
+        elif mode == 'new':
+            File = open(LOGFILE, 'w', newline='', encoding='utf-8')
+            File.write(get_time('datetime') + ' ' + '[' + str.upper(level) + ']' + ' ' + message + '\r\n')
+            File.close()
+            if hidden == 'yes':
+                LOGFILE2 = logfolder_hidden + LOGFILE_hidden
+                File = open(LOGFILE2, 'a', newline='', encoding='utf-8')
+                File.write(get_time('datetime') + ' ' + '[' + str.upper(level) + ']' + ' ' + message + '\r\n')
+                File.close()
+        else:
+            error()
+    else:
+        error()
+
+
+def mv_file(original_file_name, new_file_name, folder_name='mv_file'):
+    shutil.move(original_file_name, new_file_name)
+    message = 'File ' + original_file_name + ' moved to ' + new_file_name
+    write_log('logfly-log', 'CLI', 'error', message, folder_name=folder_name)
+
+
+def error():
+    logflyErrorMessage = 'your parameter is wrong, please re-check it! \r\nAnd you can see the manual at ' \
+                         'https://github.com/tinqlo/logfly '
+    write_log('logfly-log', 'CLI', 'error', logflyErrorMessage)
+
+
+if __name__ == '__main__':
+    write_log('Doctor Who', 'fileCLI', 'info', "this is Doctor's log, only in CLI.")
