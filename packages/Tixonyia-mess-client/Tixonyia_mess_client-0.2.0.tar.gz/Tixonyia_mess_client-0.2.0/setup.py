0d239cafc7eea3db55a843607042cb3d8fd7bdf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:41 +0000

    Add package Tixonyia-mess-client/0.2.0/Tixonyia_mess_client-0.2.0.tar.gz

---
+++ packages/Tixonyia-mess-client/Tixonyia_mess_client-0.2.0.tar.gz/Tixonyia_mess_client-0.2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="Tixonyia_mess_client",
+      version="0.2.0",
+      description="Mess Client",
+      author="Vasilev Artem",
+      author_email="220220220220@rambler.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
