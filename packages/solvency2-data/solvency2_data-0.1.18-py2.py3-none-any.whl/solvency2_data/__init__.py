55c3370e98485546f77ca5c4a1fec7ac248d6379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:26 +0000

    Add package solvency2-data/0.1.18/solvency2_data-0.1.18-py2.py3-none-any.whl

---
+++ packages/solvency2-data/solvency2_data-0.1.18-py2.py3-none-any.whl/solvency2_data/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for solvency2-data."""
+
+__version__ = "0.1.18"
+
+from .rfr import *
+from .eiopa_data import get, refresh
+from .util import set_config
+from .smith_wilson import *
