e469d5e48529afd12daad32ea961f0d3742ca02a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:45 +0000

    Add package mvt/1.4.9/mvt-1.4.9.tar.gz

---
+++ packages/mvt/mvt-1.4.9.tar.gz/mvt-1.4.9/mvt/android/modules/adb/__init__.py
@@ -0,0 +1,26 @@
+# Mobile Verification Toolkit (MVT)
+# Copyright (c) 2021 The MVT Project Authors.
+# Use of this software is governed by the MVT License 1.1 that can be found at
+#   https://license.mvt.re/1.1/
+
+from .chrome_history import ChromeHistory
+from .dumpsys_accessibility import DumpsysAccessibility
+from .dumpsys_batterystats import DumpsysBatterystats
+from .dumpsys_full import DumpsysFull
+from .dumpsys_packages import DumpsysPackages
+from .dumpsys_procstats import DumpsysProcstats
+from .dumpsys_receivers import DumpsysReceivers
+from .files import Files
+from .getprop import Getprop
+from .logcat import Logcat
+from .packages import Packages
+from .processes import Processes
+from .root_binaries import RootBinaries
+from .settings import Settings
+from .sms import SMS
+from .whatsapp import Whatsapp
+
+ADB_MODULES = [ChromeHistory, SMS, Whatsapp, Processes, Getprop, Settings,
+               DumpsysAccessibility, DumpsysBatterystats, DumpsysProcstats,
+               DumpsysPackages, DumpsysReceivers, DumpsysFull,
+               Packages, RootBinaries, Logcat, Files]
